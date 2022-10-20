82e9966045123bfd9f8243ac550e19b71b4d81a6
sora-k46 <sorrawit456789@gmail.com>
2022-10-20 22:54:20 +0700

    Extrack callback function for collecting data from microsoft

--- backend/src/modules/middleware/microsoft.ts
+++ backend/src/modules/middleware/microsoft.ts
@@ -1,27 +1,115 @@
-import passport from "passport"
 import { Strategy as MicrosoftStrategy } from "passport-microsoft"
 import OAuth2Strategy from "passport-oauth2"
-import dotenv from "dotenv"
-import path from "path"
+import axios from "axios"
+import { PrismaClient } from "@prisma/client"
 
-dotenv.config({ path: path.resolve(__dirname, "/.env")})
+/**
+ *
+ * @param prisma taking in prisma client for storing data from Microsoft to our database
+ * @returns done(null, profile) if everything goes right or done(error) when error happend
+ *
+ * when create new object of Microsoft Strategy, it takes 2 arguments
+ * 1. object that contains Application information and setup (clientID, clientSecret, etc.)
+ * 2. a function that handle the response from the microsoft graph database in this case verify(prisma)
+ *
+ * verify(prisma): take prisma and use it to send data to our database when user login into our application via microsoft
+ *
+ * more info on official document of passport-microsoft
+ */
+const verify: (prisma: PrismaClient) => OAuth2Strategy.VerifyFunction =
+    (prisma) => async (accessToken: string, refreshToken: string, profile: any, done: OAuth2Strategy.VerifyCallback) => {
+        let profile_pic: any
 
-export default () => {
-    passport.use(
-        "microsoft",
-        new MicrosoftStrategy(
-            {
-                clientID: process.env.CLIENT_ID || "",
-                clientSecret: process.env.CLIENT_SECRET || "", 
-                callbackURL: process.env.CALLBACK_URL,
-                scope: ["user.read", "mail.read", "offline_access"],
-                authorizationURL: process.env.AUTH_URL,
-                tokenURL: process.env.TOKEN_URL
-            },
-            (accessToken: string, refreshToken: string, profile: any, done: OAuth2Strategy.VerifyCallback) => {
-                console.log(profile)
-                done(null, profile)
+        // getting image of user from microsoft
+        try {
+            profile_pic = await axios.get(`https://graph.microsoft.com/v1.0/me/photos/48x48/$value`, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                    "Content-Type": "image/jpg",
+                },
+                responseType: "arraybuffer",
+            })
+        } catch (err) {
+            profile_pic = {
+                error: err,
             }
-        )
+        }
+
+        // getting student id (onPremisesSamAccountName)
+        try {
+            const { data: onPremisesSamAccountName } = await axios.get(
+                `https://graph.microsoft.com/v1.0/users/${profile.id}?$select=onPremisesSamAccountName`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`,
+                        "Content-Type": "application/json",
+                    },
+                }
+            )
+
+            // extract data as json from profile
+            const { _json } = profile
+
+            // check if the user is a student of KMUTT or not
+            if (_json.jobTitle !== "Student") throw new Error("You must be student of KMUTT")
+
+            // extract firstname and lastname
+            const fullname = _json.displayName.split(" ")
+            
+            // doing upsert operation
+            const user = await prisma.user_profile.upsert({
+                where: {
+                    userId: _json.id,
+                },
+                update: {
+                    studentId: onPremisesSamAccountName.onPremisesSamAccountName,
+                    fName: fullname[0],
+                    lName: fullname[1],
+                    email: _json.mail,
+                    image: profile_pic.data || null,
+                    student_major: "111",
+                },
+                create: {
+                    userId: _json.id,
+                    studentId: onPremisesSamAccountName.onPremisesSamAccountName,
+                    fName: fullname[0],
+                    lName: fullname[1],
+                    email: _json.mail,
+                    image: profile_pic.data || null,
+                    student_major: "111",
+                }
+            })
+            // return a callback
+            return done(null, {
+                studentId: user.studentId,
+                fName: user.fName,
+                lName: user.lName,
+                email: user.email,
+                image: user.image,
+            })
+        } catch (err: any) {
+            console.error(err)
+            return done(err)
+        }
+    }
+
+/**
+ * export function that take prisma (db ORM) as argument and return
+ * Microsoft Strategy for authentication with passport
+ *
+ * เป็นฟังชั่นที่รับเอา prisma แล้ว return กลับออกมาเป็น Microsoft Strategy เอาไว้ authenticate
+ * กับ passport ผ่าน passport.use()
+ */
+export default (prisma: PrismaClient) => {
+    return new MicrosoftStrategy(
+        {
+            clientID: process.env.CLIENT_ID || "",
+            clientSecret: process.env.CLIENT_SECRET || "",
+            callbackURL: process.env.CALLBACK_URL,
+            scope: ["user.read", "mail.read", "offline_access"],
+            authorizationURL: process.env.AUTH_URL,
+            tokenURL: process.env.TOKEN_URL,
+        },
+        verify(prisma)
     )
 }
