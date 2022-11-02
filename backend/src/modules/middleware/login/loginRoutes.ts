8b9c085c9b8e633b85bd9e5f382f80880988a4ae
sora-k46 <sorrawit456789@gmail.com>
2022-11-02 22:26:31 +0700

    Add successRedirect URL and format code

--- backend/src/modules/middleware/login/loginRoutes.ts
+++ backend/src/modules/middleware/login/loginRoutes.ts
@@ -1,19 +1,27 @@
-import { Router } from "express";
-import passport from "passport";
-import { Request, Response } from "express";
+import { Router } from "express"
+import passport from "passport"
+import { Request, Response } from "express"
 
 const router = Router()
 
-router.get("/microsoft", passport.authenticate("microsoft", {
-  prompt: "select_account",
-  session: false
-}))
+router.get(
+    "/microsoft",
+    passport.authenticate("microsoft", {
+        prompt: "select_account",
+        session: false,
+    })
+)
 
-router.get("/microsoft/callback", passport.authenticate("microsoft", {
-  failureRedirect: "/auth/microsoft",
-  session: false
-}), (req: Request, res: Response) => {
-  res.json(req.user)
-})
+router.get(
+    "/microsoft/callback",
+    passport.authenticate("microsoft", {
+        failureRedirect: "/auth/microsoft",
+        successRedirect: process.env.SUCCESS_REDIRECT_URL,
+        session: false,
+    }),
+    (req: Request, res: Response) => {
+        res.json(req.user)
+    }
+)
 
 export { router as loginRoutes }
