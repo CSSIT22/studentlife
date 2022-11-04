8574b52c86157f1d0172fd99b7a0ea88a3715e42
Thitipa <thitipanet@gmail.com>
2022-11-04 15:53:46 +0700

    Navbar beta

--- frontend/src/pages/dating/index.tsx
+++ frontend/src/pages/dating/index.tsx
@@ -1,10 +1,51 @@
-import React from 'react'
-import AppBody from '../../components/share/app/AppBody'
+import React from "react"
+import { AiFillCamera } from "react-icons/ai"
+import AppBody from "../../components/share/app/AppBody"
 
 const DatingRandomization = () => {
-  return (
-    <AppBody>Randomization</AppBody>
-  )
+    return (
+        <AppBody
+            secondarynav={[
+                {
+                    name: "Randomization",
+                    to: "/dating",
+                },
+                {
+                    name: "Heart Checking",
+                    to: "/dating/likedyou",
+                },
+                {
+                    name: "You are match with",
+                    to: "/dating/match",
+                },
+                {
+                    name: "Activity poll",
+                    to: "/dating/poll",
+                },
+                {
+                    name: "Tag of interest",
+                    to: "/dating/interest",
+                    isRight: true,
+                    disableText: true,
+                    Icon: AiFillCamera,
+                },
+                {
+                    name: "Option",
+                    to: "/dating/option",
+                    isRight: true,
+                    disableText: true,
+                },
+                {
+                    name: "Tutorial",
+                    to: "/dating/tutorial/welcome",
+                    isRight: true,
+                    disableText: true,
+                },
+            ]}
+        >
+            Randomization
+        </AppBody>
+    )
 }
 
 export default DatingRandomization
