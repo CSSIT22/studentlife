de0eae8616ec1bf7f7ede41fa6617b2e8526093a
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2022-11-05 01:30:25 +0700

    add modal and button for event in history, recyclebin

--- frontend/src/pages/announcement/history.tsx
+++ frontend/src/pages/announcement/history.tsx
@@ -2,14 +2,38 @@ import React from 'react'
 import HeaderPage from '../../components/annoucement/HeaderPage'
 import AppBody from '../../components/share/app/AppBody'
 import PostOnHistory from '../../components/annoucement/PostOnHistory';
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Button, Stack,useDisclosure,Text } from '@chakra-ui/react';
 
 const history = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
   return (
     <AppBody>
       <HeaderPage head='History'/>
       <PostOnHistory topic='Hello World' sender='SAMO-SIT' status=''/>
       <PostOnHistory topic='Hello World' sender='SAMO-SIT' status='approve'/>
       <PostOnHistory topic='Hello World' sender='SAMO-SIT' status='disapprove'/>
+
+      <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose} size={"xs"} isCentered>
+                <ModalOverlay bg="blackAlpha.300" backdropFilter="blur(10px) hue-rotate(90deg)" />
+                <ModalContent>
+                    <ModalHeader>WARNING</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody pb={6}>
+                        <Text>This announcement will be kept for 3 days. After these 3 days, you won't be able to recover this announcement.</Text>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme="blue" mr={3}>
+                            Delete
+                        </Button>
+                        <Button onClick={onClose}>Cancel</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+            <Stack position={"fixed"} bottom="5rem" width="91%">
+                <Button onClick={onOpen} height="3.5rem">Delete announcement</Button>
+                <Button>Cancel</Button>
+            </Stack>
     </AppBody>
   )
 }
--- frontend/src/pages/announcement/recyclebin.tsx
+++ frontend/src/pages/announcement/recyclebin.tsx
@@ -1,16 +1,51 @@
+import {
+    Button,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalCloseButton,
+    ModalBody,
+    ModalFooter,
+    Text,
+    useDisclosure,
+    Stack,
+    Box,
+} from "@chakra-ui/react"
 import React from "react"
 import HeaderPage from "../../components/annoucement/HeaderPage"
 import PostOnRecycle from "../../components/annoucement/PostOnRecycle"
 import AppBody from "../../components/share/app/AppBody"
 
 const recyclebin = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
     return (
         <AppBody>
             <HeaderPage head="Recycle bin" />
             <PostOnRecycle topic="Hello world" sender="SAMO-SIT" expired="48:12:02" />
             <PostOnRecycle topic="Hi" sender="SAMO-MEDIA" expired="48:12:02" />
             <PostOnRecycle topic="Hi kub" sender="SAMO-SCI" expired="48:12:02" />
-            <PostOnRecycle topic="Hi ka" sender="SAMO-ENGIN" expired="48:12:02" />
+            <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose} size={"xs"} isCentered>
+                <ModalOverlay bg="blackAlpha.300" backdropFilter="blur(10px) hue-rotate(90deg)" />
+                <ModalContent>
+                    <ModalHeader>Recover the announcement</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody pb={6}>
+                        <Text>Are you sure to recover this announcement?</Text>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme="blue" mr={3}>
+                            Recover
+                        </Button>
+                        <Button onClick={onClose}>Cancel</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+            <Stack position={"fixed"} bottom="5rem" width="91%">
+                <Button onClick={onOpen} height="3.5rem">Recover announcement</Button>
+                <Button>Cancel</Button>
+            </Stack>
         </AppBody>
     )
 }
