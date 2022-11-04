aebf206771f657a9c1fcb646f225a09108eec5a4
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2022-11-05 01:47:56 +0700

    add PostOnTop component, update font size of topic in PostOnHistory and PostOnRecycle component

--- frontend/src/components/annoucement/PostOnHistory.tsx
+++ frontend/src/components/annoucement/PostOnHistory.tsx
@@ -1,32 +1,35 @@
-import { Flex, Heading, Spacer,Box,Text } from '@chakra-ui/react'
-import React, { FC } from 'react'
-import {HiCheckCircle,HiXCircle} from 'react-icons/hi';
-import {TbLoader} from 'react-icons/tb';
+import { Flex, Heading, Spacer, Box, Text } from "@chakra-ui/react"
+import React, { FC } from "react"
+import { HiCheckCircle, HiXCircle } from "react-icons/hi"
+import { TbLoader } from "react-icons/tb"
 
-const PostOnHistory:FC<{
-    topic:string,
-    sender:string,
-    status:string
-}> = ({topic,sender,status}) => {
-    const state = (stat:string) =>{
-        if(stat == "approve"){
-            return  <HiCheckCircle fontSize={"2rem"}/>
-        }else if (stat == "disapprove"){
-            return <HiXCircle fontSize={"2rem"}/>
-        }else {
-            return <TbLoader fontSize={"2rem"}/>
+const PostOnHistory: FC<{
+    topic: string
+    sender: string
+    status: string
+}> = ({ topic, sender, status }) => {
+    const state = (stat: string) => {
+        if (stat == "approve") {
+            return <HiCheckCircle fontSize={"2rem"} />
+        } else if (stat == "disapprove") {
+            return <HiXCircle fontSize={"2rem"} />
+        } else {
+            return <TbLoader fontSize={"2rem"} />
         }
     }
-  return (
-    <Box height={"5rem"} width={"100%"} p="5" mt="5" backgroundColor="#D9D9D9" rounded="lg">
-    <Flex alignItems={"center"}>
-        <Box pr={"1rem"} width=''>{state(status)}</Box>
-        <Box>
-            <Heading size={"md"}>{topic}</Heading>
-            <Heading size={"xs"}>{sender}</Heading>
+    return (
+        <Box height={"5rem"} width={"100%"} p="5" mt="5" backgroundColor="#D9D9D9" rounded="lg">
+            <Flex alignItems={"center"}>
+                <Box pr={"1rem"} width="">
+                    {state(status)}
+                </Box>
+                <Box>
+                    <Heading size={"sm"}>{topic}</Heading>
+                    <Text fontSize={"xs"}>{sender}</Text>
+                </Box>
+            </Flex>
         </Box>
-    </Flex>
-</Box>  )
+    )
 }
 
 export default PostOnHistory
--- frontend/src/components/annoucement/PostOnRecycle.tsx
+++ frontend/src/components/annoucement/PostOnRecycle.tsx
@@ -1,21 +1,23 @@
 import { Flex, Heading, Spacer, Box, Text } from "@chakra-ui/react"
 import React, { FC } from "react"
 
-const PostOnRecycle:FC<{
-    topic:string,
-    sender:string,
-    expired:string
-}> = ({topic,sender,expired}) => {
+const PostOnRecycle: FC<{
+    topic: string
+    sender: string
+    expired: string
+}> = ({ topic, sender, expired }) => {
     return (
         <Box height={"5rem"} width={"100%"} p="5" mt="5" backgroundColor="#D9D9D9" rounded="lg">
             <Flex alignItems={"center"}>
                 <Box>
-                    <Heading size={"md"}>{topic}</Heading>
-                    <Heading size={"xs"}>{sender}</Heading>
+                    <Heading size={"sm"}>{topic}</Heading>
+                    <Text fontSize={"xs"}>{sender}</Text>
                 </Box>
                 <Spacer />
                 <Box textAlign={"right"}>
-                    <Text fontSize={"xs"}>Expired date</Text>
+                    <Text fontSize={"xs"} as="b">
+                        Expired date
+                    </Text>
                     <Text fontSize={"xs"}>{expired}</Text>
                 </Box>
             </Flex>
