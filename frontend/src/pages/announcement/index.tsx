b7059eee63dc11ecfd5401b088a18230af8ba276
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2022-11-06 02:43:13 +0700

    [UPDATE] index(pin event)

--- frontend/src/pages/announcement/index.tsx
+++ frontend/src/pages/announcement/index.tsx
@@ -1,17 +1,30 @@
 import React from "react"
 import { IoIosAddCircle } from "react-icons/io"
+import { Link } from "react-router-dom"
 import HeaderPage from "../../components/annoucement/HeaderPage"
 import PostOnAnnouncementPage from "../../components/annoucement/PostOnAnnouncementPage"
 import AppBody from "../../components/share/app/AppBody"
 
 const index = () => {
     const post = [
-        { topic: "hello World", sender: "SAMO-SIT", status: false,id:10 },
-        { topic: "SIT Esport", sender: "SAMO-SIT", status: false , id:11},
-        { topic: "SIT Valentine", sender: "SAMO-SIT", status: false ,id:12},
-        { topic: "SIT Volunteer", sender: "SAMO-SIT", status: false,id:13 },
+        { topic: "hello World", sender: "SAMO-SIT", status: false, id: 10 },
+        { topic: "SIT Esport", sender: "SAMO-SIT", status: false, id: 11 },
+        { topic: "SIT Valentine", sender: "SAMO-SIT", status: false, id: 12 },
+        { topic: "SIT Volunteer", sender: "SAMO-SIT", status: false, id: 13 },
     ]
-    const [allPost, setAllPost] = React.useState(post);
+    const [allPost, setAllPost] = React.useState(post)
+    const [selectPost, setSelectPost] = React.useState(Number)
+    const onSelectPost = (postId: number) => {
+        setSelectPost(postId)
+        // console.log(postId);
+    }
+    // const detail = () => {
+    //     return (
+    //         <Link to="/announcement/detail/:postid">
+    //             <DetailPost selectPost={selectPost} posts={allPost} />
+    //         </Link>
+    //     )
+    // }
     return (
         <AppBody>
             <HeaderPage head="Announcement" Icon={IoIosAddCircle} />
@@ -20,14 +33,34 @@ const index = () => {
                     return p.status == true
                 })
                 .map((el) => {
-                    return <PostOnAnnouncementPage topic={el.topic} sender={el.sender} status={el.status} allPost={allPost} setAllPost={setAllPost} id={el.id}/>
+                    return (
+                        <PostOnAnnouncementPage
+                            topic={el.topic}
+                            sender={el.sender}
+                            status={el.status}
+                            allPost={allPost}
+                            setAllPost={setAllPost}
+                            id={el.id}
+                            onSelectPost={onSelectPost}
+                        />
+                    )
                 })}
             {allPost
                 .filter((p) => {
                     return p.status == false
                 })
                 .map((el) => {
-                    return <PostOnAnnouncementPage topic={el.topic} sender={el.sender} status={el.status} allPost={allPost} setAllPost={setAllPost} id={el.id}/>
+                    return (
+                        <PostOnAnnouncementPage
+                            topic={el.topic}
+                            sender={el.sender}
+                            status={el.status}
+                            allPost={allPost}
+                            setAllPost={setAllPost}
+                            id={el.id}
+                            onSelectPost={onSelectPost}
+                        />
+                    )
                 })}
         </AppBody>
     )
