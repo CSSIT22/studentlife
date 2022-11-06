96302f400e370c1c642c2c6bcad54caa7053ebd2
Ryyyyyy12 <kevalin.sris@kmutt.ac.th>
2022-11-06 11:38:05 +0700

    Revert "Merge branch 'notification' of https://github.com/CSSIT22/studentlife into notification"
    This reverts commit 28905adcb6fbf7b096cb33d4e6b66135e75ce537, reversing
    changes made to 93477a894b85685affd97a280ffeafa86bf27f5c.

--- frontend/src/components/notification/Modulelist.tsx
+++ frontend/src/components/notification/Modulelist.tsx
@@ -1,26 +1,8 @@
 import { Button } from "@chakra-ui/react"
 import React from "react"
-import { Menu, MenuButton, MenuList, MenuItem, Stack } from "@chakra-ui/react"
+
 const Modulelist = () => {
-    return (
-        <Menu>
-            <MenuButton as={Button}>module: All</MenuButton>
-            <MenuList>
-                <Stack direction={"column"} padding={4} height="75%" overflow="auto">
-                    <MenuItem>All</MenuItem>
-                    <MenuItem>Shop</MenuItem>
-                    <MenuItem>Schedule</MenuItem>
-                    <MenuItem>Announcement</MenuItem>
-                    <MenuItem>Chat</MenuItem>
-                    <MenuItem>Dating</MenuItem>
-                    <MenuItem>To Do</MenuItem>
-                    <MenuItem>Page Community</MenuItem>
-                    <MenuItem>Transaction</MenuItem>
-                </Stack>
-            </MenuList>
-            
-        </Menu>
-    )
+    return <Button>module: All</Button>
 }
 
 export default Modulelist
