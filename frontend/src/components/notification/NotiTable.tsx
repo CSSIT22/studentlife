89f9b7390773c1abc6e5efa4e958dca3316e511a
Lilyliiin <98150547+Lilyliiin@users.noreply.github.com>
2022-11-06 20:01:58 +0700

    Update NotiTable

--- frontend/src/components/notification/NotiTable.tsx
+++ frontend/src/components/notification/NotiTable.tsx
@@ -1,10 +1,20 @@
-import { Box, Button, Center, Flex, Spacer, Stack, Text } from "@chakra-ui/react"
+import { Box, Button, Center, CloseButton, Flex, Spacer, Stack, Text, useDisclosure } from "@chakra-ui/react"
 import React from "react"
 import Modulelist from "./Modulelist"
 import NotiList from "./NotiList"
 import { FiSettings } from "react-icons/fi"
 import MarkRead from "./MarkRead"
 import { Link } from "react-router-dom"
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+  } from '@chakra-ui/react'
+import NotiSetting from "./NotiSetting"
 
 const NotiTable = () => {
     return (
@@ -18,9 +28,9 @@ const NotiTable = () => {
                 <Box>
                     <Stack direction={"row"}>
                         <MarkRead />
-                        <Button size={"1em"} bg={"transparent"}>
-                            <FiSettings size={"1.2em"} />
-                        </Button>
+                            <Button size={"1em"} bg={"transparent"}>
+                                <ShowSetting />
+                            </Button>
                     </Stack>
                 </Box>
             </Flex>
@@ -37,5 +47,41 @@ const NotiTable = () => {
         </Box>
     )
 }
+
+function ShowSetting() {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+  
+    return (
+      <>
+        <Button size={"1em"} onClick={onOpen}><FiSettings size={"1.2em"}/></Button>
+            <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose} isCentered>
+                
+                <ModalOverlay>
+                
+                    <Box 
+                        borderRadius="lg"
+                        borderWidth="1px"
+                        borderColor="black"
+                        backgroundColor="white"
+                        width={{ base: "60%", md: "30%" }}
+                        height={{ base: "45vh"}}
+                        padding={4}
+                        margin ="auto"
+                        minH ='md'
+                    >
+                        <Stack align="end">
+                            <CloseButton onClick={onClose}/>
+                        </Stack>
+                        <Stack>
+                            <NotiSetting/>
+                        </Stack>
+                    </Box>
+                    
+                </ModalOverlay>
+                    
+            </Modal>
+      </>
+    )
+  }
 
 export default NotiTable
