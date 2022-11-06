55587520e67e827e977035db00684d958843d357
Khush Agarwal <khush.agar@kmutt.ac.th>
2022-11-06 16:51:06 +0700

    Adding Product Display

--- frontend/src/pages/shop/recommendation/index.tsx
+++ frontend/src/pages/shop/recommendation/index.tsx
@@ -1,5 +1,6 @@
 import { Breadcrumb, Container, Divider, Flex, Grid, GridItem, Heading } from "@chakra-ui/react"
 import React from "react"
+import ProductDisplay from "../../../components/shop/ProductDisplay"
 import ShopAppBody from "../../../components/shop/ShopAppBody"
 const Recommendation = () => {
     return (
