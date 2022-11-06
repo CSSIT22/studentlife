0cb08677e6531c209b27cba2cf62c90ebfcb957f
Khush Agarwal <khush.agar@kmutt.ac.th>
2022-11-06 15:36:47 +0700

    Adding Pages and ShopAppBody Component

---
+++ frontend/src/pages/shop/recommendation/index.tsx
@@ -0,0 +1,12 @@
+import { Breadcrumb, Container, Divider, Flex, Grid, GridItem, Heading } from "@chakra-ui/react"
+import React from "react"
+import ShopAppBody from "../../../components/shop/ShopAppBody"
+const Recommendation = () => {
+    return (
+        <ShopAppBody>
+            <Heading>Recommendation</Heading>
+        </ShopAppBody>
+    )
+}
+
+export default Recommendation
