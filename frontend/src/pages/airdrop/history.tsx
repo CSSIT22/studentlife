960872065aa84f3c40efb0f2fe2d434eb5682696
phawitpp <phawit.monc@kmutt.ac.th>
2022-11-07 12:14:34 +0700

    [ADD] button for receive & history page

--- frontend/src/pages/airdrop/history.tsx
+++ frontend/src/pages/airdrop/history.tsx
@@ -2,8 +2,8 @@ import React, { useState } from "react"
 import AppBody from "../../components/share/app/AppBody"
 import PageBox from "../../components/airdrop/pageBox"
 import { HiUpload, HiDownload } from "react-icons/hi"
-import { MdOutlineHistory, MdImage } from "react-icons/md"
-import { Container, Flex, Box, Text, Divider, Hide } from "@chakra-ui/react"
+import { MdOutlineHistory, MdImage, MdDone, MdOutlineClose, MdInfoOutline } from "react-icons/md"
+import { Container, Flex, Box, Text, Divider, Hide, HStack, IconButton } from "@chakra-ui/react"
 const linkMenu = [
     { name: "Drop", icon: HiUpload, to: "/airdrop" },
     { name: "Receive", icon: HiDownload, to: "/airdrop/receive" },
@@ -34,7 +34,17 @@ export default function Drophistory() {
                     </Hide>
                     <Box as={dummyData[0].type} size={"2rem"} />
                     <Text color={"gray.400"}>{dummyData[0].date}</Text>
-                    <Box> info</Box>
+                    <HStack>
+                        <IconButton
+                            aria-label="infomation"
+                            icon={<MdInfoOutline />}
+                            rounded={"3xl"}
+                            border={"1px"}
+                            borderColor={"gray.300"}
+                            shadow={"xs"}
+                            bgColor={"white"}
+                        ></IconButton>
+                    </HStack>
                 </Flex>
                 <Divider orientation="horizontal" />
                 <Flex direction={"row"} justifyContent={"space-around"} alignItems={"center"} py={"3"}>
@@ -44,7 +54,17 @@ export default function Drophistory() {
                     </Hide>
                     <Box as={HiUpload} size={"2rem"} />
                     <Text color={"gray.400"}>{dummyData[0].date}</Text>
-                    <Box> info</Box>
+                    <HStack>
+                        <IconButton
+                            aria-label="infomation"
+                            icon={<MdInfoOutline />}
+                            rounded={"3xl"}
+                            border={"1px"}
+                            borderColor={"gray.300"}
+                            shadow={"xs"}
+                            bgColor={"white"}
+                        ></IconButton>
+                    </HStack>
                 </Flex>
                 <Divider orientation="horizontal" />
                 <Flex direction={"row"} justifyContent={"space-around"} alignItems={"center"} py={"3"}>
@@ -54,7 +74,17 @@ export default function Drophistory() {
                     </Hide>
                     <Box as={dummyData[0].type} size={"2rem"} />
                     <Text color={"gray.400"}>{dummyData[0].date}</Text>
-                    <Box> info</Box>
+                    <HStack>
+                        <IconButton
+                            aria-label="infomation"
+                            icon={<MdInfoOutline />}
+                            rounded={"3xl"}
+                            border={"1px"}
+                            borderColor={"gray.300"}
+                            shadow={"xs"}
+                            bgColor={"white"}
+                        ></IconButton>
+                    </HStack>
                 </Flex>
 
                 <Divider orientation="horizontal" />
--- frontend/src/pages/airdrop/receive.tsx
+++ frontend/src/pages/airdrop/receive.tsx
@@ -2,8 +2,8 @@ import React from "react"
 import AppBody from "../../components/share/app/AppBody"
 import PageBox from "../../components/airdrop/pageBox"
 import { HiUpload, HiDownload } from "react-icons/hi"
-import { MdOutlineHistory, MdImage, MdDone, MdOutlineClose } from "react-icons/md"
-import { Container, Flex, HStack, Icon, Text, VStack, Box, Divider, Hide } from "@chakra-ui/react"
+import { MdOutlineHistory, MdImage, MdDone, MdOutlineClose, MdInfoOutline } from "react-icons/md"
+import { Container, Flex, HStack, Icon, Text, VStack, Box, Divider, Hide, IconButton } from "@chakra-ui/react"
 const linkMenu = [
     { name: "Drop", icon: HiUpload, to: "/airdrop" },
     { name: "Receive", icon: HiDownload, to: "/airdrop/receive" },
@@ -35,24 +35,82 @@ export default function Receivedrop() {
                 </Box>
                 {/* component for list will coming sooner */}
                 <Divider />
-                <Flex direction={"row"} justifyContent={"space-around"} alignItems={"center"} py={"3"}>
+                <Flex direction={"row"} justifyContent={"space-around"} alignItems={"center"} py={"3"} gap={3}>
                     <Box as={dummyData[0].icon} size={"3rem"} />
                     <Hide below={"md"}>
                         <Text>{dummyData[0].name}</Text>
                     </Hide>
 
-                    <Text>{dummyData[0].sender}</Text>
-                    <Box> Button Group</Box>
+                    <Text fontSize={["0.76rem","md"]}>{dummyData[0].name}</Text>
+
+                    <HStack>
+                        <IconButton
+                            aria-label="accept"
+                            icon={<MdDone />}
+                            rounded={"3xl"}
+                            border={"1px"}
+                            borderColor={"gray.300"}
+                            shadow={"xs"}
+                            bgColor={"white"}
+                        ></IconButton>
+                        <IconButton
+                            aria-label="deny"
+                            icon={<MdOutlineClose />}
+                            rounded={"3xl"}
+                            border={"1px"}
+                            borderColor={"gray.300"}
+                            shadow={"xs"}
+                            bgColor={"white"}
+                        ></IconButton>
+                        <IconButton
+                            aria-label="infomation"
+                            icon={<MdInfoOutline />}
+                            rounded={"3xl"}
+                            border={"1px"}
+                            borderColor={"gray.300"}
+                            shadow={"xs"}
+                            bgColor={"white"}
+                        ></IconButton>
+                    </HStack>
                 </Flex>
                 <Divider />
-                <Flex direction={"row"} justifyContent={"space-around"} alignItems={"center"} py={"3"}>
+                <Flex direction={"row"} justifyContent={"space-around"} alignItems={"center"} py={"3"} w={"100%"}>
                     <Box as={dummyData[0].icon} size={"3rem"} />
                     <Hide below={"md"}>
                         <Text>{dummyData[0].name}</Text>
                     </Hide>
 
-                    <Text>{dummyData[0].sender}</Text>
-                    <Box> Button Group</Box>
+                    <Text fontSize={["0.76rem","md"]}>{dummyData[0].name}</Text>
+                    <HStack>
+                        <IconButton
+                            aria-label="accept"
+                            icon={<MdDone />}
+                            rounded={"3xl"}
+                            border={"1px"}
+                            borderColor={"gray.300"}
+                            shadow={"xs"}
+                            bgColor={"white"}
+                        ></IconButton>
+                        <IconButton
+                            aria-label="deny"
+                            icon={<MdOutlineClose />}
+                            rounded={"3xl"}
+                            border={"1px"}
+                            borderColor={"gray.300"}
+                            shadow={"xs"}
+                            bgColor={"white"}
+                        ></IconButton>
+                        <IconButton
+                            aria-label="infomation"
+                            icon={<MdInfoOutline />}
+                            rounded={"3xl"}
+                            border={"1px"}
+                            borderColor={"gray.300"}
+                            shadow={"xs"}
+                            bgColor={"white"}
+                            
+                        ></IconButton>
+                    </HStack>
                 </Flex>
                 <Divider />
             </PageBox>
