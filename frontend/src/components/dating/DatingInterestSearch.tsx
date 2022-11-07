235277afee4f7b0875e0517905caf7b050907b59
Pawin Nakvisai <pawinnak2@gmail.com>
2022-11-07 18:25:29 +0700

    Fixed the DatingInterestSearch

--- frontend/src/components/dating/DatingInterestSearch.tsx
+++ frontend/src/components/dating/DatingInterestSearch.tsx
@@ -1,22 +1,45 @@
-import { Input } from "@chakra-ui/react"
-import React, { Dispatch, FC, SetStateAction } from "react"
+import { Box, Input, Text } from "@chakra-ui/react"
+import React, { Component, Dispatch, FC, SetStateAction, useCallback, useEffect, useRef, useState } from "react"
 
-const DatingInterestSearch:FC<{setSearchQuery: Dispatch<SetStateAction<string>>; searchQuery: string}> = ({setSearchQuery, searchQuery}) => {
-    // Check if user has press enter when currently in the search bar
-    function handleSearchEnter(event: React.KeyboardEvent<HTMLInputElement>) {    
-        if (event.key === "Enter" && searchQuery != "") {
-            alert("Query: " + searchQuery)
-        }
-        return false
+const DatingInterestSearch: FC<{ setSearchQuery: Dispatch<SetStateAction<string>>; searchQuery: string }> = ({ setSearchQuery, searchQuery }) => {
+    const [timer, setTimer] = useState<number | null>(null)
+    const didMount = useDidMount()
+
+    useEffect(() => {
+        if (didMount) alert(searchQuery)
+      }, [searchQuery])
+
+    function useDidMount() {
+        const [didMount, setDidMount] = useState(false)
+        useEffect(() => { setDidMount(true) }, [])
+      
+        return didMount
     }
 
-    function handleSearchType(event: React.ChangeEvent<HTMLInputElement>) {
-        let searchQueryInBox = (document.getElementById("search") as HTMLInputElement).value
-        setSearchQuery(searchQueryInBox)
+
+    // Check if user has press enter when currently in the search bar
+    function handleSearchChange(event: React.ChangeEvent<HTMLInputElement>) {
+        if (timer) {
+            clearTimeout(timer)
+            setTimer(null)
+        }
+        setTimer(
+            setTimeout(() => {
+                setSearchQuery(event.target.value)
+            }, 1000)
+        )
     }
 
     return (
-        <Input type="search" placeholder="🔍  Search" size="md" borderRadius="full" id="search" name="search" onChange={(e) => handleSearchType(e)} onKeyPress={(e) => handleSearchEnter(e)} />
+        <Input
+            type="search"
+            placeholder="🔍  Search"
+            size="md"
+            borderRadius="full"
+            id="search"
+            name="search"
+            onChange={(e) => handleSearchChange(e)}
+        />
     )
 }
 
