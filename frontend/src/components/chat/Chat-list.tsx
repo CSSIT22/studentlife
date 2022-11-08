e6e1a6171a8faa48fc0e1982cff1249d9d19c09f
Neng Sitiporn <sitiporn.wimo@kmutt.ac.th>
2022-11-09 01:23:39 +0700

    add desktop chat-list

--- frontend/src/components/chat/Chat-list.tsx
+++ frontend/src/components/chat/Chat-list.tsx
@@ -1,23 +1,115 @@
-import { Box, Button, Flex } from "@chakra-ui/react"
-import { FC } from "react"
+import { Box, Button, Flex, Img } from "@chakra-ui/react"
+import { Avatar} from "@chakra-ui/react"
+import { useNavigate } from "react-router-dom"
+import { FC, useState } from "react"
+
+type room = { roomID: String; roomName: String; roomtype: "individual" | "group", img :String }[]
+
+
+const mockRoom: room = [
+    { roomID: "1324", roomName: "Neng", roomtype: "individual",img :"https://s.thistine.com/dog" },
+    { roomID: "1123", roomName: "Oil", roomtype: "individual" ,img :"https://s.thistine.com/dog"},
+    { roomID: "3333", roomName: "Gift", roomtype: "individual" , img :"https://s.thistine.com/dog"},
+    { roomID: "4444", roomName: "Tine", roomtype: "individual" , img :"https://s.thistine.com/dog"},
+    { roomID: "5555", roomName: "4Young", roomtype: "group",img:"https://s.thistine.com/rodo" },
+    { roomID: "6666", roomName: "Toddy", roomtype: "individual" , img:"https://s.thistine.com/dog"},
+    { roomID: "7777", roomName: "Kevin", roomtype: "individual" , img:"https://s.thistine.com/dog" },
+    { roomID: "8888", roomName: "Parn", roomtype: "individual" ,img:"https://s.thistine.com/dog"},
+    { roomID: "9999", roomName: "Almas", roomtype: "individual",img:"https://s.thistine.com/dog" },
+]
+
 
 const Clist: FC<any> = () => {
+    const [userRoom,setuserRoom] = useState<room>(mockRoom)
+    const [target, setTarget] = useState(1);
+    const navigate = useNavigate();
+    //function handle
+    function Navigate(target : any){
+        return navigate(`/chat/${target}`);
+    }
+
+
+
+    const renderButton = () => {
+        if (mockRoom.length != 0) {
+            return (
+                <Flex fontSize={"2xl"} marginBottom={5} alignSelf="center">
+                    <Button
+                        marginRight={2}
+                        bg={"transparent"}
+                        _active={{ background: "transparent" }}
+                        _hover={{ background: "transparent", fontSize: "2xl" }}
+                        fontSize={target === 1 ? "2xl" : "lg"}
+                        textDecoration={target === 1 ? "underline" : ""}
+                        onClick={() => setTarget(1)}
+                    >
+                        Chat
+                    </Button>
+                    <Button
+                        marginRight={2}
+                        bg={"transparent"}
+                        _active={{ background: "transparent" }}
+                        _hover={{ background: "transparent", fontSize: "2xl" }}
+                        fontSize={target === 2 ? "2xl" : "lg"}
+                        textDecoration={target === 2 ? "underline" : ""}
+                        onClick={() => setTarget(2)}
+                    >
+                        Group
+                    </Button>
+                </Flex>
+            )
+        } else {
+            return <Flex />
+        }
+    }
+
+    
+
+    const renderRoom = (e: any) => {
+        if (target === 1 && e.roomtype === "individual") {
+            return (
+                <Flex
+                    justify={""}
+                    alignItems={"center"}
+                    key={e.roomID}
+                    marginY={3}
+                    _hover={{
+                        transform: "scale(1.5)",
+                    }}
+                    transitionDuration="300ms"
+                    onClick={()=>Navigate(e.roomID)}
+                >
+                    <Avatar name={e.Roomname} src={e.img} />
+                    <Box marginLeft={"5"}>{e.roomName} </Box>
+                </Flex>
+            )
+        }
+        if (target === 2 && e.roomtype === "group") {
+            return (
+                <Flex
+                    justify={""}
+                    alignItems={"center"}
+                    key={e.roomID}
+                    marginY={2}
+                    _hover={{
+                        transform: "scale(1.5)",
+                    }}
+                    transitionDuration="300ms"
+                    
+                >
+                    <Avatar name={e.Roomname} src={e.img} />
+                    <Box marginLeft={2}>{e.roomName}</Box>
+                </Flex>
+            )
+        }
+    }
     return (
-        <Box minH={"80vh"} background="orange.200kk">
-            <Flex
-                width={"30%"}
-                background="orange.200"
-                justify={"space-around"}
-                p={5}
-                rounded={"lg"}
-                fontWeight={"bold"}
-                color={"white"}
-                alignItems="center"
-            >
-                <Box  textDecoration={"underline"} fontSize={"xl"}>
-                    Chat
-                </Box>
-                <Box>Group</Box>
+        <Box minH={{ base: "80vh", md: "700px" }} background="orange.200kk" width={{ base: "100%", md: "300px" }} bg={"orange.200"} rounded={"2xl"}>
+            <Flex width={"100%"} height={"20%"} p={5} rounded={"lg"} fontWeight={"bold"} color={"white"} direction={"column"}>
+                {renderButton()}
+                {userRoom.map((e: any) => {
+                    return renderRoom(e);
+                })}
             </Flex>
         </Box>
     )
--- frontend/src/pages/chat/[roomID].tsx
+++ frontend/src/pages/chat/[roomID].tsx
@@ -4,12 +4,26 @@ import { useParams } from "react-router-dom"
 import Clist from "../../components/chat/Chat-list"
 import AppBody from "../../components/share/app/AppBody"
 
+type room = { roomID: String; roomName: String; roomtype: "individual" | "group", img :String }[]
+
+
+const mockRoom: room = [
+    { roomID: "1324", roomName: "Neng", roomtype: "individual",img :"https://s.thistine.com/dog" },
+    { roomID: "1123", roomName: "Oil", roomtype: "individual" ,img :"https://s.thistine.com/dog"},
+    { roomID: "3333", roomName: "Gift", roomtype: "individual" , img :"https://s.thistine.com/dog"},
+    { roomID: "4444", roomName: "Tine", roomtype: "individual" , img :"https://s.thistine.com/dog"},
+    { roomID: "5555", roomName: "4Young", roomtype: "group",img:"https://s.thistine.com/rodo" },
+    { roomID: "6666", roomName: "Toddy", roomtype: "individual" , img:"https://s.thistine.com/dog"},
+    { roomID: "7777", roomName: "Kevin", roomtype: "individual" , img:"https://s.thistine.com/dog" },
+    { roomID: "8888", roomName: "Parn", roomtype: "individual" ,img:"https://s.thistine.com/dog"},
+    { roomID: "9999", roomName: "Almas", roomtype: "individual",img:"https://s.thistine.com/dog" },
+]
+
 const Room = () => {
     let param = useParams()
-    const [setTarget, Target] = useState("chat")
     return (
         <AppBody>
-            <Clist target={Target} />
+            <Clist room ={mockRoom} />
         </AppBody>
     )
 }
