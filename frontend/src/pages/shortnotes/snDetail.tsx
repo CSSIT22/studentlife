ef9754d0407fcf9df028ffea6784a560db311606
Pasinun.w <pasinun.w@gmail.com>
2022-11-08 16:19:42 +0700

    [FIX] comment list  arrange

--- frontend/src/pages/shortnotes/snDetail.tsx
+++ frontend/src/pages/shortnotes/snDetail.tsx
@@ -49,13 +49,14 @@ import {
 import AppBody from "../../components/share/app/AppBody"
 import { ChevronDownIcon, HamburgerIcon } from "@chakra-ui/icons"
 import { HiDotsHorizontal } from "react-icons/hi"
+import CmList from "../../components/shortnotes/cmList"
 
 const snDetail = () => {
     return (
         <AppBody>
-            <Grid templateColumns="repeat(10, 1fr)" bg={"white"} boxShadow={"xl"} rounded={8}>
+            <Grid templateColumns="repeat(14, 1fr)" bg={"white"} boxShadow={"xl"} rounded={8} mb={4}>
                 <Spacer />
-                <GridItem colSpan={8} p={4}>
+                <GridItem colSpan={12} py={6}>
                     <HStack>
                         <Heading size={"lg"} mb={1}>
                             Shortnote name
@@ -64,46 +65,61 @@ const snDetail = () => {
                         <Menu>
                             <MenuButton as={IconButton} aria-label="Options" icon={<HiDotsHorizontal />} variant="ghost" />
                             <MenuList>
-                                <MenuItem command="">Add to library</MenuItem>
-                                <MenuItem command="">Upload file</MenuItem>
-                                <MenuItem command="">Delete</MenuItem>
+                                <MenuItem>Add to library</MenuItem>
+                                <MenuItem>Upload file</MenuItem>
+                                <MenuItem>Delete</MenuItem>
                             </MenuList>
                         </Menu>
                     </HStack>
 
-                    <Text bg={"orange"} rounded={8} w={20} textAlign={"center"} mb={4}>
+                    <Heading size={"sm"} bg={"orange"} rounded={8} w={20} py={1} textAlign={"center"} mb={4} boxShadow={"xl"}>
                         CSC213
-                    </Text>
+                    </Heading>
                     <Box mb={4}>
                         <Heading size={"md"}>Description</Heading>
                         <Text>
                             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis ut cumque consequuntur vel, inventore hic suscipit
-                            perspiciatis at non! Sequi autem earum odio rem ipsum voluptatibus officia molestiae pariatur culpa!{" "}
+                            perspiciatis at non! Sequi autem earum odio rem ipsum voluptatibus officia molestiae pariatur culpa!Lorem ipsum dolor sit,
+                            amet consectetur adipisicing elit. Debitis ut cumque consequuntur vel, inventore hic suscipit perspiciatis at non! Sequi
+                            autem earum odio rem ipsum voluptatibus officia molestiae pariatur culpa!{" "}
                         </Text>
                     </Box>
-
-                    <Box mb={4}>
+                    <Box>
                         <Heading size={"md"}>Link</Heading>
                         <Text>modlink.me/shortnote/filename.pdf</Text>
                     </Box>
+                </GridItem>
+            </Grid>
+            <Grid templateColumns="repeat(14, 1fr)" bg={"white"} boxShadow={"xl"} rounded={8}>
+                <Spacer />
+                <GridItem colSpan={12} py={6}>
+                    <Heading size={"md"} mb={1}>
+                        Comments
+                    </Heading>
 
-                    <Box>
-                        <Textarea h={150} mb={2} />
-                        <Flex direction={"row"} justifyContent={"end"}>
-                            <Button colorScheme={"orange"}>Comment</Button>
-                        </Flex>
-                    </Box>
+                    <Textarea h={150} mb={2} py={4} placeholder={"What are your thought..."} />
+                    <Flex direction={"row"} justifyContent={"end"}>
+                        <Button colorScheme={"orange"}>Comment</Button>
+                    </Flex>
                     <VStack gap={4}>
-                        <Box bg={"white"} boxShadow={"xl"} rounded={8} p={4}>
-                            <Heading size={"sm"}>Firstname lastname</Heading>
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente ab at quibusdam dicta nulla ipsum in sequi, enim modi?
-                            Non, id! Quae illo iste id cum quia aut doloribus eligendi?
-                        </Box>
-                        <Box bg={"white"} boxShadow={"xl"} rounded={8} p={4}>
-                            <Heading size={"sm"}>Firstname lastname</Heading>
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente ab at quibusdam dicta nulla ipsum in sequi, enim modi?
-                            Non, id! Quae illo iste id cum quia aut doloribus eligendi?
-                        </Box>
+                        <CmList
+                            name={"firstname lastname"}
+                            desc={
+                                "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente ab at quibusdam dicta nulla ipsum in sequi, enim modiNon, id! Quae illo iste id cum quia aut doloribus eligendi"
+                            }
+                        />
+                        <CmList
+                            name={"firstname lastname"}
+                            desc={
+                                "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente ab at quibusdam dicta nulla ipsum in sequi, enim modiNon, id! Quae illo iste id cum quia aut doloribus eligendi"
+                            }
+                        />
+                        <CmList
+                            name={"firstname lastname"}
+                            desc={
+                                "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente ab at quibusdam dicta nulla ipsum in sequi, enim modiNon, id! Quae illo iste id cum quia aut doloribus eligendi"
+                            }
+                        />
                     </VStack>
                 </GridItem>
             </Grid>
