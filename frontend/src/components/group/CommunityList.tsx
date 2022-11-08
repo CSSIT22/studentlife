547f9f49be1e9376e48d7e5940727440de4824df
Passakorn <boss4848988@gmail.com>
2022-11-09 01:12:17 +0700

    [FIX] params name

--- frontend/src/components/group/CommunityList.tsx
+++ frontend/src/components/group/CommunityList.tsx
@@ -2,20 +2,25 @@ import { HStack, Box, Image, Text } from "@chakra-ui/react"
 import React, { FC } from "react"
 import { MdPublic, MdPublicOff } from "react-icons/md"
 
-const CommunityList: FC<{ communityName: string; active: string; image: string; privacy: boolean }> = ({ communityName, active, image, privacy }) => {
+const CommunityList: FC<{ communityName: string; lastActive: string; coverPhoto: string; isPrivate: boolean }> = ({
+    communityName,
+    lastActive,
+    coverPhoto,
+    isPrivate,
+}) => {
     return (
-        <Box _hover={{ backgroundColor: "gray.400", cursor: "pointer" }} borderRadius="md" backgroundColor="gray.300" mt={2}>
+        <Box _hover={{ backgroundColor: "gray.200", cursor: "pointer" }} borderRadius="md" backgroundColor="gray.300" mt={2}>
             <Box p={2} borderRadius="md">
                 <HStack gap={2}>
-                    <Image ml={1} borderRadius="md" boxSize="55px" src={image} alt="Cover Photo" />
+                    <Image ml={1} borderRadius="md" boxSize="55px" src={coverPhoto} alt="Cover Photo" />
                     <div>
                         <Box display="flex" alignItems="center" gap={1}>
-                            {privacy ? <MdPublic /> : <MdPublicOff />}
+                            {isPrivate ? <MdPublicOff /> : <MdPublic />}
                             <Text as="b" fontSize="sm">
                                 {communityName}
                             </Text>
                         </Box>
-                        <Text fontSize="sm">Last active {active} days ago</Text>
+                        <Text fontSize="sm">Last active {lastActive} days ago</Text>
                     </div>
                 </HStack>
             </Box>
