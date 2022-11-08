e00f4b22e5a82f077bfcc7d14cf8f6789d7316d4
Passakorn <boss4848988@gmail.com>
2022-11-08 17:15:25 +0700

    [UPDATE] privacy icon to a list component

--- frontend/src/components/group/CommunityList.tsx
+++ frontend/src/components/group/CommunityList.tsx
@@ -1,16 +1,20 @@
 import { HStack, Box, Image, Text } from "@chakra-ui/react"
 import React, { FC } from "react"
+import { MdPublic, MdPublicOff } from "react-icons/md"
 
-const CommunityList: FC<{ communityName: string; active: string; image: string }> = ({ communityName, active, image }) => {
+const CommunityList: FC<{ communityName: string; active: string; image: string; privacy: boolean }> = ({ communityName, active, image, privacy }) => {
     return (
-        <Box borderRadius="md" backgroundColor="gray.300" mt={2}>
+        <Box _hover={{ backgroundColor: "gray.400", cursor: "pointer" }} borderRadius="md" backgroundColor="gray.300" mt={2}>
             <Box p={2} borderRadius="md">
                 <HStack gap={2}>
                     <Image ml={1} borderRadius="md" boxSize="55px" src={image} alt="Cover Photo" />
                     <div>
-                        <Text as="b" fontSize="sm">
-                            {communityName}
-                        </Text>
+                        <Box display="flex" alignItems="center" gap={1}>
+                            {privacy ? <MdPublic /> : <MdPublicOff />}
+                            <Text as="b" fontSize="sm">
+                                {communityName}
+                            </Text>
+                        </Box>
                         <Text fontSize="sm">Last active {active} days ago</Text>
                     </div>
                 </HStack>
