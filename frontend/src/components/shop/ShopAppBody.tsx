c9c01891320c615d86a0985df7b4910c93b4346c
Khush Agarwal <khush.agar@kmutt.ac.th>
2022-11-08 17:11:12 +0700

    Made changes to the navigation bar

--- frontend/src/components/shop/ShopAppBody.tsx
+++ frontend/src/components/shop/ShopAppBody.tsx
@@ -1,26 +1,27 @@
 import React, { Component } from "react"
 import AppBody from "../share/app/AppBody"
+import { FiShoppingCart } from "react-icons/fi"
 
 const ShopAppBody = (props: any) => {
     return <AppBody secondarynav={[
         {
             name: "Explore",
             to: "/shop"
-        }
-        // Need to add these without the problem of navbar alignment
-        //,
-        // {
-        //     name: "Categories",
-        //     to: "/shop/categories"
-        // },
+        },
+        {
+            name: "Categories",
+            to: "/shop/categories"
+        },
         // {
         //     name: "Recommended Items",
         //     to: "/shop/recommendation"
         // },
-        // {
-        //     name: "Cart",
-        //     to: "/shop/cart"
-        // }
+        {
+            name: "",
+            to: "/shop/cart",
+            Icon: FiShoppingCart,
+            isRight: true
+        }
     ]
     }> 
         {props.children}
