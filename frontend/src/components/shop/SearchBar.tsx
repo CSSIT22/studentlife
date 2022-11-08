878a2fbacf97966ada06f21960ac7c4333674f5b
Khush Agarwal <khush.agar@kmutt.ac.th>
2022-11-08 17:10:18 +0700

    Added Search Bar

---
+++ frontend/src/components/shop/SearchBar.tsx
@@ -0,0 +1,16 @@
+import { Center, Flex, Heading, Icon, Input, InputGroup, InputLeftElement, Select } from "@chakra-ui/react"
+import { MdPadding } from "react-icons/md"
+import { TbSearch } from "react-icons/tb"
+// Search Bar without Actions -> Dummy
+const Searchbar = () => {
+    return (
+        <div>
+            <InputGroup>
+                <InputLeftElement p-6 pointerEvents="none" children={<TbSearch />} />
+                <Input type="search" placeholder="Search" borderRadius="full" size={"md"} background="white"></Input>
+            </InputGroup>
+        </div>
+    )
+}
+
+export default Searchbar
