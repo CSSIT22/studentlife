b63a7ef6daf2a3da2bc4ea135eab1b14b543f770
Pawin Nakvisai <pawinnak2@gmail.com>
2022-11-08 13:10:30 +0700

    Change tutorial to 1 page

---
+++ frontend/src/pages/dating/tutorial.tsx
@@ -0,0 +1,7 @@
+import DatingAppBody from "../../components/dating/DatingAppBody"
+
+const DatingRandomization = () => {
+    return <DatingAppBody>Tutorial</DatingAppBody>
+}
+
+export default DatingRandomization
--- frontend/src/pages/dating/tutorial/match.tsx
+++
@@ -1,7 +0,0 @@
-import DatingAppBody from "../../../components/dating/DatingAppBody"
-
-const TutorialMatch = () => {
-    return <DatingAppBody>You Are Match With Tutorial</DatingAppBody>
-}
-
-export default TutorialMatch
