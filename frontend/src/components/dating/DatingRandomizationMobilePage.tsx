3b4d2533db18d19022c368abd33469c95e99e773
Pawin Nakvisai <pawinnak2@gmail.com>
2022-11-09 22:25:47 +0700

    Fix overflow tinder card, change style attrs

--- frontend/src/components/dating/DatingRandomizationMobilePage.tsx
+++ frontend/src/components/dating/DatingRandomizationMobilePage.tsx
@@ -1,28 +1,10 @@
 import TinderCard from "react-tinder-card"
-import { Box, Center, Heading, SimpleGrid, Tag, Text } from "@chakra-ui/react"
+import { Box, Center, SimpleGrid, Tag, Text } from "@chakra-ui/react"
 import DatingAppBody from "./DatingAppBody"
-
-const db = [
-    {
-        name: "Jennie Kim",
-        url: "https://lh3.googleusercontent.com/Km2FSe-U_oW1ZT1LK1u-p0nsH_QTucR7l_sUU2UvlgZpAjQaj2Oik4te9iFUVmqthLCd-6VLPtoDL9iAcs7mL7tcpSOd0tCMAtPUISBFtjMh0WI=w1600-rj-l80-nu-e365",
-    },
-    {
-        name: "Kim Jisoo",
-        url: "https://lh3.googleusercontent.com/MDVfO5G8cmGAKLcNXXdik1iTJe92ZcNoHbPTB4lbn0KutHvtWcBXyFEApa9x20HHg0NCrVQfxuU5u3mIsFYhM71aPJJ5XrXfOZOBRJFSYfAAM1U=w1600-rj-l80-nu-e365",
-    },
-    {
-        name: "Lalisa Manoban",
-        url: "https://lh3.googleusercontent.com/FQ0PlEhb6lDNLSAYiZEb_80MYlzn-ev3pEGJXC8TvBQlTcKdxAPsp3Lsnn9EZod88FHSMu6GFn2QAKnRsaRg-OBt-m_zNYow6M85Jrw1RC8FGzAD=w1600-rj-l80-nu-e365",
-    },
-    {
-        name: "Park Chaeyoung",
-        url: "https://lh3.googleusercontent.com/iIzDn013HQ8NR_KZhaXGeLld6jaszvmRbdrKMGoYuB5MZOZI7ZM6V58yVFHqIuOuJGALhKcKn8vwXTBLf-csPpwaHE5f7YEFIj_KS2SVMcVbQlc=w1600-rj-l80-nu-e365",
-    },
-]
+import { CARD_QUEUE } from "./shared/card_queue"
 
 const DatingRandomizationMobilePage = () => {
-    const characters = db
+    const characters = CARD_QUEUE
 
     const swiped = (direction: any, nameToDelete: any) => {
         console.log("Swiping " + nameToDelete + " to the " + direction)
@@ -34,7 +16,7 @@ const DatingRandomizationMobilePage = () => {
 
     return (
         <DatingAppBody>
-            <SimpleGrid>
+            <SimpleGrid overflow="hidden">
                 <Box>
                     <Box className="cardContainer">
                         {characters.map((character) => (
@@ -56,7 +38,8 @@ const DatingRandomizationMobilePage = () => {
                                         className="card"
                                         pl="1rem"
                                         id={character.name}
-                                        style={{ position: "absolute", top: "30px" }}
+                                        position="absolute"
+                                        top="30px"
                                         boxShadow="0px 10px 15px -3px rgba(0, 0, 0, 0.1), 0px 4px 6px -2px rgba(0, 0, 0, 0.05)"
                                     ></Box>
                                 </Center>
@@ -65,22 +48,14 @@ const DatingRandomizationMobilePage = () => {
                     </Box>
                 </Box>
                 <Box display="flex">
-                    <Text
-                        style={{ fontFamily: "Inter", fontStyle: "normal", fontWeight: "700", fontSize: "20px", lineHeight: "120%" }}
-                        pt="468px"
-                        pl="18px"
-                    >
+                    <Text fontFamily="Inter" fontStyle="normal" fontWeight="700" fontSize="20px" lineHeight="120%" pt="468px" pl="18px">
                         Firstname L.
                     </Text>
-                    <Text
-                        style={{ fontFamily: "Inter", fontStyle: "normal", fontWeight: "400", fontSize: "20px", lineHeight: "120%" }}
-                        pt="468px"
-                        pl="18px"
-                    >
+                    <Text fontFamily="Inter" fontStyle="normal" fontWeight="400" fontSize="20px" lineHeight="120%" pt="468px" pl="18px">
                         M , Age
                     </Text>
                 </Box>
-                <Box style={{ fontFamily: "Inter", fontStyle: "normal", fontWeight: "400", fontSize: "16px", lineHeight: "150%" }}>
+                <Box fontFamily="Inter" fontStyle="normal" fontWeight="400" fontSize="20px" lineHeight="120%">
                     <Text pl="18px" pt="10px">
                         Faculty
                     </Text>
---
+++ frontend/src/components/dating/shared/card_queue.ts
@@ -0,0 +1,18 @@
+export const CARD_QUEUE = [
+    {
+        name: "Jennie Kim",
+        url: "https://lh3.googleusercontent.com/Km2FSe-U_oW1ZT1LK1u-p0nsH_QTucR7l_sUU2UvlgZpAjQaj2Oik4te9iFUVmqthLCd-6VLPtoDL9iAcs7mL7tcpSOd0tCMAtPUISBFtjMh0WI=w1600-rj-l80-nu-e365",
+    },
+    {
+        name: "Kim Jisoo",
+        url: "https://lh3.googleusercontent.com/MDVfO5G8cmGAKLcNXXdik1iTJe92ZcNoHbPTB4lbn0KutHvtWcBXyFEApa9x20HHg0NCrVQfxuU5u3mIsFYhM71aPJJ5XrXfOZOBRJFSYfAAM1U=w1600-rj-l80-nu-e365",
+    },
+    {
+        name: "Lalisa Manoban",
+        url: "https://lh3.googleusercontent.com/FQ0PlEhb6lDNLSAYiZEb_80MYlzn-ev3pEGJXC8TvBQlTcKdxAPsp3Lsnn9EZod88FHSMu6GFn2QAKnRsaRg-OBt-m_zNYow6M85Jrw1RC8FGzAD=w1600-rj-l80-nu-e365",
+    },
+    {
+        name: "Park Chaeyoung",
+        url: "https://lh3.googleusercontent.com/iIzDn013HQ8NR_KZhaXGeLld6jaszvmRbdrKMGoYuB5MZOZI7ZM6V58yVFHqIuOuJGALhKcKn8vwXTBLf-csPpwaHE5f7YEFIj_KS2SVMcVbQlc=w1600-rj-l80-nu-e365",
+    },
+]
