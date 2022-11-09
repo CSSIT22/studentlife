d95f248230e81487449cda787ca27855cb942807
Ryyyyyy12 <kevalin.sris@kmutt.ac.th>
2022-11-09 16:09:36 +0700

    UPDATE scrollable moduleList

--- frontend/src/components/notification/Modulelist.tsx
+++ frontend/src/components/notification/Modulelist.tsx
@@ -1,4 +1,4 @@
-import { Button, Menu, MenuButton, MenuItem, MenuList, Stack } from "@chakra-ui/react"
+import { Box, Button, Menu, MenuButton, MenuItem, MenuList, Stack } from "@chakra-ui/react"
 import React from "react"
 
 const Modulelist = () => {
@@ -8,17 +8,19 @@ const Modulelist = () => {
                 All
             </MenuButton>
             <MenuList>
-                <Stack direction={"column"} padding={4} height="75%" overflow="auto">
-                    <MenuItem>All</MenuItem>
-                    <MenuItem>Shop</MenuItem>
-                    <MenuItem>Schedule</MenuItem>
-                    <MenuItem>Announcement</MenuItem>
-                    <MenuItem>Chat</MenuItem>
-                    <MenuItem>Dating</MenuItem>
-                    <MenuItem>To Do</MenuItem>
-                    <MenuItem>Page Community</MenuItem>
-                    <MenuItem>Transaction</MenuItem>
-                </Stack>
+                <Box height="30vh" overflow="auto">
+                    <Stack direction={"column"} padding={4}>
+                        <MenuItem>All</MenuItem>
+                        <MenuItem>Shop</MenuItem>
+                        <MenuItem>Schedule</MenuItem>
+                        <MenuItem>Announcement</MenuItem>
+                        <MenuItem>Chat</MenuItem>
+                        <MenuItem>Dating</MenuItem>
+                        <MenuItem>To Do</MenuItem>
+                        <MenuItem>Page Community</MenuItem>
+                        <MenuItem>Transaction</MenuItem>
+                    </Stack>
+                </Box>
             </MenuList>
         </Menu>
     )
