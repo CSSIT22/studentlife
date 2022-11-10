4185c7b77b74d1e92ba0d4bb33a7a1ec93f8e4b3
saranratR <fahhiha36@gmail.com>
2022-11-10 19:23:06 +0700

    add box

--- frontend/src/pages/schedule/timetable.tsx
+++ frontend/src/pages/schedule/timetable.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import AppBody from '../../components/share/app/AppBody'
+import React from "react"
+import AppBody from "../../components/share/app/AppBody"
+import { Box, SimpleGrid } from "@chakra-ui/react"
 
 const timetable = () => {
-  return (
-    <AppBody>timetable</AppBody>
-  )
+    return (
+        <AppBody>
+            timetable
+            <SimpleGrid>
+                <Box boxShadow="md" p="6" rounded="md" bg="white">
+                    Date
+                </Box>
+                <Box boxShadow="md" p="6" rounded="md" bg="white">
+                    Month
+                </Box>
+                <Box boxShadow="md" p="6" rounded="md" bg="white">
+                    Year
+                </Box>
+            </SimpleGrid>
+        </AppBody>
+    )
 }
 
 export default timetable
