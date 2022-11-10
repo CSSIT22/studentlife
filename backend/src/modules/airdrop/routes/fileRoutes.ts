70d2a2ab019aae89166b2716b08db5a3b865f004
phawitpp <phawit.monc@kmutt.ac.th>
2022-11-10 23:52:41 +0700

    [ADD] store file in DB (unsolve issues)

--- backend/src/modules/airdrop/routes/fileRoutes.ts
+++ backend/src/modules/airdrop/routes/fileRoutes.ts
@@ -1,31 +1,60 @@
-import express,{Request,Response} from "express"
-const path = require("path");
-const fs = require("fs");
-const multer = require("multer");
+import express, { Request, Response } from "express"
+import { PrismaClient } from "@prisma/client"
 
+//file manage module
+const path = require("path")
+const fs = require("fs")
+const multer = require("multer")
+
+//prisma
+const prisma = new PrismaClient()
 //config destionation here
 const storage = multer.diskStorage({
-    destination: function (req:any, file:any, cb:any) {
-     const fileType = req.body.type;
-     
-      cb(null, path.join(__dirname,"../files"+"/"+fileType));
+    destination: function (req: any, file: any, cb: any) {
+        const fileType = req.body.type
+
+        cb(null, path.join(__dirname, "../files" + "/" + fileType))
     },
-    filename: function (req:any, file:any, cb:any) {
-      cb(null, file.originalname) 
-    }
-  })
-const upload = multer({storage:storage});
+    filename: function (req: any, file: any, cb: any) {
+        cb(null, file.originalname)
+    },
+})
+const upload = multer({ storage: storage })
 
 const fileRoutes = express()
 
-
-fileRoutes.post("/upload",upload.array('files'),(req:Request,res:Response)=>{
-    console.log("Upload sucessful");
-    console.log(req.body);
-});
-fileRoutes.get("/download/:type/:id",(req:Request,res:Response)=>{
-  const directoryPath = path.join(__dirname, '../files'+"/"+req.params.type);
-  res.download(directoryPath+"/"+req.params.id);
-
+fileRoutes.get("/", async (req: Request, res: Response)=>{
+    console.log(req.user?.userId)
 })
-export default fileRoutes;
+
+fileRoutes.post("/upload", upload.array("files"), async (req: Request, res: Response) => {
+    console.log("Upload sucessful")
+    console.log(req.body)
+    const sender = req.user?.userId;
+    console.log(sender);
+    try {
+        const payload: any = []
+        //mapping files
+        ;(req.files as Array<Express.Multer.File>).map((item: any) => {
+            const newDate = new Date(req.body.expireDate);
+            payload.push({
+                fileName: item.originalname,
+                fileSender: sender,
+                sendType: req.body.type,
+                fileDesc: req.body.description,
+                fileExpired: req.body.expireDate,
+            })
+        })
+
+        const fileUpload = await prisma.file_Info.createMany({
+            data: payload,
+        })
+    } catch (err) {
+        console.log(err)
+    }
+})
+fileRoutes.get("/download/:type/:id", (req: Request, res: Response) => {
+    const directoryPath = path.join(__dirname, "../files" + "/" + req.params.type)
+    res.download(directoryPath + "/" + req.params.id)
+})
+export default fileRoutes
