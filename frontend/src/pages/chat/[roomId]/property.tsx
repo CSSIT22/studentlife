3dca31f10607ac39e5b5a918106b16af78e8cce4
Kanyarat <kanyarat.prem@kmutt.ac.th>
2022-11-10 21:37:03 +0700

    property back to room

--- frontend/src/pages/chat/[roomId]/property.tsx
+++ frontend/src/pages/chat/[roomId]/property.tsx
@@ -1,29 +1,103 @@
-import { Box, Container, HStack, VStack } from "@chakra-ui/react"
+import { Box, Button, Container, HStack, useDisclosure, VStack } from "@chakra-ui/react"
+import { Heading } from "@chakra-ui/react"
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton } from '@chakra-ui/react'
+
 import { ArrowBackIcon } from "@chakra-ui/icons"
-import { IconButton, Heading } from "@chakra-ui/react"
-import { List, ListItem, ListIcon, OrderedList, UnorderedList } from "@chakra-ui/react"
-import { CgProfile } from "react-icons/cg";
-import { MdOutlineDriveFileRenameOutline, MdPostAdd, MdColorLens, MdFlag } from "react-icons/md";
-import { RiUserSettingsLine } from "react-icons/ri";
+import { CgProfile } from "react-icons/cg"
+import { MdOutlineDriveFileRenameOutline, MdPostAdd, MdColorLens, MdFlag } from "react-icons/md"
+import { RiUserSettingsLine } from "react-icons/ri"
+import { FaUserFriends, FaUserPlus, FaHome, FaDoorOpen } from "react-icons/fa"
+import { AiFillPicture } from "react-icons/ai"
+
 import React from "react"
 import AppBody from "../../../components/share/app/AppBody"
+import { useNavigate, useParams } from "react-router-dom"
+
+type room = { roomID: String; roomName: String; roomtype: "individual" | "group"; img: String }[]
 
 const Property = () => {
+    let param = useParams();
+
+    const navigate = useNavigate()
+    //function handle
+    function Navigate() {
+        return navigate(`/chat/${param.roomID}`)
+    }
+
     return (
         <AppBody>
-            <HStack spacing="24px">
-                <IconButton aria-label="Back to chat room" size="md" icon={<ArrowBackIcon />} />
+            <HStack spacing={5}>
+                <Button aria-label="Back to chat room" size="md" leftIcon={<ArrowBackIcon />} onClick={Navigate()}>
+                </Button>
                 <Heading size="lg">Chat properties</Heading>
             </HStack>
-            <VStack marginLeft={20} marginTop={10} spacing={12} align="flex-start" fontSize="20">
-                <HStack><CgProfile></CgProfile><Box>View Profile</Box></HStack>
-                <HStack><MdOutlineDriveFileRenameOutline></MdOutlineDriveFileRenameOutline><Box>Set room name</Box></HStack>
-                <HStack><RiUserSettingsLine></RiUserSettingsLine><Box>Set nickname</Box></HStack>
-                <HStack><MdPostAdd></MdPostAdd><Box>Add quote</Box></HStack>
-                <HStack><MdColorLens></MdColorLens><Box>Change room color</Box></HStack>
-                <HStack><MdFlag></MdFlag><Box>Report user</Box></HStack>
+            <VStack marginLeft={12} marginTop={10} spacing={8} align="flex-start" fontSize="20">
+                <Button leftIcon={<CgProfile />} variant="ghost" size="lg">
+                    View Profile
+                </Button>
+                <Button leftIcon={<MdOutlineDriveFileRenameOutline />} variant="ghost" size="lg">
+                    Set room name
+                </Button>
+                <Button leftIcon={<RiUserSettingsLine />} variant="ghost" size="lg">
+                    Set nickname
+                </Button>
+                <Button leftIcon={<MdPostAdd />} variant="ghost" size="lg">
+                    Add quote
+                </Button>
+                <Button leftIcon={<MdColorLens />} variant="ghost" size="lg">
+                    Change room color
+                </Button>
+                <Button leftIcon={<MdFlag />} variant="ghost" size="lg">
+                    Report user
+                </Button>
+                {/* Group */}
+                <Button leftIcon={<FaUserFriends />} variant="ghost" size="lg">
+                    Member
+                </Button>
+                <Button leftIcon={<FaUserPlus />} variant="ghost" size="lg">
+                    Invite people
+                </Button>
+                <Button leftIcon={<AiFillPicture />} variant="ghost" size="lg">
+                    Set room profile
+                </Button>
+                <Button leftIcon={<FaHome />} variant="ghost" size="lg">
+                    Create community
+                </Button>
+                <Button leftIcon={<FaDoorOpen />} variant="ghost" size="lg">
+                    Leave group
+                </Button>
+                <Button>
+                    <showModal />
+                </Button>
             </VStack>
         </AppBody>
     )
 }
+
+function showModal() {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    return (
+      <>
+        <Button onClick={onOpen}>Open Modal</Button>
+  
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Modal Title</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              Test Test Test
+            </ModalBody>
+  
+            <ModalFooter>
+              <Button colorScheme='blue' mr={3} onClick={onClose}>
+                Close
+              </Button>
+              <Button variant='ghost'>Secondary Action</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </>
+    )
+  }
 export default Property
