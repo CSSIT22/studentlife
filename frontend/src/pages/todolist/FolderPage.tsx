a96836a43d496e2568c92297bb1410e99a274e4d
phoomaw10 <phoomaw10@gmail.com>
2022-11-10 20:38:52 +0700

    newone

--- frontend/src/pages/todolist/FolderPage.tsx
+++ frontend/src/pages/todolist/FolderPage.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
-const FolderPage = () => {
-    return <div>FolderPage</div>
+const folderpage = () => {
+    return <div>folder page</div>
 }
 
-export default FolderPage
+export default folderpage
