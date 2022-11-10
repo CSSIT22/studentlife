b74c23bd3ae9d767406ab107c1ee962d332101bf
bbbung <kanyapat.mekv@kmutt.ac.th>
2022-11-11 00:06:15 +0700

    [UPDATE] template on test, ExpandOnTop

---
+++ frontend/src/components/annoucement/ExpandOnTop.tsx
@@ -0,0 +1,22 @@
+import { Flex, Heading, Box,Text} from '@chakra-ui/react'
+import React, {FC} from 'react'
+import { TfiAnnouncement } from 'react-icons/tfi'
+
+const ExpandOnTop:FC<{
+    topic: string,
+    sender:string
+}> = ({topic,sender}) => {
+  return (
+    <Flex alignItems={"center"} py={"5"} borderBottom={"1px solid"}>
+                    <Box pr={"5"}>
+                        <TfiAnnouncement fontSize={"1.5rem"} />
+                    </Box>
+                    <Box>
+                        <Heading size={"sm"}>{topic}</Heading>
+                        <Text fontSize={"xs"}>{sender}</Text>
+                    </Box>
+                </Flex>
+  )
+}
+
+export default ExpandOnTop
