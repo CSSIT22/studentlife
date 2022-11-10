36b8cebbd9317e2c325ea2a9583c7ca00ccb98f5
parnlees <yanisa0439@gmail.com>
2022-11-10 23:08:28 +0700

    create task

--- frontend/src/pages/todolist/CreateTask.tsx
+++ frontend/src/pages/todolist/CreateTask.tsx
@@ -29,9 +29,59 @@ import ToDoListAppBody from "src/components/todolist/ToDoListAppBody"
 const createtask = () => {
     return (
         <ToDoListAppBody>
-            <Heading as="h2" size="3xl" noOfLines={1}>
+            <Heading as="h1" size="3xl" noOfLines={1}>
                 Create Task
             </Heading>
+
+            <Box margin-top={10}>
+                <Heading as="h2" size="md" noOfLines={1} marginY={10}>
+                    Task Name
+                </Heading>
+                <Input placeholder="Task Name" size="md" />
+
+                <Heading as="h2" size="md" noOfLines={1} marginY={10}>
+                    Description
+                </Heading>
+                <Input placeholder="Descrription" size="md" />
+
+                <Heading as="h2" size="md" noOfLines={1} marginY={10}>
+                    Due Date
+                </Heading>
+                {/* <Input placeholder="Select Date and Time" size="md" type="datetime-local" /> */}
+                <label>
+                    <input type="date" name="bday" required pattern="\d{4}/\d{2}/\d{2}" />
+                </label>
+
+                <Heading as="h2" size="md" noOfLines={1} marginY={10}>
+                    Time
+                </Heading>
+                <form>
+                    <input id="appt-time" type="time" name="appt-time" />
+                </form>
+
+                <Heading as="h2" size="md" noOfLines={1} marginY={10}>
+                    Type
+                </Heading>
+                <Select placeholder="Choose" size="md">
+                    <option value="option1">Individual</option>
+                    <option value="option2">Group</option>
+                </Select>
+
+                <Heading as="h2" size="md" noOfLines={1} marginY={10}>
+                    Alert
+                </Heading>
+                <Select placeholder="Choose" size="md">
+                    <option value="option1">1 day before due date</option>
+                    <option value="option1">3 days before due date</option>
+                    <option value="option1">7 days before due date</option>
+                </Select>
+
+                <Box display="flex" justifyContent="center" alignItems="center" marginY={10}>
+                    <Button colorScheme="teal" size="lg" bgColor={"orange.500"}>
+                        Done
+                    </Button>
+                </Box>
+            </Box>
         </ToDoListAppBody>
     )
 }
