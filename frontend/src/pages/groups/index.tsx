efa7832b2d79970c82cb858a42c9c167d85b1b17
Passakorn <boss4848988@gmail.com>
2022-11-10 22:19:50 +0700

    [FIX] main page responsive design

--- frontend/src/pages/groups/index.tsx
+++ frontend/src/pages/groups/index.tsx
@@ -1,165 +1,237 @@
-import { Box, Button, Grid, GridItem, Input, InputGroup, InputLeftElement, VStack, Text, Image, HStack } from "@chakra-ui/react"
-import React from "react"
-import CommunityList from "../../components/group/CommunityList"
-import InvatationBox from "../../components/group/InvatationBox"
+import { Show, Box, HStack, Button, Input, Stack, Text, Flex, Grid } from "@chakra-ui/react"
+import React, { Suspense, useState } from "react"
 import AppBody from "../../components/share/app/AppBody"
-const invitations = [
-    {
-        inviteID: 1,
-        communityName: "Programmer community",
-        memberNumber: 8000,
-        coverPhoto: "https://picsum.photos/id/300/200",
-        isPrivate: true,
-        userName: "Passakorn puttama", //name of the person who invited
-        expireDate: "28",
-    },
-    {
-        inviteID: 2,
-        communityName: "Noob community",
-        memberNumber: 4000,
-        coverPhoto: "https://picsum.photos/id/301/200",
-        isPrivate: false,
-        userName: "Kitty Melody", //name of the person who invited
-        expireDate: "28",
-    },
-]
-const communitys = [
-    {
-        ID: 1,
-        name: "Dota2",
-        Owner: "1",
-        Member: 666,
-        Tag: [2, 4],
-        Describe: "Best mental therapy center",
-        isPrivate: true,
-        coverPhoto: "https://picsum.photos/id/1/200",
-    },
-    {
-        ID: 2,
-        name: "Memeworld",
-        Owner: "3",
-        Member: 300,
-        Tag: [3],
-        Describe: "Storage of meme around the world",
-        isPrivate: false,
-        coverPhoto: "https://picsum.photos/id/2/200",
-    },
-    {
-        ID: 3,
-        name: "IndianFood",
-        Owner: "2",
-        Member: 150,
-        Tag: [1, 2],
-        Describe: "No masara no flavor we can’t eat.",
-        isPrivate: true,
-        coverPhoto: "https://picsum.photos/id/3/200",
-    },
-    {
-        ID: 4,
-        name: "ThaiStreetFood",
-        Owner: "3",
-        Member: 50,
-        Tag: [1],
-        Describe: "Secret thai street food that have to try once",
-        isPrivate: false,
-        coverPhoto: "https://picsum.photos/id/4/200",
-    },
-]
+import { FaSearch, FaPlus } from "react-icons/fa"
+import { TiWarning } from "react-icons/ti"
+import { userData } from "./data"
+import CommunityList from "../../components/group/CommunityList"
+import SuggestBox from "../../components/group/SuggestBox"
+import InvitationBox from "../../components/group/InvitationBox"
+import useWindowDimensions from "./hooks/useWindowDimensions.js"
 
-const Groups = () => {
+const index = () => {
+    const [searchBtn, setSearchBtn] = useState(false) //for close/open seach bar
+    const [searchValue, setSearchValue] = useState("") //for store search value
+    const handleChange = (event: any) => setSearchValue(event.target.value)
+    const handleSearchBtn = () => {
+        setSearchBtn(!searchBtn)
+        setSearchValue("")
+    }
+    //for mobile
+    const [allBtn, setAllBtn] = useState(true)
+    const [inviteBtn, setInviteBtn] = useState(false)
+    const [suggestBtn, setSuggestBtn] = useState(false)
+    const handleAllBtnOnClick = () => {
+        setAllBtn(true)
+        setInviteBtn(false)
+        setSuggestBtn(false)
+    }
+    const handleInviteOnClick = () => {
+        setAllBtn(false)
+        setInviteBtn(true)
+        setSuggestBtn(false)
+    }
+    const handleSuggestOnClick = () => {
+        setAllBtn(false)
+        setInviteBtn(false)
+        setSuggestBtn(true)
+    }
+    //md: 768px
+    const { height, width } = useWindowDimensions()
     return (
         <AppBody>
-            <Grid templateColumns="repeat(8, 1fr)" gap="2">
-                {/* Left side */}
-                <GridItem colSpan={3} w="100%">
-                    <VStack align="center">
-                        <Input
-                            fontWeight="bold"
-                            variant="filled"
-                            placeholder="Search Communitys"
-                            textAlign="center"
-                            focusBorderColor="tomato"
-                            fontSize="sm"
-                        />
-                        <Button colorScheme="orange" variant="solid" width="95%" fontSize="sm">
-                            + Craete New Community
-                        </Button>
-                        <Box
-                            className="Community_Lists"
-                            width="100%"
-                            height="300px"
-                            sx={{
-                                "-webkit-overflow-scrolling": "touch" /* enables momentum-scrolling on iOS */,
-                                overflowY: "scroll",
-                                scrollBehavior: "smooth",
-                                "::-webkit-scrollbar-track": {
-                                    background: "none",
-                                },
-                                "::-webkit-scrollbar-thumb": {
-                                    background: "orange",
-                                },
-                            }}
-                        >
-                            {communitys ? (
-                                <div>
-                                    <Box borderRadius="md" backgroundColor="gray.100" width="100%" pt={4} textAlign="start" px={5} pb={4}>
-                                        <Text as="b" fontSize="sm">
-                                            Community you manage
-                                        </Text>
-                                        {communitys.map((community) => (
-                                            <CommunityList
-                                                key={community.ID}
-                                                communityName={community.name}
-                                                isPrivate={community.isPrivate}
-                                                coverPhoto={community.coverPhoto}
-                                                lastActive={"3"}
-                                            />
-                                        ))}
-                                    </Box>
-                                    <Box mt={2} borderRadius="md" backgroundColor="gray.100" width="100%" textAlign="start" px={5} py={4}>
-                                        <Text as="b" fontSize="sm">
-                                            Community you've joined
-                                        </Text>
-                                        {communitys.map((community) => (
-                                            <CommunityList
-                                                key={community.ID}
-                                                communityName={community.name}
-                                                isPrivate={community.isPrivate}
-                                                coverPhoto={community.coverPhoto}
-                                                lastActive={"3"}
-                                            />
-                                        ))}
-                                    </Box>
-                                </div>
+            {/* <div>
+                width: {width} ~ height: {height}
+            </div> */}
+            <Flex gap={2} direction={{ base: "column", md: "row" }}>
+                <Box>
+                    <Show below="md">
+                        <HStack justify={"space-between"}>
+                            <HStack gap={1} display={searchBtn ? "none" : "flex"}>
+                                <Button backgroundColor={allBtn ? "gray.300" : "gray.100"} onClick={handleAllBtnOnClick}>
+                                    All
+                                </Button>
+                                <Button backgroundColor={inviteBtn ? "gray.300" : "gray.100"} onClick={handleInviteOnClick}>
+                                    Invite
+                                </Button>
+                                <Button backgroundColor={suggestBtn ? "gray.300" : "gray.100"} onClick={handleSuggestOnClick}>
+                                    Suggested
+                                </Button>
+                            </HStack>{" "}
+                            {allBtn ? (
+                                <Stack direction={"row"} justify="end" width={"100%"} gap={1}>
+                                    <Button display={searchBtn ? "none" : "flex"}>
+                                        <FaPlus />
+                                    </Button>
+                                    <Input
+                                        width={"100%"}
+                                        display={searchBtn ? "" : "none"}
+                                        variant={"filled"}
+                                        type={"search"}
+                                        value={searchValue}
+                                        onChange={handleChange}
+                                        placeholder="Seacrh Community"
+                                        focusBorderColor="gray.200"
+                                    ></Input>
+                                    <Button onClick={handleSearchBtn}>
+                                        <FaSearch />
+                                    </Button>
+                                </Stack>
                             ) : (
-                                <Text>empty</Text>
+                                <div></div>
                             )}
+                        </HStack>
+                    </Show>
+                    <Show above="md">
+                        <Box textAlign={"center"}>
+                            <Input
+                                textAlign={"center"}
+                                width={"100%"}
+                                variant={"filled"}
+                                type={"search"}
+                                value={searchValue}
+                                onChange={handleChange}
+                                placeholder="Seacrh Community"
+                                focusBorderColor="gray.200"
+                            ></Input>
+                            <Button mt="2" colorScheme="orange" variant="solid" width="95%" fontSize="sm">
+                                + Create New Community
+                            </Button>
                         </Box>
-                    </VStack>
-                </GridItem>
-                {/* right side */}
-                <GridItem colSpan={5} w="100%" bg="tomato">
-                    <Box borderRadius="md" backgroundColor="gray.100" width="100%" pt={4} textAlign="start" pl={5} pr={5} pb={4}>
-                        <Text as="b" fontSize="sm">
-                            Invitation
-                        </Text>
-                        <Text fontSize="sm">These people have been invited to join the community</Text>
-                        {invitations.map((i) => (
-                            <InvatationBox
-                                key={i.inviteID}
-                                userName={i.userName}
-                                communityName={i.communityName}
-                                memberNumber={i.memberNumber}
-                                isPrivate={i.isPrivate}
-                                coverPhoto={i.coverPhoto}
-                                expireDate={i.expireDate}
-                            />
-                        ))}
+                    </Show>
+                    <Box display={allBtn || width > 768 ? "block" : "none"}>
+                        <Box mt={4} mb={3}>
+                            <Text as="b">Community you manage</Text>
+                        </Box>
+
+                        {userData.ownCommunitys.filter((community) => community.name.toLowerCase().includes(searchValue.toLowerCase())).length > 0 ? (
+                            userData.ownCommunitys
+                                .filter((community) => {
+                                    return searchValue.toLowerCase() == "" ? community : community.name.toLowerCase().includes(searchValue)
+                                })
+                                .map((community) => (
+                                    <CommunityList
+                                        communityName={community.name}
+                                        lastActive={"9"}
+                                        coverPhoto={community.coverPhoto}
+                                        isPrivate={community.isPrivate}
+                                    />
+                                ))
+                        ) : (
+                            <Box borderRadius="md" backgroundColor="red.200" mt={2}>
+                                <Box p={2} borderRadius="md">
+                                    <HStack gap={2}>
+                                        <Box height={"55px"}></Box>
+                                        <div>
+                                            <Box display="flex" alignItems="center" gap={1}>
+                                                <TiWarning />
+                                                <Text as="b" fontSize="sm">
+                                                    Community not found :(
+                                                </Text>
+                                            </Box>
+                                            <Text fontSize="sm">Try to search again</Text>
+                                        </div>
+                                    </HStack>
+                                </Box>
+                            </Box>
+                        )}
+
+                        <Box mt={4} mb={3}>
+                            <Text as="b">Community you've joined</Text>
+                        </Box>
+                        {userData.joinedCommunitys.filter((community) => community.name.toLowerCase().includes(searchValue.toLowerCase())).length >
+                        0 ? (
+                            userData.joinedCommunitys
+                                .filter((community) => {
+                                    return searchValue.toLowerCase() == "" ? community : community.name.toLowerCase().includes(searchValue)
+                                })
+                                .map((community) => (
+                                    <CommunityList
+                                        communityName={community.name}
+                                        lastActive={"9"}
+                                        coverPhoto={community.coverPhoto}
+                                        isPrivate={community.isPrivate}
+                                    />
+                                ))
+                        ) : (
+                            <Box borderRadius="md" backgroundColor="red.200" mt={2}>
+                                <Box p={2} borderRadius="md">
+                                    <HStack gap={2}>
+                                        <Box height={"55px"}></Box>
+                                        <div>
+                                            <Box display="flex" alignItems="center" gap={1}>
+                                                <TiWarning />
+                                                <Text as="b" fontSize="sm">
+                                                    Community not found :(
+                                                </Text>
+                                            </Box>
+                                            <Text fontSize="sm">Try to search again</Text>
+                                        </div>
+                                    </HStack>
+                                </Box>
+                            </Box>
+                        )}
                     </Box>
-                </GridItem>
-            </Grid>
+                </Box>
+                {/* <Show above="md"> */}
+                <Show>
+                    <Box width={"100%"}>
+                        <Box
+                            display={inviteBtn || width > 768 ? "block" : "none"}
+                            borderRadius="md"
+                            backgroundColor="gray.100"
+                            width="100%"
+                            pt={4}
+                            textAlign="start"
+                            pl={5}
+                            pr={5}
+                            pb={4}
+                        >
+                            <Text as="b">Invitation</Text>
+                            <Text fontSize="sm">These people have been invited to join the community</Text>
+                            {userData.invitations.map((i) => (
+                                <InvitationBox
+                                    key={i.inviteID}
+                                    userName={i.userName}
+                                    communityName={i.communityName}
+                                    memberNumber={i.memberNumber}
+                                    isPrivate={i.isPrivate}
+                                    coverPhoto={i.coverPhoto}
+                                    expireDate={i.expireDate}
+                                />
+                            ))}
+                        </Box>
+                        <Box
+                            display={suggestBtn || width > 768 ? "block" : "none"}
+                            mt={2}
+                            borderRadius="md"
+                            backgroundColor="gray.100"
+                            width="100%"
+                            pt={4}
+                            textAlign="start"
+                            pl={5}
+                            pr={5}
+                            pb={4}
+                        >
+                            <Text as="b">Suggested for you</Text>
+                            <Text fontSize="sm">Communitys you might be interested in.</Text>
+                            <Grid gap={2} templateColumns={{ sm: "repeat(1,1fr)", md: "repeat(2, 1fr)" }} width="100%">
+                                {userData.invitations.map((i) => (
+                                    <SuggestBox
+                                        key={i.inviteID}
+                                        communityName={i.communityName}
+                                        memberNumber={i.memberNumber}
+                                        isPrivate={i.isPrivate}
+                                        coverPhoto={i.coverPhoto}
+                                    />
+                                ))}
+                            </Grid>
+                        </Box>
+                    </Box>
+                </Show>
+            </Flex>
         </AppBody>
     )
 }
 
-export default Groups
+export default index
