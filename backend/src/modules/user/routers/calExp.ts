03031942435261160d05fa7cfdc62fe661479714
Pattarapon Koomphon <45892157+Kevinindy2k@users.noreply.github.com>
2022-11-10 16:49:56 +0700

    Add all useful ExpRouter

--- backend/src/modules/user/routers/calExp.ts
+++ backend/src/modules/user/routers/calExp.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express"
 const calExp = async (req: Request, res: Response) => {
     try {
         const { prisma } = res
+        const userId = req.user?.userId
     } catch (err) {
         res.status(500).send("Not found Formula")
     }
