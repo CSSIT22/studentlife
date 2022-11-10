c939fdd8c8bce3b6f8b049b836486950a45fe277
saranratR <fahhiha36@gmail.com>
2022-11-10 22:45:43 +0700

    add button

--- frontend/src/pages/schedule/timetable.tsx
+++ frontend/src/pages/schedule/timetable.tsx
@@ -1,12 +1,27 @@
 import React from "react"
 import AppBody from "../../components/share/app/AppBody"
-import { Box, SimpleGrid } from "@chakra-ui/react"
+import { Box, extendTheme, SimpleGrid } from "@chakra-ui/react"
+import { IconButton } from "@chakra-ui/react"
+import { AddIcon, ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons"
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton } from "@chakra-ui/react"
+import { Button, ButtonGroup } from "@chakra-ui/react"
+//import { ChevronRightIcon } from "@chakra-ui/icons"
+//import { AddIcon } from "@chakra-ui/icons"
+const theme = extendTheme({
+    colors: {
+        brand: {
+            "200": "#9AE6B4",
+        },
+    },
+})
 
 const timetable = () => {
     return (
         <AppBody>
             timetable
-            <SimpleGrid>
+            <SimpleGrid columns={[1, 6]} spacing="40px">
+                <IconButton aria-label="previous" icon={<ChevronLeftIcon />} />
+
                 <Box boxShadow="md" p="6" rounded="md" bg="white">
                     Date
                 </Box>
@@ -16,6 +31,17 @@ const timetable = () => {
                 <Box boxShadow="md" p="6" rounded="md" bg="white">
                     Year
                 </Box>
+                <IconButton aria-label="next" icon={<ChevronRightIcon />} />
+                <IconButton
+                    w="60px"
+                    h="62px"
+                    colorScheme="green"
+                    aria-label="Add event"
+                    size="sm"
+                    icon={<AddIcon />}
+                    borderRightRadius="55"
+                    borderLeftRadius="55"
+                />
             </SimpleGrid>
         </AppBody>
     )
