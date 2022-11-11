70c1fac1eb3d346d15b7f6170176eba8c8555927
Khush Agarwal <khush.agar@kmutt.ac.th>
2022-11-11 16:15:21 +0700

    Added Themed Button and changed font of page title

--- frontend/src/components/shop/PageTitle.tsx
+++ frontend/src/components/shop/PageTitle.tsx
@@ -4,7 +4,7 @@ import React, { FC } from "react"
 const PageTitle: FC<{ title: string, p?: string }> = ({ title,p }) => {
     return (
         <Center pb={p ? p : 7}>
-            <Text fontSize={"4xl"} as="h1" fontWeight="bold">
+            <Text  fontSize={"4xl"} as="h1" fontWeight="700">
                 {title}
             </Text>
         </Center>
