d68bdfbbba590dd8d5ab98fb544c082e1df74b50
phoomaw10 <phoomaw10@gmail.com>
2022-11-11 15:34:46 +0700

    Pthun

--- frontend/src/pages/todolist/FolderPage.tsx
+++ frontend/src/pages/todolist/FolderPage.tsx
@@ -1,7 +1,44 @@
+import {
+    List,
+    ListItem,
+    ListIcon,
+    OrderedList,
+    UnorderedList,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure,
+    Button,
+    Image,
+    Heading,
+    Input,
+    Center,
+    Select,
+    Spacer,
+    Box,
+    StackDivider,
+    VStack,
+    GridItem,
+    Flex,
+    Text,
+    Link,
+} from "@chakra-ui/react"
 import React from "react"
+import ToDoListAppBody from "src/components/todolist/ToDoListAppBody"
+import AppBody from "src/components/share/app/AppBody"
 
 const folderpage = () => {
-    return <div>folder page</div>
+    return (
+        <ToDoListAppBody>
+            <Heading as="h1" size="3xl">
+                Folder Page
+            </Heading>
+        </ToDoListAppBody>
+    )
 }
 
 export default folderpage
