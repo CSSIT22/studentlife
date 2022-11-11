10bf9f4c5cd8974c5a4f2046246866f0d08e6106
Khush Agarwal <khush.agar@kmutt.ac.th>
2022-11-11 16:16:24 +0700

    Added some Dummy Content

--- frontend/src/pages/shop/categories/categoryDetails.tsx
+++ frontend/src/pages/shop/categories/categoryDetails.tsx
@@ -2,11 +2,13 @@ import React from "react"
 import PageTitle from "../../../components/shop/PageTitle"
 import ShopAppBody from "../../../components/shop/ShopAppBody"
 import { Center, Text } from "@chakra-ui/react"
+import ProductList from "../../../components/shop/ProductList"
 const categoryDetails = () => {
     return (
         <ShopAppBody>
             <PageTitle p="1" title="Category: {selected category}" />
             <Center><Text>xxx Items Found in this category</Text></Center>
+            <ProductList />
         </ShopAppBody>
     )
 }
