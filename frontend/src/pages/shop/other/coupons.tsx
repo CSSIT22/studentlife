3648e2bca8e375987352ed082fa6664f8a128361
Khush Agarwal <khush.agar@kmutt.ac.th>
2022-11-12 20:18:23 +0700

    Changed to generate coupons

--- frontend/src/pages/shop/other/coupons.tsx
+++ frontend/src/pages/shop/other/coupons.tsx
@@ -8,9 +8,26 @@ const Coupons = () => {
     return (
         <ShopAppBody>
             <PageTitle title="Your Coupons" />
-            <CouponDisplay></CouponDisplay>
+            <Flex gap={5} direction="column">
+                {generateCoupons(7)}
+            </Flex>
         </ShopAppBody>
     )
 }
 
 export default Coupons
+function generateCoupons(n: number) {
+    const coupons = []
+    for (let i = 0; i < n; i++) {
+        coupons.push(
+            <CouponDisplay
+                couponCode={"Free" + (i + 10)}
+                discountAmount={50}
+                details="Buy for 500 get 50 discount"
+                validUntil="10/Sep/2023"
+                minSpend={250}
+            ></CouponDisplay>
+        )
+    }
+    return coupons
+}
