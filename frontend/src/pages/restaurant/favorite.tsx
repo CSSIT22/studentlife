0cfea58c3dd9f7c5d4a3c73700d1e9dad7c9a7c0
Jakrapat-M <jakrapat.matures@mail.kmutt.ac.th>
2022-11-12 22:02:43 +0700

    fav show only status = true, log delete status

--- frontend/src/pages/restaurant/favorite.tsx
+++ frontend/src/pages/restaurant/favorite.tsx
@@ -1,12 +1,13 @@
-import { Heading, Box, Grid, GridItem } from "@chakra-ui/react"
+import { Heading, Box, Grid, GridItem, Button } from "@chakra-ui/react"
 import React from "react"
-import { Link } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import FavoriteContent from "../../components/restaurant/FavoriteContent"
 import Searchbar from "../../components/restaurant/searchbar"
 import AppBody from "../../components/share/app/AppBody"
 import { Restaurant } from "./data/restaurant"
 
-const favorite = () => {
+function favorite() {
+    const showfv = Restaurant.filter((e1) => e1.status === true)
     return (
         <AppBody
             secondarynav={[
@@ -17,14 +18,22 @@ const favorite = () => {
         >
             <Searchbar />
 
-            <Heading mt={"20px"} textAlign="center">
+            <Heading color={"#E65300"} mt={"20px"} textAlign="center">
                 Favorite
             </Heading>
             <Grid templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(2, 1fr)", lg: "repeat(3, 1fr)" }} gap={"6"}>
-                {Restaurant.map((e1) => {
+                {showfv.map((e1) => {
                     return (
                         <GridItem>
-                                <FavoriteContent resName={e1.resName} phone={e1.phone} open={e1.open} close={e1.close} website={e1.website} link={`/restaurant/detail/${e1.id}`}/>
+                            <FavoriteContent
+                                id={e1.id}
+                                resName={e1.resName}
+                                phone={e1.phone}
+                                open={e1.open}
+                                close={e1.close}
+                                website={e1.website}
+                                link={`/restaurant/detail/${e1.id}`}
+                            />
                         </GridItem>
                     )
                 })}
