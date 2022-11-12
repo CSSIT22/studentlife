128af8f4e4e73212e6c298fabb40f1c503aaa280
Thitipa <thitipanet@gmail.com>
2022-11-12 17:18:58 +0700

    Add some elements to rating page

--- frontend/src/pages/dating/rating.tsx
+++ frontend/src/pages/dating/rating.tsx
@@ -1,7 +1,22 @@
-import AppBody from "../../components/share/app/AppBody"
+import { Heading, Stack, Text, Box, Image, Flex } from "@chakra-ui/react"
+import DatingAppBody from "../../components/dating/DatingAppBody"
 
 const Rating = () => {
-    return <AppBody>Rating</AppBody>
+    return (
+        <DatingAppBody>
+            <Stack color="black" pt="20px">
+                <Heading>Rating</Heading>
+                <Text fontSize="xl">You are friend with</Text>
+                <Text>Search bar 1 EA</Text>
+                <Box mt="50px" p="20px" bg="#E67F45" borderRadius={"10px"}>
+                    <Flex>
+                        <Image borderRadius="full" boxSize="78px" src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
+                        <Text>Firstname Lastname</Text>
+                    </Flex>
+                </Box>
+            </Stack>
+        </DatingAppBody>
+    )
 }
 
 export default Rating
