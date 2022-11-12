b0875043a9df1ac0939dd08ae49a323f2339a38f
Pasinun.w <95624019+Unuisad@users.noreply.github.com>
2022-11-12 21:23:38 +0700

    [MOD] remove <Box>

--- frontend/src/components/shortnotes/inLiList.tsx
+++ frontend/src/components/shortnotes/inLiList.tsx
@@ -6,16 +6,14 @@ const inLiList: FC<{
     course: String
 }> = ({ name, course }) => {
     return (
-        <Box boxShadow={"md"} bg={"white"} rounded={8} p={5} w="100%">
-            <Grid templateColumns="repeat(2, 1fr)">
-                <GridItem colSpan={1}>
-                    <Text>{name}</Text>
-                </GridItem>
-                <GridItem colSpan={1}>
-                    <Text>{course}</Text>
-                </GridItem>
-            </Grid>
-        </Box>
+        <Grid templateColumns="repeat(5, 1fr)" boxShadow={"md"} bg={"white"} rounded={8} p={5} w="100%" h={"100px"} alignItems={"center"}>
+            <GridItem colSpan={4}>
+                <Text>{name}</Text>
+            </GridItem>
+            <GridItem colSpan={1} textAlign={"right"}>
+                <Text>{course}</Text>
+            </GridItem>
+        </Grid>
     )
 }
 
