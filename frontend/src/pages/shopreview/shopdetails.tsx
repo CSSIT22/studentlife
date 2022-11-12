1b73b9be7871d433c68dfa0831750c8e387ad09f
Joeleely <Varanyu50015@gmail.com>
2022-11-12 15:14:20 +0700

    Update shopdetails.tsx

--- frontend/src/pages/shopreview/shopdetails.tsx
+++ frontend/src/pages/shopreview/shopdetails.tsx
@@ -1,11 +1,21 @@
-import { Heading } from "@chakra-ui/react"
+import { ChevronLeftIcon } from "@chakra-ui/icons"
+import { Flex, Heading, Link, SimpleGrid } from "@chakra-ui/react"
 import React from "react"
+import Myreview from "src/components/shopreview/Myreview"
 import AppBody from "../../components/share/app/AppBody"
+import DetailBox from "../../components/shopreview/DetailBox"
+import MentDetail from "../../components/shopreview/ReviewDetail"
+import ShopName from "../../components/shopreview/ShopName"
 
 const shopdetails = () => {
     return (
         <AppBody>
-            <Heading>Shop details!!!</Heading>
+            <Flex mb={5} direction={"row"} alignItems={"center"}>
+                <Link href="/shopreview">
+                    <ChevronLeftIcon w={8} h={8} />
+                </Link>
+                <Heading>Shop detail!!</Heading>
+            </Flex>
         </AppBody>
     )
 }
