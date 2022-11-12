792dea076648f37dd28e7f36e759cd4438e4dd91
Nattapat <56881355+Nattapat-is-my-name@users.noreply.github.com>
2022-11-13 00:33:34 +0700

    Update loginRoutes.ts
    create User_Back

--- backend/src/modules/backendService/login/loginRoutes.ts
+++ backend/src/modules/backendService/login/loginRoutes.ts
@@ -2,6 +2,9 @@ import { Router } from "express"
 import passport from "passport"
 import { Request, Response } from "express"
 import UserAgent from "user-agents"
+import { PrismaClient, Prisma } from "@prisma/client"
+
+const prisma = new PrismaClient()
 const router = Router()
 
 router.get(
@@ -19,10 +22,35 @@ router.get(
         successRedirect: process.env.SUCCESS_REDIRECT_URL,
         session: true,
     }),
-    (req: Request, res: Response) => {
-        // console.log(req.headers["user-agent"])
+    async (req: Request, res: Response) => {
+        //console.log(req.headers["user-agent"])
         const device = new UserAgent(req.headers["user-agent"])
-        // console.log(device)
+        const { prisma } = res
+        console.log(req.user?.userId)
+        try {
+            const user = await prisma.user_Back.create({
+                data: {
+                    userId: req.user?.userId || "",
+                    token: req.session.id,
+                    loginSession: {
+                        create: {
+                            detail: {
+                                create: {
+                                    deviceInfo: device.data.deviceCategory || "Unknow",
+                                    ip: device.data.platform,
+                                    tokenExpired: req.session.cookie.expires || Date.now().toString(),
+                                },
+                            },
+                        },
+                    },
+                },
+            })
+        } catch (error) {
+            res.status(500).send("These is error is login")
+            console.log(error)
+        }
+
+        console.log(device.data)
         res.json({ ...req.user, ...device.data })
     }
 )
