0fffc8486acb4583c09f7aabf1baf1b2a5ea9ae7
Khush Agarwal <khush.agar@kmutt.ac.th>
2022-11-12 18:42:39 +0700

    Added Space

--- frontend/src/components/shop/CouponDisplay.tsx
+++ frontend/src/components/shop/CouponDisplay.tsx
@@ -9,7 +9,7 @@ const CouponDisplay = () => {
                 <GridItem rowSpan={8} colSpan={2} overflow={"hidden"} bg= "red">
                     <Flex h="full" justify="center" align="center">
                         <Text>// Coupon Code</Text>
-                    </Flex>
+                    </Flex>  
                 </GridItem>
                 <GridItem rowSpan={8} colSpan={1} overflow={"hidden"} bg="red">
                     <Box h="full" w="min" bg="black" p="0.3"></Box>
