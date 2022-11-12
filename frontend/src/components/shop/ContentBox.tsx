653e996a248c6b37382c7d9c47aeb8061928fc69
Khush Agarwal <khush.agar@kmutt.ac.th>
2022-11-12 20:17:20 +0700

    Made props editable

--- frontend/src/components/shop/ContentBox.tsx
+++ frontend/src/components/shop/ContentBox.tsx
@@ -3,7 +3,7 @@ import React, { FC } from "react"
 
 const ContentBox = (props: any) => {
     return (
-        <Box bg="#f1f1f1" borderRadius="lg" shadow="md">
+        <Box bg={props.bg ? props.bg : "#f1f1f1"} borderRadius={props.br ? props.br : "xl"} shadow={props.sh ? props.sh : "md"}>
             {props.children}
         </Box>
     )
