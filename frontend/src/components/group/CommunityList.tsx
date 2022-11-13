957ac7c76ac7e7996aa72fb79c359f5533aeb0f7
Passakorn <boss4848988@gmail.com>
2022-11-13 11:12:14 +0700

    [UPDATE] community list
    Add a link to a component to link to the community

--- frontend/src/components/group/CommunityList.tsx
+++ frontend/src/components/group/CommunityList.tsx
@@ -1,31 +1,36 @@
 import { HStack, Box, Image, Text } from "@chakra-ui/react"
 import React, { FC } from "react"
 import { MdPublic, MdPublicOff } from "react-icons/md"
+import { Link } from "react-router-dom"
 
-const CommunityList: FC<{ communityName: string; lastActive: string; coverPhoto: string; isPrivate: boolean; roleID: number}> = ({
+const CommunityList: FC<{ communityID: number; communityName: string; lastActive: string; coverPhoto: string; isPrivate: boolean; roleID: number }> = ({
     communityName,
     lastActive,
     coverPhoto,
     isPrivate,
-    roleID
+    roleID,
+    communityID
+
 }) => {
     return (
-        <Box _hover={{ backgroundColor: "gray.100", cursor: "pointer" }} borderRadius="md" backgroundColor="gray.200" mt={2}>
-            <Box p={2} borderRadius="md">
-                <HStack gap={2}>
-                    <Image ml={1} borderRadius="md" boxSize="55px" src={coverPhoto} alt="Cover Photo" />
-                    <div>
-                        <Box display="flex" alignItems="center" gap={1}>
-                            {isPrivate ? <MdPublicOff /> : <MdPublic />}
-                            <Text as="b" fontSize="sm">
-                                {communityName}
-                            </Text>
-                        </Box>
-                        <Text fontSize="sm">Last active {lastActive} days ago</Text>
-                    </div>
-                </HStack>
+        <Link to={`/groups/id/${communityID}`}>
+            <Box _hover={{ backgroundColor: "gray.100", cursor: "pointer" }} borderRadius="md" backgroundColor="gray.200" mt={2}>
+                <Box p={2} borderRadius="md">
+                    <HStack gap={2}>
+                        <Image ml={1} borderRadius="md" boxSize="55px" src={coverPhoto} alt="Cover Photo" />
+                        <div>
+                            <Box display="flex" alignItems="center" gap={1}>
+                                {isPrivate ? <MdPublicOff /> : <MdPublic />}
+                                <Text as="b" fontSize="sm">
+                                    {communityName}
+                                </Text>
+                            </Box>
+                            <Text fontSize="sm">Last active {lastActive} days ago</Text>
+                        </div>
+                    </HStack>
+                </Box>
             </Box>
-        </Box>
+        </Link>
     )
 }
 
