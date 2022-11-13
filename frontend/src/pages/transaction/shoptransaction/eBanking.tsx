31f91da3013bbf4f3ab77e87d441ee4eeb2e2a18
bmfelis <91175673+bmfelis@users.noreply.github.com>
2022-11-14 00:14:31 +0700

    Revert "Revert "Merge branch 'transaction' of https://github.com/CSSIT22/studentlife into transaction""
    This reverts commit d4b75a9d2d675828de291ee23cfabd4473c610c9.

---
+++ frontend/src/pages/transaction/shoptransaction/eBanking.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const eBanking = () => {
+    return <div>eBanking</div>
+}
+
+export default eBanking
---
+++ frontend/src/pages/transaction/shoptransaction/qrCode.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const qrCode = () => {
+    return <div>qrCode</div>
+}
+
+export default qrCode
