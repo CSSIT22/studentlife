d9fa64733e8626f77eb6b4e65c419eff549cef79
Passakorn <boss4848988@gmail.com>
2022-11-13 12:00:48 +0700

    [CREATE] search bar on the file page

--- frontend/src/pages/groups/id/[communityID]/file.tsx
+++ frontend/src/pages/groups/id/[communityID]/file.tsx
@@ -1,12 +1,16 @@
-import { Flex, Heading, Button, Box, Text } from '@chakra-ui/react'
-import React from 'react'
+import { Flex, Heading, Button, Box, Text, HStack, Input } from '@chakra-ui/react'
+import React, { useState } from 'react'
 import NavCommunity from 'src/components/group/NavCommunity'
 import AppBody from 'src/components/share/app/AppBody'
 import { BrowserRouter, BrowserRouter as Router, useParams, Link } from "react-router-dom";
 import { userData } from '../../data';
+import { FaSearch } from 'react-icons/fa';
 
 const file = () => {
     let { communityID }: any = useParams<{ communityID: string }>()
+    const [searchValue, setSearchValue] = useState("") //for store search value
+    const handleChange = (event: any) => setSearchValue(event.target.value)
+
     // const tags = [{ tagID: 1, tagName: "#Sport" }, { tagID: 2, tagName: "#Music" }, { tagID: 3, tagName: "#Gaming" }]
     return <AppBody>
         <NavCommunity
@@ -19,7 +23,18 @@ const file = () => {
             communityID={1000}
             tags={userData.Tag}
             activeBtn={3}
-        /><Text>file</Text>
+        /><HStack justify={"space-between"} borderRadius={"md"} p={3} pl={4} pr={4} backgroundColor={"gray.300"}><Text as={"b"}>File</Text>
+            <HStack justify={"flex-end"} width={"100%"}><Input
+                // width={"100%"}
+                // display={searchBtn ? "" : "none"}
+                variant={"filled"}
+                maxWidth={"200px"}
+                type={"search"}
+                value={searchValue}
+                onChange={handleChange}
+                placeholder="Seacrh File"
+                focusBorderColor="gray.200"
+            ></Input><Button>Upload</Button></HStack></HStack>
     </AppBody>
 
 }
