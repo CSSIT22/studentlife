9b55171d34ed769e57442a2ec064e9335bb5b49c
parnlees <yanisa0439@gmail.com>
2022-11-13 18:43:29 +0700

    next button for edittash

--- frontend/src/pages/todolist/EditTask.tsx
+++ frontend/src/pages/todolist/EditTask.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import {
     Icon,
     IconButton,
@@ -30,14 +30,19 @@ import AppBody from "src/components/share/app/AppBody"
 import ToDoListAppBody from "src/components/todolist/ToDoListAppBody"
 import { ArrowBackIcon } from "@chakra-ui/icons"
 
+// const edittask = () => {
+//     const { isOpen: isBackOpen, onOpen: onBackOpen, onClose: onBackClose } = useDisclosure()
+
 const edittask = () => {
-    const { isOpen: isBackOpen, onOpen: onBackOpen, onClose: onBackClose } = useDisclosure()
+    const [type, setType] = useState("individual")
+    console.log(type)
 
     return (
         <ToDoListAppBody>
             <Heading as="h2" size="3xl" noOfLines={1}>
                 <Box display="flex" justifyContent="space-between" alignItems="center">
-                    <Button bgColor="orange.200" onClick={onBackOpen}>
+                    <Button bgColor="orange.200">
+                        {/* onClick={onBackOpen} */}
                         <Link href="/todolist">
                             <ArrowBackIcon />
                         </Link>
@@ -66,12 +71,19 @@ const edittask = () => {
                 </Heading>
                 <Text fontSize="sm">Time of Task.</Text>
 
-                <Heading as="h2" size="md" noOfLines={1} marginY={10}>
+                {/* <Heading as="h2" size="md" noOfLines={1} marginY={10}>
                     Type
                 </Heading>
                 <Select placeholder="Choose" size="md">
                     <option value="option1">Individual</option>
                     <option value="option2">Group</option>
+                </Select> */}
+                <Heading className="Type" as="h2" size="md" noOfLines={1} mt={8} mb={2}>
+                    Type
+                </Heading>
+                <Select placeholder="Choose" size="md" className="Type" onChange={(e) => setType(e.target.value)}>
+                    <option value="individual">Individual</option>
+                    <option value="group">Group</option>
                 </Select>
 
                 <Heading as="h2" size="md" noOfLines={1} marginY={10}>
@@ -83,13 +95,26 @@ const edittask = () => {
                     <option value="option1">7 days before due date</option>
                 </Select>
 
-                <Box display="flex" justifyContent="center" alignItems="center" marginY={10}>
+                {/* <Box display="flex" justifyContent="center" alignItems="center" marginY={10}>
                     <Button colorScheme="teal" size="lg" bgColor={"orange.500"}>
                         <Link href="/todolist">Finish</Link>
                     </Button>
+                </Box> */}
+
+                <Box display="flex" justifyContent="center" alignItems="center" marginY={10}>
+                    {type == "individual" ? (
+                        <Button colorScheme="teal" size="lg" bgColor={"orange.500"}>
+                            <Link href="/todolist">Done</Link>
+                        </Button>
+                    ) : (
+                        <Button colorScheme="teal" size="lg" bgColor={"orange.500"}>
+                            Next
+                        </Button>
+                    )}
                 </Box>
             </Box>
         </ToDoListAppBody>
     )
+    // }
 }
 export default edittask
