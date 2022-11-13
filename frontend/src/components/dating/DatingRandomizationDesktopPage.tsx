7a3d0dcd15203ba8a9622127c24a9f6c054708a7
Pawin Nakvisai <pawinnak2@gmail.com>
2022-11-13 14:37:54 +0700

    remove randomize desktop and mobile page component

--- frontend/src/components/dating/DatingRandomizationDesktopPage.tsx
+++
@@ -1,7 +0,0 @@
-import DatingAppBody from "./DatingAppBody"
-
-const DatingRandomizationDesktopPage = () => {
-    return <DatingAppBody>Randomization (Desktop)</DatingAppBody>
-}
-
-export default DatingRandomizationDesktopPage
