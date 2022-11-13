8dced724f0279e5b91a1e80130ab9cb37253f12d
Passakorn <boss4848988@gmail.com>
2022-11-13 13:23:02 +0700

    [ADD] file list
    not done yet

--- frontend/src/pages/groups/id/[communityID]/file.tsx
+++ frontend/src/pages/groups/id/[communityID]/file.tsx
@@ -1,11 +1,13 @@
-import { Flex, Heading, Button, Box, Text, HStack, Input } from '@chakra-ui/react'
+import { Flex, Heading, VStack, Button, Box, Text, HStack, Input, Popover, PopoverBody, PopoverContent, PopoverFooter, PopoverHeader, PopoverTrigger, Portal } from '@chakra-ui/react'
 import React, { useState } from 'react'
 import NavCommunity from 'src/components/group/NavCommunity'
 import AppBody from 'src/components/share/app/AppBody'
 import { BrowserRouter, BrowserRouter as Router, useParams, Link } from "react-router-dom";
 import { userData } from '../../data';
-import { FaSearch } from 'react-icons/fa';
-
+import { FaDownload, FaBan, FaExclamationCircle, FaHandMiddleFinger, FaSearch, FaUser, FaUserLock, FaUserShield } from 'react-icons/fa';
+import FriendInviteList from 'src/components/group/FriendInviteList';
+import { BsFillFileEarmarkTextFill, BsThreeDots } from "react-icons/bs"
+import { RiDeleteBinFill } from "react-icons/ri"
 const file = () => {
     let { communityID }: any = useParams<{ communityID: string }>()
     const [searchValue, setSearchValue] = useState("") //for store search value
@@ -35,7 +37,47 @@ const file = () => {
                 placeholder="Seacrh File"
                 focusBorderColor="gray.200"
             ></Input><Button>Upload</Button></HStack></HStack>
-    </AppBody>
+        <Box mt={2} borderRadius={"md"} gap={2} backgroundColor={"gray.300"} p={3} pl={4} pr={4}>
+            <Flex direction="row" >
+                <Text as="b">File name</Text>
+            </Flex>
+            <Flex direction={"row"} justify="space-between" alignItems={"center"} mt={2}  >
+                <Flex direction="row" gap={{ base: 3, sm: 2 }} alignItems={"center"}>
+                    <BsFillFileEarmarkTextFill size={"30px"} />
+                    <Flex alignItems={"center"} gap={{ md: 2 }} direction={{ sm: "column", md: "row" }}>
+                        <Text as={"b"}>File</Text>
+                        <Flex alignItems={"center"} direction={{ base: "column", sm: "row" }} gap={{ base: 0, sm: 1 }}>
+                            <Text fontSize={"sm"}>Passakorn Puttama</Text>
+                            <Text display={{ base: "none", sm: "block" }}>·</Text>
+                            <Text display={{ base: "none", sm: "block" }} fontSize={"sm"}>March 2, 2021 at 1:53 PM</Text>
+                        </Flex>
+                    </Flex>
+                </Flex>
+                <Popover>
+                    <PopoverTrigger>
+                        <Box _hover={{ cursor: "pointer" }} p={2} borderRadius="md">
+                            <BsThreeDots fontSize={"25px"} />
+                        </Box>
+                    </PopoverTrigger>
+                    <Portal>
+                        <PopoverContent width="180px">
+                            <PopoverBody>
+                                <Box gap={1} _hover={{ cursor: "pointer" }} display="flex" alignItems={"center"}>
+                                    <FaDownload />
+                                    <Text>Download</Text>
+                                </Box>
+                                <Box gap={1} _hover={{ cursor: "pointer" }} display="flex" alignItems={"center"}>
+                                    <RiDeleteBinFill />
+                                    <Text>Delete</Text>
+                                </Box>
+
+                            </PopoverBody>
+                        </PopoverContent>
+                    </Portal>
+                </Popover>
+            </Flex>
+        </Box>
+    </AppBody >
 
 }
 
