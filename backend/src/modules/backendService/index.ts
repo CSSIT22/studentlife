ce27184be75bf4f10d1772d83c855afed9d46a86
sora-k46 <sorrawit456789@gmail.com>
2022-11-13 18:01:09 +0700

    Create GET /tokens route

--- backend/src/modules/backendService/index.ts
+++ backend/src/modules/backendService/index.ts
@@ -1,5 +1,10 @@
+import { Request, Response } from "express"
 import express from "express"
+import { verifyUser } from "./middleware/verifyUser"
 
 const backendserviceRoutes = express()
 
+backendserviceRoutes.get("/tokens", verifyUser, (req: Request, res: Response) => {
+    res.send("This route send user's tokens info")
+})
 export default backendserviceRoutes
