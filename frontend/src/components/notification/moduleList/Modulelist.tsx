40183844c60ac2ff9e0a8f5ba79a0d7fca136953
BbenHhan <benyalak.hanp@kmutt.ac.th>
2022-11-13 19:47:42 +0700

    Update color Modulelist

--- frontend/src/components/notification/moduleList/Modulelist.tsx
+++ frontend/src/components/notification/moduleList/Modulelist.tsx
@@ -8,12 +8,22 @@ const Modulelist: FC<{ onClick: Function; selectedModule: string }> = ({ onClick
             <MenuButton color={"orange.500"} size="sm" as={Button} bg={"transparent"}>
                 {selectedModule}
             </MenuButton>
-            <MenuList>
-                <Box height="30vh" overflow="auto">
+            <MenuList background={"#E69C73"}>
+                {/* <Box as="button"
+            bg={"white"}
+            height="100%" 
+            overflow="auto"
+            _hover={{ bg: "#E69C73", transitionDuration: "0.2s" }}
+            transitionDuration="0.2s"
+            borderRadius="2xl"> */}
+                <Box w="100%" height="100%" overflow="auto" background={"#E69C73"}>
                     <Stack direction={"column"} padding={4}>
                         {MODULES.map((el) => {
                             return (
-                                <MenuItem onClick={() => onClick(el.name)} key={el.id}>
+                                <MenuItem onClick={() => onClick(el.name)} 
+                                key={el.id} 
+                                bg={"white"} 
+                                as={Button}>
                                     {el.name}
                                 </MenuItem>
                             )
