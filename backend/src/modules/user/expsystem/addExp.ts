e05fcd0d692ba54358ddf536746de534311889e6
Pattarapon Koomphon <45892157+Kevinindy2k@users.noreply.github.com>
2022-11-13 18:02:15 +0700

    Remove AddExp and CalExp From Router

--- backend/src/modules/user/expsystem/addExp.ts
+++ backend/src/modules/user/expsystem/addExp.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express"
 
-const getExp = async (req: Request, res: Response) => {
+const addExp = async (req: Request, res: Response) => {
     try {
     } catch (err) {
         res.status(400).send("Error To Get Your Exp")
     }
 }
 
-export default getExp
+export default addExp
