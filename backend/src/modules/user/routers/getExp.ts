5b981f85a81b246d536b94b030d68f59019976b9
Pattarapon Koomphon <45892157+Kevinindy2k@users.noreply.github.com>
2022-11-13 18:56:13 +0700

    GetExp 100%

--- backend/src/modules/user/routers/getExp.ts
+++ backend/src/modules/user/routers/getExp.ts
@@ -2,6 +2,10 @@ import { Request, Response } from "express"
 
 const getExp = async (req: Request, res: Response) => {
     try {
+        const { prisma } = res
+        const userId = req.params["id"]
+        const exp = await prisma.eXP.findFirstOrThrow({ where: { userId }, select: { currentXP: true } })
+        res.end(exp.currentXP)
     } catch (err) {
         res.status(400).send("Error To Get Your Exp")
     }
