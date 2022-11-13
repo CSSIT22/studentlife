966f54b30c9aec64b38e24e7425e3363b0377d7d
BbenHhan <benyalak.hanp@kmutt.ac.th>
2022-11-13 22:40:15 +0700

    Update Modulelist.tsx

--- frontend/src/components/notification/moduleList/Modulelist.tsx
+++ frontend/src/components/notification/moduleList/Modulelist.tsx
@@ -9,13 +9,6 @@ const Modulelist: FC<{ onClick: Function; selectedModule: string }> = ({ onClick
                 {selectedModule}
             </MenuButton>
             <MenuList background={"#E69C73"}>
-                {/* <Box as="button"
-            bg={"white"}
-            height="100%" 
-            overflow="auto"
-            _hover={{ bg: "#E69C73", transitionDuration: "0.2s" }}
-            transitionDuration="0.2s"
-            borderRadius="2xl"> */}
                 <Box w="100%" height="100%" overflow="auto" background={"#E69C73"}>
                     <Stack direction={"column"} padding={4}>
                         {MODULES.map((el) => {
