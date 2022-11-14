64a1e1b1e821d513359836a30cef2dcb184d9885
Passakorn <boss4848988@gmail.com>
2022-11-14 20:42:34 +0700

    [CREATE] edit page

---
+++ frontend/src/pages/groups/id/[communityID]/edit/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AppBody from 'src/components/share/app/AppBody'
+
+const communityEdit = () => {
+    return (
+        <AppBody>
+            edit
+        </AppBody>
+    )
+}
+
+export default communityEdit
