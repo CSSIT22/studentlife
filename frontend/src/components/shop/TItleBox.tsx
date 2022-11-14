653933a7139423778e6f2110a278e53b752a2e8b
Khush Agarwal <khush.agar@kmutt.ac.th>
2022-11-14 10:25:10 +0700

    Added option to change shadow

--- frontend/src/components/shop/TItleBox.tsx
+++ frontend/src/components/shop/TItleBox.tsx
@@ -3,9 +3,10 @@ import React, { FC } from "react"
 
 const TitleBox: FC<{
     title: string
-}> = ({ title }) => {
+    sh?: string
+}> = ({ title, sh }) => {
     return (
-        <Box bg="#f1f1f1" borderRadius="lg" shadow="md">
+        <Box bg="#fff" borderRadius="lg" shadow={sh? sh : "lg"} border="#Fafafa solid 1px">
             <Text px="5" py="1.5"  fontWeight="bold" fontSize="lg"> {title} </Text>
         </Box>
     )
