5a9d458d7fff8285b340a5d56af1d95a6a6e5b16
Pattarapon Koomphon <45892157+Kevinindy2k@users.noreply.github.com>
2022-11-15 21:04:13 +0700

    Update CalExp And GetExp

--- backend/src/modules/user/routers/getExp.ts
+++ backend/src/modules/user/routers/getExp.ts
@@ -1,15 +1,28 @@
 import { Request, Response } from "express"
+import calExp from "../expsystem/calExp"
 // import calExp from "../expsystem/calexp"
 
 const getExp = async (req: Request, res: Response) => {
     try {
         const { prisma } = res
         const userId = req.user?.userId || ""
+        const checkexp = await prisma.eXP.findFirst({ where: { userId } })
+        // calExp(prisma, req.user?.userId || "", "chatAddfriend")
+        if (checkexp === null) {
+            await prisma.eXP.create({
+                data: {
+                    currentXP: 0,
+                    level: 1,
+                    userId: userId,
+                },
+            })
+        }
 
         const exp = await prisma.eXP.findFirstOrThrow({ where: { userId }, select: { currentXP: true } })
         res.json({
             exp: exp.currentXP,
         })
+        console.log(exp)
     } catch (err) {
         res.status(400).send("Error To Get Your CurrentExp")
     }
