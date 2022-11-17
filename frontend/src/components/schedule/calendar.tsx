fe890abd74379bfdaf1bf646bbe6b5d36efea4fc
Dream_Winanya <winanya2545@gmail.com>
2022-11-17 09:33:08 +0700

    [Update] Test calendar npm

--- frontend/src/components/schedule/calendar.tsx
+++ frontend/src/components/schedule/calendar.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+// import React from 'react'
 
-const calendar = () => {
-  return (
-    <div>calendar</div>
-  )
+// const calendar = () => {
+//   return (
+//     <div>calendar</div>
+//   )
+// }
+
+import React, { useState } from "react"
+import Calendar from "react-calendar"
+
+function calendar() {
+    const [value, onChange] = useState(new Date())
+
+    return (
+        <div>
+            <Calendar onChange={onChange} value={value} />
+        </div>
+    )
 }
-
 export default calendar
