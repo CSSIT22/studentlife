be747cb22933edeff3837bd31338377d44127aea
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2022-11-17 17:02:30 +0700

    [UPDATE] detail page (select lang of post)

--- frontend/src/pages/announcement/detail/[postId].tsx
+++ frontend/src/pages/announcement/detail/[postId].tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Show, Spacer, Stack, Text } from "@chakra-ui/react"
+import { Box, Flex, Grid, GridItem, Heading, Select, Show, Spacer, Stack, Text } from "@chakra-ui/react"
 import React from "react"
 import { GrClose } from "react-icons/gr"
 import { Link, useParams } from "react-router-dom"
@@ -24,6 +24,7 @@ const detail = () => {
             ]}
             p={{ md: "3rem" }}
         >
+           
             <Flex alignItems={"center"}>
                 <Show below="lg">
                     <Text as={"b"} fontSize="xl">
@@ -34,7 +35,16 @@ const detail = () => {
                 </Show>
                 {/* <Spacer /> */}
             </Flex>
+          
             <Stack spacing={3} p="5">
+            <Grid templateColumns={{base:"1fr 1fr",lg:"1fr 3fr"}} my={5}>
+                <GridItem>
+                    <Select placeholder="select language" bg="blue.600" color="white">
+                        <option>Korea</option>
+                        <option>Japanes</option>
+                    </Select>
+                </GridItem>
+            </Grid>
                 <Heading as="h2" size="xl">
                     {post.map((el) => {
                         return el.topic
