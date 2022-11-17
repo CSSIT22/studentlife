f50474f64570dccb0390ea644b0a494cf59ba516
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2022-11-18 00:33:59 +0700

    [UPDATE] route getPostOnAnnouncement

--- backend/src/modules/announcement/index.ts
+++ backend/src/modules/announcement/index.ts
@@ -1,5 +1,91 @@
+import { verifyUser } from './../backendService/middleware/verifyUser';
+import { post } from "./../../../../types/announcement/index"
 import express from "express"
+import { getHeapCodeStatistics } from "v8"
 
 const announcementRoutes = express()
+
+announcementRoutes.use(express.json())
+
+export let posts: post[] = [
+    {
+        postId: 0,
+        lang_id: 1000,
+        topic: "Hello World",
+        detail: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        sender: "SAMO-SIT",
+        status: "approve",
+        pinStatus: false,
+        isApprove: true,
+        targetType: "Year",
+        targetValue: "1",
+        postAt: new Date(),
+        expiredOfPost: new Date("2022-11-30"),
+        expiredAfterDelete: new Date(""),
+        addMoreLang: [
+            { id: 0, lang_id: 1001, topic: "สวัสดีชาวโลก", detail: "สวัสดีฉันชื่อ modlifes" },
+            { id: 1, lang_id: 1002, topic: "unnyeong", detail: "Korean Kimchi" },
+        ],
+    },
+    {
+        postId: 1,
+        lang_id: 1000,
+        topic: "Hello World1",
+        detail: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        sender: "SAMO-SIT",
+        status: "waiting",
+        pinStatus: false,
+        isApprove: false,
+        targetType: "Year",
+        targetValue: "1",
+        postAt: new Date(),
+        expiredOfPost: new Date("2022-11-10"),
+        expiredAfterDelete: new Date(""),
+        addMoreLang: [],
+    },
+    {
+        postId: 2,
+        lang_id: 1000,
+        topic: "Hello World2",
+        detail: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        sender: "SAMO-SIT",
+        status: "waiting",
+        pinStatus: false,
+        isApprove: true,
+        targetType: "Year",
+        targetValue: "1",
+        postAt: new Date(),
+        expiredOfPost: new Date("2022-11-30"),
+        expiredAfterDelete: new Date(""),
+        addMoreLang: [],
+    },
+]
+
+export const getPost = () => {
+    return posts
+}
+export const setPost = (newData: post[]) => {
+    posts = newData
+}
+
+announcementRoutes.get("/getPostOnAnnouncement", verifyUser,(req, res) => {
+    const minute = 1000 * 60
+    const hour = minute * 60
+    const day = hour * 24
+    const date = new Date()
+    const currentD = Math.round(date.getTime() / day)
+    let selectpost: post[] = []
+    getPost().forEach((post) => {
+        if (post.isApprove == true) {
+            const expired = new Date(post.expiredOfPost)
+            const expiredPost = Math.round(expired.getTime() / day)
+            const diff = expiredPost - currentD
+            if (diff > 0) {
+                selectpost.push(post)
+            }
+        } 
+    })
+    res.send(selectpost)
+})
 
 export default announcementRoutes
