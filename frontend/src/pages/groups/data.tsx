ad3357a5dbd74573a9dbeb05b0c795376874066b
Dolly <patthadol43@gmail.com>
2022-11-17 14:20:01 +0700

    Add some new invite lists

--- frontend/src/pages/groups/data.tsx
+++ frontend/src/pages/groups/data.tsx
@@ -149,7 +149,38 @@ export const userData = {
 
 
         },
+        {
+            ID: 8,
+            userName: "Patthadol Raksapram",
+            profile: "https://scontent.fbkk5-3.fna.fbcdn.net/v/t1.6435-9/157338507_1846511502191185_4895186676389840729_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEWYeuHzFxGPBEaRu6NHsD2-9RSc4ypjeP71FJzjKmN44nsCjqztbaYtPl-GpFuybuh7Oxx7ZwLBv0_qw4OZn7k&_nc_ohc=kGJaaq9f5IMAX95XfxF&_nc_ht=scontent.fbkk5-3.fna&oh=00_AfDmapktgPqRRADyyauISECJrMKis28M5TL46S7yS8PAWA&oe=639D58AC",
+            isSelected: false,
 
+
+        },
+        {
+            ID: 9,
+            userName: "Vatcharamai Rodring",
+            profile: "https://scontent.fbkk5-3.fna.fbcdn.net/v/t1.18169-9/10447621_604965639627003_3069778378348126097_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=cdbe9c&_nc_eui2=AeGBmv_dzprhdwCAHqxk4oLt_2PyJJzwmGb_Y_IknPCYZq8z4qhqWbH_719-cs36-gRapaf9-D2RbNTlm0J1zS89&_nc_ohc=FjuNkIEMOIwAX9RvLkN&_nc_ht=scontent.fbkk5-3.fna&oh=00_AfD94Wfyi6CYSXcmf3GbZRMVusB-Vx1g9ZzbNHRNauBvHA&oe=639D57CB",
+            isSelected: false,
+
+
+        },
+        {
+            ID: 10,
+            userName: "Pakkawat Wassa",
+            profile: "https://scontent.fbkk5-4.fna.fbcdn.net/v/t39.30808-6/274859505_3166628116958449_1089440485493579767_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGsnAw-AeUjyRzZH-RY_FbD-7Tb6723loL7tNvrvbeWgpsrGMaVNPwDjnJqdH8tU_dWkW0o_hxnprssPoIJS9xD&_nc_ohc=5aASsnCrgF8AX-kTKiM&tn=wOlhBGkPCuwHhJFq&_nc_ht=scontent.fbkk5-4.fna&oh=00_AfC9ovxpAKutd4I6vtRW--MR7sYvVI7w-YPlVni6nkFeDw&oe=637B5307",
+            isSelected: false,
+
+
+        },
+        {
+            ID: 11,
+            userName: "Puvadech Niyomdaycha",
+            profile: "https://scontent.fbkk5-4.fna.fbcdn.net/v/t39.30808-6/240729477_4123958257714617_152558809044861253_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGencwhgyYplFGrOns7k4K0d8X884TiHjZ3xfzzhOIeNornBBHgQCMRDuyO0zjAg3f6hQhkQmKSysWQPSE34LWZ&_nc_ohc=S2BQ9uLZDPEAX-_bzNp&_nc_ht=scontent.fbkk5-4.fna&oh=00_AfDb5bMk_1oSICjq4uFzhyGGxtcfHVJ_ZInT3siB42Dbcw&oe=637A19A0",
+            isSelected: false,
+
+
+        },
         {
             ID: 2,
             userName: "Winai Pattaya",
@@ -184,7 +215,7 @@ export const userData = {
         },
         {
             ID: 6,
-            userName: "Tomson ooi",
+            userName: "Tomson Ooi",
             profile: "https://picsum.photos/id/65/200/300",
             isSelected: false,
 
