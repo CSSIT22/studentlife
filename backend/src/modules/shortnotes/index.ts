87b0221722706315d0cb49ffb9ddb12ea4e968dd
Pasinun.w <95624019+Unuisad@users.noreply.github.com>
2022-11-17 16:22:57 +0700

    [ADD] routes

--- backend/src/modules/shortnotes/index.ts
+++ backend/src/modules/shortnotes/index.ts
@@ -1,5 +1,8 @@
 import express from "express"
-
 const shortnotesRoutes = express()
+
+shortnotesRoutes.get("/getShortnotes", (req, res) => {
+    res.send("data")
+})
 
 export default shortnotesRoutes
