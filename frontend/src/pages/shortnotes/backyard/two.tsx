3a97371d1a76b9b7859d5e323d850e3a8cede820
Pasinun.w <95624019+Unuisad@users.noreply.github.com>
2022-11-19 00:21:09 +0700

    [ADD] test getShortnotes API

--- frontend/src/pages/shortnotes/backyard/two.tsx
+++ frontend/src/pages/shortnotes/backyard/two.tsx
@@ -6,13 +6,21 @@ import AppBody from "src/components/share/app/AppBody"
 import API from "src/function/API"
 
 const pg = () => {
+    const [sn, setSn] = useState([])
     useEffect(() => {
-        API.get("/shortnotes/getShortnotes").then((res) => {
-            console.log(res.data)
+        API.get("/shortnotes/getShortnotes").then((item) => {
+            setSn(item.data)
+            console.log(item.data)
         })
     }, [])
 
-    return <AppBody></AppBody>
+    return (
+        <AppBody>
+            {sn.map((sn: any, key) => (
+                <Box key={key}>{sn.topic}</Box>
+            ))}
+        </AppBody>
+    )
 }
 
 export default pg
