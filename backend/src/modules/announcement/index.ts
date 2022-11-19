5f4cada0c154effccef2497e97b5f1699a64927b
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2022-11-19 13:12:14 +0700

    [UPDATE] front-approvalDetail back-index

--- backend/src/modules/announcement/index.ts
+++ backend/src/modules/announcement/index.ts
@@ -8,6 +8,7 @@ import getWaitingPost from "./routes/getwaitingpost"
 import getDeletePost from "./routes/getdeletepost"
 import editPinStatus from "./routes/editpinstatus"
 import getDetailApprove from "./routes/getdetailapprove"
+import editstatusOnApprovel from "./routes/editstatusonapproval"
 
 const announcementRoutes = express()
 
@@ -53,7 +54,7 @@ export let posts: post[] = [
     },
     {
         postId: 2,
-        userId: "03",
+        userId: "02",
         lang_id: 1000,
         topic: "Hello World2",
         detail: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
@@ -83,6 +84,56 @@ export let posts: post[] = [
         postAt: new Date(),
         expiredOfPost: new Date("2022-11-30"),
         expiredAfterDelete: new Date("2022-11-20"),
+        addMoreLang: [],
+    },{
+        postId: 4,
+        userId:"05",
+        lang_id: 1000,
+        topic: "Hello World4",
+        detail: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        sender: "SAMO-SIT",
+        status: "approve",
+        pinStatus: false,
+        isApprove: true,
+        targetType: "Year",
+        targetValue: "1",
+        postAt: new Date(),
+        expiredOfPost: new Date("2022-11-30"),
+        expiredAfterDelete: new Date(""),
+        addMoreLang: [],
+    },
+    {
+        postId: 5,
+        userId:"06",
+        lang_id: 1000,
+        topic: "Hello World5",
+        detail: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        sender: "SAMO-SIT",
+        status: "disapprove",
+        pinStatus: false,
+        isApprove: false,
+        targetType: "Year",
+        targetValue: "1",
+        postAt: new Date(),
+        expiredOfPost: new Date("2022-11-12"),
+        expiredAfterDelete: new Date(""),
+        addMoreLang: [],
+    },
+    {
+        postId: 6,
+        userId:"07",
+        lang_id: 1000,
+        topic: "Hello World6",
+        detail: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        sender: "SAMO-SIT",
+        status: "delete",
+        pinStatus: false,
+        isApprove: true,
+        targetType: "Year",
+        targetValue: "1",
+        postAt: new Date(),
+        expiredOfPost: new Date("2022-11-24"),
+        expiredAfterDelete: new Date("2022-11-28"),
         addMoreLang: [],
     },
 ]
@@ -128,5 +179,23 @@ announcementRoutes.get("/getdeletepost", getDeletePost)
 announcementRoutes.post("/editpinstatus",editPinStatus)
 
 announcementRoutes.get("/getdetailapprove/:id", getDetailApprove)
+
+announcementRoutes.post("/editstatusonapprove", editstatusOnApprovel)
+
+announcementRoutes.get("/test", async (req,res) =>{
+    const prisma = res.prisma
+    try {
+        const res = await prisma.user_Profile.findUnique({
+            where: {
+                userId: req.user?.userId || ""
+            }
+        })
+        console.log(res)
+    } catch (err: any) {
+        console.log(err)
+        res.status(500)
+    }
+    res.send(req.user)
+})
 
 export default announcementRoutes
