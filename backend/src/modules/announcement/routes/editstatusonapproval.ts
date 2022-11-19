3e89493df1921ad5e72df1f0c92d3c9f4a3cafb0
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2022-11-19 13:11:26 +0700

    [ADD] route editstatusonapproval

--- backend/src/modules/announcement/routes/editstatusonapproval.ts
+++ backend/src/modules/announcement/routes/editstatusonapproval.ts
@@ -1,7 +1,23 @@
+import { post } from "@apiType/announcement";
 import { Request, Response } from "express";
+import { getPost, setPost } from "..";
 
 const editstatusOnApprovel = (req:Request,res:Response) =>{
-
+    const postId = req.body.postId
+    const status = req.body.status
+    const isapprove = req.body.isapprove
+    let editstatusA:post | null = null
+    const newData = getPost().map((post) => {
+        if(post.postId == postId){
+            post.status = status
+            post.isApprove = isapprove
+            editstatusA = post
+        }
+        return post
+    })
+    setPost(newData)
+    console.log(newData);
+    res.send(editstatusA);
 }
 
 export default editstatusOnApprovel;
