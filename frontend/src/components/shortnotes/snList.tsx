6e9876461ed3fd01550c7ad0f10d995b9579e394
Pasinun.w <95624019+Unuisad@users.noreply.github.com>
2022-11-20 04:18:39 +0700

    [FIX] align Sn_name to start when text too long

--- frontend/src/components/shortnotes/snList.tsx
+++ frontend/src/components/shortnotes/snList.tsx
@@ -11,7 +11,9 @@ const snList: FC<{
         <Box boxShadow={"md"} bg={"white"} rounded={8} p={5} w="100%">
             <Grid templateColumns="repeat(9, 1fr)">
                 <GridItem colSpan={4}>
-                    <Text textAlign={"left"}>{topic}</Text>
+                    <Flex alignItems={"center"} justifyContent={"start"} h={"100%"}>
+                        <Text>{topic}</Text>
+                    </Flex>
                 </GridItem>
                 <GridItem colSpan={2}>
                     <Flex alignItems={"center"} justifyContent={"center"} h={"100%"}>
