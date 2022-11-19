665940703aee4e73ddfd7b3f47915559604664c6
Pasinun.w <95624019+Unuisad@users.noreply.github.com>
2022-11-20 04:21:38 +0700

    [ADD] comments scripts for call data from DB

--- backend/src/modules/shortnotes/routes/getCourses.ts
+++ backend/src/modules/shortnotes/routes/getCourses.ts
@@ -1,8 +1,12 @@
 import { Request, Response } from "express"
+import { prisma } from "@prisma/client"
 import { course } from ".."
 
-const getCourses = (req: Request, res: Response) => {
+const getCourses = async (req: Request, res: Response) => {
+    //const prisma = res.prisma
+    //const course = await prisma.course.findMany()
     res.send(course)
+    console.log(course)
 }
 
 export default getCourses
--- backend/src/modules/shortnotes/routes/getResentShortnotes.ts
+++ backend/src/modules/shortnotes/routes/getResentShortnotes.ts
@@ -1,7 +1,15 @@
 import { Request, Response } from "express"
 import { rsn } from ".."
 
-const getResentShortnotes = (req: Request, res: Response) => {
+const getResentShortnotes = async (req: Request, res: Response) => {
+    // const prisma = res.prisma
+    // const user = req.user?.userId
+    // const rsn:any = await prisma.sn_Recent.findMany({
+    //     include:{
+    //         userId: user,
+    //     },
+    // })
+    // console.log(rsn)
     res.send(rsn)
 }
 
