317dbe08bbde01727105f8b3746f415695d242aa
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2022-11-19 14:38:21 +0700

    [UPDATE] front-ModalForEvent, history back-index

--- backend/src/modules/announcement/index.ts
+++ backend/src/modules/announcement/index.ts
@@ -8,7 +8,7 @@ import getWaitingPost from "./routes/getwaitingpost"
 import getDeletePost from "./routes/getdeletepost"
 import editPinStatus from "./routes/editpinstatus"
 import getDetailApprove from "./routes/getdetailapprove"
-import editstatusOnApprovel from "./routes/editstatusonapproval"
+import editstatusOnApproval from "./routes/editstatusonapproval"
 
 const announcementRoutes = express()
 
@@ -20,7 +20,7 @@ export let posts: post[] = [
         userId: "01",
         lang_id: 1000,
         topic: "Hello World",
-        detail: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        detail: "yyyLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
         sender: "SAMO-SIT",
         status: "approve",
         pinStatus: false,
@@ -29,7 +29,7 @@ export let posts: post[] = [
         targetValue: "1",
         postAt: new Date(),
         expiredOfPost: new Date("2022-11-30"),
-        expiredAfterDelete: new Date(""),
+        expiredAfterDelete: new Date("2022-11-21"),
         addMoreLang: [
             { id: 0, lang_id: 1001, topic: "สวัสดีชาวโลก", detail: "สวัสดีฉันชื่อ modlifes" },
             { id: 1, lang_id: 1002, topic: "unnyeong", detail: "Korean Kimchi" },
@@ -40,7 +40,7 @@ export let posts: post[] = [
         userId: "02",
         lang_id: 1000,
         topic: "Hello World1",
-        detail: "yyy Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        detail: "Lrem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
         sender: "SAMO-SIT",
         status: "waiting",
         pinStatus: false,
@@ -49,7 +49,7 @@ export let posts: post[] = [
         targetValue: "1",
         postAt: new Date(),
         expiredOfPost: new Date("2022-11-10"),
-        expiredAfterDelete: new Date(""),
+        expiredAfterDelete: new Date("2022-11-21"),
         addMoreLang: [],
     },
     {
@@ -57,7 +57,7 @@ export let posts: post[] = [
         userId: "02",
         lang_id: 1000,
         topic: "Hello World2",
-        detail: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        detail: "yyyLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
         sender: "SAMO-SIT",
         status: "approve",
         pinStatus: false,
@@ -66,7 +66,7 @@ export let posts: post[] = [
         targetValue: "1",
         postAt: new Date(),
         expiredOfPost: new Date("2022-11-30"),
-        expiredAfterDelete: new Date(""),
+        expiredAfterDelete: new Date("2022-11-20"),
         addMoreLang: [],
     },
     {
@@ -99,7 +99,7 @@ export let posts: post[] = [
         targetValue: "1",
         postAt: new Date(),
         expiredOfPost: new Date("2022-11-30"),
-        expiredAfterDelete: new Date(""),
+        expiredAfterDelete: new Date("2022-11-21"),
         addMoreLang: [],
     },
     {
@@ -116,7 +116,7 @@ export let posts: post[] = [
         targetValue: "1",
         postAt: new Date(),
         expiredOfPost: new Date("2022-11-12"),
-        expiredAfterDelete: new Date(""),
+        expiredAfterDelete: new Date("2022-11-21"),
         addMoreLang: [],
     },
     {
@@ -133,7 +133,7 @@ export let posts: post[] = [
         targetValue: "1",
         postAt: new Date(),
         expiredOfPost: new Date("2022-11-24"),
-        expiredAfterDelete: new Date("2022-11-28"),
+        expiredAfterDelete: new Date("2022-11-20"),
         addMoreLang: [],
     },
 ]
@@ -180,22 +180,50 @@ announcementRoutes.post("/editpinstatus",editPinStatus)
 
 announcementRoutes.get("/getdetailapprove/:id", getDetailApprove)
 
-announcementRoutes.post("/editstatusonapprove", editstatusOnApprovel)
+announcementRoutes.post("/editstatusonapprove", editstatusOnApproval)
 
-announcementRoutes.get("/test", async (req,res) =>{
-    const prisma = res.prisma
-    try {
-        const res = await prisma.user_Profile.findUnique({
-            where: {
-                userId: req.user?.userId || ""
-            }
-        })
-        console.log(res)
-    } catch (err: any) {
-        console.log(err)
-        res.status(500)
-    }
-    res.send(req.user)
+announcementRoutes.post("/editstatusonhistory", (req,res) => {
+    const postId = req.body.postId
+    const status = req.body.status
+    const isapprove = req.body.isapprove
+    let editstatusH:post | null = null
+    const newData = getPost().map((post) => {
+        if(post.postId == postId){
+            post.status = status
+            post.isApprove = isapprove
+            editstatusH = post
+        }
+        return post
+    })
+    setPost(newData)
+    // console.log(newData);
+    res.send(editstatusH);
 })
+
+
+// announcementRoutes.get("/test", async (req,res) =>{
+//     const prisma = res.prisma
+//     try {
+//         const res = await prisma.user_Profile.findUnique({
+//             where: {
+//                 userId: req.user?.userId || ""
+//             }
+//         })
+//         const faculty = await prisma.major.findUnique({
+//             where: {
+//                 majorId: res?.majorId || ""
+//             },
+//             select: {
+//                 majorFaculty:true
+//             }
+//         })
+//         console.log(res)
+//         console.log(faculty)
+//     } catch (err: any) {
+//         console.log(err)
+//         res.status(500)
+//     }
+//     res.send(req.user)
+// })
 
 export default announcementRoutes
