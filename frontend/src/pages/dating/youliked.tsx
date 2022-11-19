67da06034197dd8150a0c0e06b330932d17672ce
IMFROHT <89932182+Tatathoraberd@users.noreply.github.com>
2022-11-19 22:45:09 +0700

    edit you like page

--- frontend/src/pages/dating/youliked.tsx
+++ frontend/src/pages/dating/youliked.tsx
@@ -54,24 +54,25 @@ const YouLiked = () => {
             <Box display="flex" justifyContent="center">
                 <Box bg="#FFF2E6" position="fixed" w="100%" justifyContent="space-between" top={{ base: 21, md: 157 }} id="bottomBar">
                     <Box maxW="100%" pt={{ base: "40px", md: "7px" }}></Box>
-                    <HStack
-                        gap={{ base: "20px", md: "100px" }}
-                        display="flex"
-                        justifyContent="center"
-                        pt={{ base: "40px", md: "30px" }}
-                        pb="30px"
-                    >
-                        <DatingLikedYouButton backgroundColor="orange.800" />
-                        <DatingYouLikedButton backgroundColor="orange.600" />
+                    <HStack gap={{ base: "20px", md: "100px" }} display="flex" justifyContent="center" pt={{ base: "40px", md: "30px" }} pb="30px">
+                        <DatingLikedYouButton backgroundColor="orange.600" />
+                        <DatingYouLikedButton backgroundColor="orange.800" />
                     </HStack>
                 </Box>
             </Box>
 
-            <Box display={{ base: "grid", md: "block" }} gridTemplateColumns="repeat(auto-fill, 200px)" gridGap="10px" justifyContent="center">
+            <Box
+                display={{ base: "grid", md: "block" }}
+                ml={{ base: "5px", md: "0px" }}
+                gridTemplateColumns="repeat(auto-fill, 165px)"
+                gridGap="10px"
+                justifyContent="center"
+                mt="120px"
+            >
                 {HState.heart_history
                     .filter((el) => !giveToUser?.some((f) => f.UserId == el.UserId))
                     .map(({ UserId, Fname, Lname, Gender, Age, Faculty, url, interestId }) => (
-                        <Box key={UserId} w={{ base: "159px", md: "100%" }} ml="10px" mr="10px">
+                        <Box key={UserId} w={{ base: "159px", md: "100%" }} ml={{ md: "10px" }} mr={{ md: "10px" }}>
                             <SimpleGrid display="flex" columns={{ base: 1, md: 2 }} gap="56px">
                                 <Box>
                                     <DatingCheckImage url={url} />
@@ -83,8 +84,8 @@ const YouLiked = () => {
                                         mt={{ base: "6px", md: "12px" }}
                                         mb={{ md: "12px" }}
                                     >
-                                        <DatingLikedYouCrossButton isMobile={isMobile} handleClick={handleClick} UserId={UserId} />
-                                        <DatingLikedYouHeartButton isMobile={isMobile} handleClick={handleClick} UserId={UserId} />
+                                        {/* <DatingLikedYouCrossButton isMobile={isMobile} handleClick={handleClick} UserId={UserId} />
+                                        <DatingLikedYouHeartButton isMobile={isMobile} handleClick={handleClick} UserId={UserId} /> */}
                                     </HStack>
                                 </Box>
                                 <DatingCheckDesktopDetails
