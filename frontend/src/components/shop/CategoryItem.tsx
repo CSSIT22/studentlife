9a112bbf524ecc4f873da687f8d892e2a186117c
ImUlyssus <98200468+ImUlyssus@users.noreply.github.com>
2022-11-19 23:43:39 +0700

    Updated Category UI

--- frontend/src/components/shop/CategoryItem.tsx
+++ frontend/src/components/shop/CategoryItem.tsx
@@ -15,15 +15,15 @@ export const CategoryItem: FC<{
         to = {link? link : "/shop/categories/categoryDetails"}
         state = {{cat_id: id, cat_name: name}}
          >
-        <Box bg={"white"} w="9rem" h="11rem" borderRadius="lg" rounded="md" shadow="md" position="relative" overflow={"hidden"}>
-            <Box pt= "5" px="5">
+        <Box bg={"white"} w="9rem" h="11rem" borderRadius="lg" rounded="md" shadow="md" position="relative" overflow={"hidden"} border='1px solid #E65300'>
+            <Box py= "2" px="5">
                 <Center >
-                    <Image src={image} alt={`Picture of ${name}`} h="7rem" w="9rem" p="0" objectFit="contain" borderRadius="50%"/>
+                    <Image src={image} alt={`Picture of ${name}`} h="7rem" w="9rem" p="0" objectFit="contain" borderRadius="50%" border='1px solid #E65300'/>
                 </Center>
             </Box>
-
+            <Box h="min" w="full" bg="#E65300" p="0.3"></Box>
             <Center pt=" 2" pb = "4" px="4">
-                <Box fontSize="lg" fontWeight="semibold" as="h4" alignContent={"center"} lineHeight="tight" noOfLines={ 1}>
+                <Box color='#E65300' fontSize="lg" fontWeight="semibold" as="h4" alignContent={"center"} lineHeight="tight" noOfLines={ 1}>
                    {name}
                 </Box>
             </Center>
