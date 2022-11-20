87adfef9f58396ddff0adf1f97a257e4a988786f
Pasinun.w <95624019+Unuisad@users.noreply.github.com>
2022-11-20 20:34:40 +0700

    [ADD] try catch

--- backend/src/modules/shortnotes/routes/postShortnote.ts
+++ backend/src/modules/shortnotes/routes/postShortnote.ts
@@ -2,22 +2,26 @@ import { prisma } from "@prisma/client"
 import { Request, Response } from "express"
 
 const postShortnote = async (req: Request<any>, res: Response<any>) => {
-    const prisma = res.prisma
-    const user = req.user?.userId
+    try {
+        const prisma = res.prisma
+        const user = req.user?.userId
 
-    const payload: any = {
-        snName: req.body.snName,
-        snDesc: req.body.snDesc,
-        snLink: req.body.snLink,
-        courseId: req.body.snName,
-        isPublic: req.body.isPublic,
-        userId: user,
+        const payload: any = {
+            snName: req.body.snName,
+            snDesc: req.body.snDesc,
+            snLink: req.body.snLink,
+            courseId: req.body.snName,
+            isPublic: req.body.isPublic,
+            userId: user,
+        }
+
+        const sn = await prisma.sn_Head.create({
+            data: payload,
+        })
+        res.send(sn)
+    } catch (err) {
+        res.send("some error")
     }
-
-    const sn = await prisma.sn_Head.create({
-        data: payload,
-    })
-    res.send(sn)
 }
 
 export default postShortnote
