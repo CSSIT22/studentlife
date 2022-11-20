f9c7976c9b3105f4b5913fff635d7442b3e4b37e
beamnaeieiza <98155226+beamnaeieiza@users.noreply.github.com>
2022-11-20 20:58:54 +0700

    cretaskba

--- frontend/src/pages/todolist/CreateTask.tsx
+++ frontend/src/pages/todolist/CreateTask.tsx
@@ -23,9 +23,38 @@ import {
     Text,
     Link,
 } from "@chakra-ui/react"
-import { BrowserRouter as Router, Route, Link as RouteLink } from "react-router-dom"
+import { BrowserRouter as Router, Route, Link as RouteLink, Form } from "react-router-dom"
 import AppBody from "src/components/share/app/AppBody"
 import ToDoListAppBody from "src/components/todolist/ToDoListAppBody"
+
+// const creTask = document.getElementById("btn")! as HTMLButtonElement
+// const inputEl = document.getElementById("input")! as HTMLInputElement
+// const listEl = document.getElementById("list")!
+// const submitBtn = document.getElementById("submit")!
+
+// submitBtn.addEventListener("click", () => {})
+
+// const [taskName, setTaskName] = React.useState(String)
+
+// const submitForm = (event: React.FormEvent<HTMLFormElement>) => {
+//     // Preventing the page from reloading
+//     event.preventDefault()
+//     console.log(document.getElementById("taskName"))
+//     console.log(document.getElementById("desc"))
+// }
+
+// function saveData(e: SubmitEvent) {
+//     e.preventDefault()
+//     const taskName = inputEl.value
+//     createList(taskName)
+// }
+
+// function createList(text: string) {
+//     const liEl = document.createElement("li")
+//     liEl.append(text)
+//     listEl.append(liEl)
+//     inputEl.value = ""
+// }
 
 const createtask = () => {
     const [type, setType] = useState("individual")
@@ -41,12 +70,12 @@ const createtask = () => {
                 <Heading as="h2" size="md" noOfLines={1} mt={8} mb={2}>
                     Task Name
                 </Heading>
-                <Input placeholder="Task Name" size="md" />
+                <Input placeholder="Task Name" size="md" id="taskName" />
 
                 <Heading as="h2" size="md" noOfLines={1} mt={8} mb={2}>
                     Description
                 </Heading>
-                <Input placeholder="Description" size="md" />
+                <Input placeholder="Description" size="md" id="desc" />
 
                 <Heading as="h2" size="md" noOfLines={1} mt={8} mb={2}>
                     Due Date
@@ -81,7 +110,7 @@ const createtask = () => {
 
                 <Box display="flex" justifyContent="center" alignItems="center" marginY={10}>
                     {type == "individual" ? (
-                        <Button colorScheme="teal" size="lg" bgColor={"orange.500"}>
+                        <Button colorScheme="teal" size="lg" id="submit" bgColor={"orange.500"}>
                             <Link href="/todolist/task">Done</Link>
                         </Button>
                     ) : (
