c58be0520808b1733fab9fc3b9020f0f94546d53
Passakorn Puttama <boss4848988@gmail.com>
2022-11-20 14:25:23 +0700

    [UPDATE] main page design and fetch API from backend
    - Change the color of the mobile button
    - Fix mobile responsive
    - Added fectch API from backend
    - Change useWindowDimensions name to isMobile

--- frontend/src/pages/groups/index.tsx
+++ frontend/src/pages/groups/index.tsx
@@ -1,5 +1,5 @@
-import { Show, Box, HStack, Button, Input, Stack, Text, Flex, Grid, Link } from "@chakra-ui/react"
-import React, { Suspense, useState } from "react"
+import { Show, Box, HStack, Button, Input, Stack, Text, Flex, Grid, useBoolean } from "@chakra-ui/react"
+import React, { Suspense, useEffect, useState } from "react"
 import AppBody from "../../components/share/app/AppBody"
 import { FaSearch, FaPlus } from "react-icons/fa"
 import { TiWarning } from "react-icons/ti"
@@ -7,7 +7,9 @@ import { userData } from "./data"
 import CommunityList from "../../components/group/CommunityList"
 import SuggestBox from "../../components/group/SuggestBox"
 import InvitationBox from "../../components/group/InvitationBox"
-import useWindowDimensions from "./hooks/useWindowDimensions"
+import useWindowDimensions from 'src/components/group/hooks/useWindowDimensions'
+import { Link } from "react-router-dom"
+import API from "src/function/API"
 
 const index = () => {
     const [searchBtn, setSearchBtn] = useState(false) //for close/open seach bar
@@ -17,69 +19,105 @@ const index = () => {
         setSearchBtn(!searchBtn)
         setSearchValue("")
     }
+
     //for mobile
-    const [allBtn, setAllBtn] = useState(true)
-    const [inviteBtn, setInviteBtn] = useState(false)
-    const [suggestBtn, setSuggestBtn] = useState(false)
-    const [acceptBtn, setAcceptBtn] = useState(false)
-    const [declinetBtn, setDeclinetBtn] = useState(false)
-
-    const handleAllBtnOnClick = () => {
-        setAllBtn(true)
-        setInviteBtn(false)
-        setSuggestBtn(false)
-    }
-    const handleInviteOnClick = () => {
-        setAllBtn(false)
-        setInviteBtn(true)
-        setSuggestBtn(false)
-    }
-    const handleSuggestOnClick = () => {
-        setAllBtn(false)
-        setInviteBtn(false)
-        setSuggestBtn(true)
-    }
-
-    const handleAcceptOnClick = () => {
-        setAcceptBtn(true)
-    }
-
-    const handleDeclineOnClick = () => {
-        setDeclinetBtn(true)
-    }
-
-
-
+    const [{ allBtn, inviteBtn, suggestBtn }, setBtn] = useState({ allBtn: true, inviteBtn: false, suggestBtn: false })
     //md: 768px
     const { height, width } = useWindowDimensions()
+    let isMobile = (width || 0) > 768
+
+    //get community form backend
+    const [isError, { on }] = useBoolean()
+    const [isLoading, { off }] = useBoolean(true)
+    const [communitys, setCommunitys] = useState<any>([])
+
+    useEffect(() => {
+        API.get("/group/getcommunitys")
+            .then(res => setCommunitys(res.data))
+            .catch((err) => on()).finally(() => off())
+    }, [])
+    if (isLoading) {
+        return (
+            // will fix the design later
+            <AppBody>
+                <Box>
+                    <Text>Loading...</Text>
+                </Box>
+            </AppBody>
+        )
+    }
+    if (isError) {
+        // will fix the design later
+        return (
+            <AppBody>
+                <Box>
+                    <Text>Something went wrong...</Text>
+                </Box>
+            </AppBody>
+        )
+    }
     return (
         <AppBody>
             {/* <div>
                 width: {width} ~ height: {height}
             </div> */}
+
             <Flex gap={2} direction={{ base: "column", md: "row" }} mb={4}>
                 <Box>
                     <Show below="md">
                         <HStack justify={"space-between"}>
                             <HStack gap={1} display={searchBtn ? "none" : "flex"}>
-                                <Button backgroundColor={allBtn ? "gray.300" : "gray.100"} onClick={handleAllBtnOnClick}>
+                                <Button
+                                    borderRadius='xl'
+                                    _hover={allBtn ? { background: 'gray.400' } : { background: 'orange.200' }}
+                                    size='sm'
+                                    color="#FFFFFF"
+                                    backgroundColor={allBtn ? "#6b7999" : "#e65300"}
+                                    onClick={() => setBtn({ allBtn: true, inviteBtn: false, suggestBtn: false })}
+                                    sx={{ boxShadow: "rgba(0, 0, 0, 0.24) 2px 3px 5px" }}
+                                >
                                     All
                                 </Button>
-                                <Button backgroundColor={inviteBtn ? "gray.300" : "gray.100"} onClick={handleInviteOnClick}>
+                                <Button
+                                    sx={{ boxShadow: "rgba(0, 0, 0, 0.24) 2px 3px 5px" }}
+                                    borderRadius='xl'
+                                    size='sm'
+                                    _hover={inviteBtn ? { background: 'gray.400' } : { background: 'orange.200' }}
+                                    color="#FFFFFF"
+                                    backgroundColor={inviteBtn ? "#6b7999" : "#e65300"}
+                                    onClick={() => setBtn({ allBtn: false, inviteBtn: true, suggestBtn: false })}>
                                     Invited
                                 </Button>
-                                <Button backgroundColor={suggestBtn ? "gray.300" : "gray.100"} onClick={handleSuggestOnClick}>
+                                <Button
+                                    sx={{ boxShadow: "rgba(0, 0, 0, 0.24) 2px 3px 5px" }}
+                                    borderRadius='xl'
+                                    size='sm'
+                                    _hover={suggestBtn ? { background: 'gray.400' } : { background: 'orange.200' }}
+                                    color="#FFFFFF"
+                                    backgroundColor={suggestBtn ? "#6b7999" : "#e65300"}
+                                    onClick={() => setBtn({ allBtn: false, inviteBtn: false, suggestBtn: true })}>
                                     Suggested
                                 </Button>
                             </HStack>{" "}
                             {allBtn ? (
                                 <Stack direction={"row"} justify="end" width={"100%"} gap={1}>
-                                    <Link href="http://127.0.0.1:5173/groups/create">
-                                        <Button display={searchBtn ? "none" : "flex"}>
+                                    <Link to="/groups/create">
+                                        <Button
+                                            _hover={{ background: 'gray.50' }}
+                                            sx={{ boxShadow: "rgba(17, 12, 46, 0.15) 0px 3px 5px 0px" }}
+                                            borderRadius='xl'
+                                            size='sm'
+                                            bg='#FFFFFF'
+                                            display={searchBtn ? "none" : "flex"}>
                                             <FaPlus />
                                         </Button>
                                     </Link>
                                     <Input
+                                        bg='#FFFFFF'
+                                        _hover={{ background: 'gray.50' }}
+                                        sx={{ boxShadow: "rgba(17, 12, 46, 0.15) 0px 2px 3px 0px" }}
+                                        borderRadius='xl'
+                                        size='sm'
                                         width={"100%"}
                                         display={searchBtn ? "" : "none"}
                                         variant={"filled"}
@@ -89,7 +127,13 @@ const index = () => {
                                         placeholder="Seacrh Community"
                                         focusBorderColor="gray.200"
                                     ></Input>
-                                    <Button onClick={handleSearchBtn}>
+                                    <Button
+                                        _hover={{ background: 'gray.50' }}
+                                        sx={{ boxShadow: "rgba(17, 12, 46, 0.15) 0px 3px 5px 0px" }}
+                                        bg='#FFFFFF'
+                                        borderRadius='xl'
+                                        size='sm'
+                                        onClick={handleSearchBtn}>
                                         <FaSearch />
                                     </Button>
                                 </Stack>
@@ -113,7 +157,7 @@ const index = () => {
                                 placeholder="Seacrh Communities"
                                 focusBorderColor="gray.200"
                             ></Input>
-                            <Link href='groups/create' _hover={{ textDecoration: "none" }} >
+                            <Link to={'/groups/create'}  >
                                 <Button mt="2" colorScheme="orange" _hover={{ background: 'orange.200' }} variant="solid" width="95%" fontSize="sm">
                                     + Create New Community
                                 </Button>
@@ -121,17 +165,17 @@ const index = () => {
 
                         </Box>
                     </Show>
-                    <Box display={allBtn || (width || 0) > 768 ? "block" : "none"}>
-                        <Box mt={4} mb={3} background={{ md: "orange.400", base: "" }} p={{ md: "3", base: "" }} borderRadius={"md"}>
+                    <Box display={allBtn || isMobile ? "block" : "none"}>
+                        <Box mt={4} mb={3} background={{ md: "orange.400", base: "" }} p={{ md: "3", base: "4" }} borderRadius={"md"}>
                             <Text as="b" color={{ md: "white", base: "black" }}>Community you manage</Text>
 
 
-                            {userData.ownCommunitys.filter((community) => community.name.toLowerCase().includes(searchValue.toLowerCase())).length > 0 ? (
-                                userData.ownCommunitys
-                                    .filter((community) => {
+                            {communitys.ownCommunitys.filter((community: any) => community.name.toLowerCase().includes(searchValue.toLowerCase())).length > 0 ? (
+                                communitys.ownCommunitys
+                                    .filter((community: any) => {
                                         return searchValue.toLowerCase() == "" ? community : community.name.toLowerCase().includes(searchValue)
                                     })
-                                    .map((community) => (community.roleID >= 3 ? (
+                                    .map((community: any) => (community.roleID >= 3 ? (
                                         <CommunityList
                                             communityID={community.ID}
                                             key={community.ID}
@@ -163,17 +207,17 @@ const index = () => {
                             )}
                         </Box>
 
-                        <Box mt={4} mb={3} background={{ md: "orange.400", base: "" }} p={{ md: "3", base: "" }} borderRadius={"md"}>
+                        <Box mt={4} mb={3} background={{ md: "orange.400", base: "" }} p={{ md: "3", base: "4" }} borderRadius={"md"}>
                             <Text as="b" color={{ md: "white", base: "black" }}>Community you've joined</Text>
 
-                            {userData.joinedCommunitys.filter((community) => community.name.toLowerCase().includes(searchValue.toLowerCase())).length >
+                            {communitys.joinedCommunitys.filter((community: any) => community.name.toLowerCase().includes(searchValue.toLowerCase())).length >
                                 0 ? (
-                                userData.joinedCommunitys
-                                    .filter((community) => {
+                                communitys.joinedCommunitys
+                                    .filter((community: any) => {
                                         return searchValue.toLowerCase() == "" ? community : community.name.toLowerCase().includes(searchValue)
                                     })
 
-                                    .map((community) => (community.roleID < 3 ? (
+                                    .map((community: any) => (community.roleID < 3 ? (
                                         <CommunityList
                                             communityID={community.ID}
                                             key={community.ID}
@@ -210,7 +254,7 @@ const index = () => {
                 <Show>
                     <Box width={"100%"} >
                         <Box
-                            display={inviteBtn || (width || 0) > 768 ? "block" : "none"}
+                            display={inviteBtn || isMobile ? "block" : "none"}
                             borderRadius="md"
                             background={{ md: "orange.400", base: "" }}
                             width="100%"
@@ -222,7 +266,7 @@ const index = () => {
                         >
                             <Text as="b" color={{ md: "white", base: "black" }}>Invitation</Text>
                             <Text fontSize="sm" color={{ md: "white", base: "black" }}>These people have invited you to join the community</Text>
-                            {userData.invitations.map((i) => (
+                            {communitys.invitations.map((i: any) => (
                                 <InvitationBox
                                     key={i.inviteID}
                                     userName={i.userName}
@@ -235,7 +279,7 @@ const index = () => {
                             ))}
                         </Box>
                         <Box
-                            display={suggestBtn || (width || 0) > 768 ? "block" : "none"}
+                            display={suggestBtn || isMobile ? "block" : "none"}
                             mt={2}
                             borderRadius="md"
                             background={{ md: "orange.400", base: "" }}
@@ -249,7 +293,7 @@ const index = () => {
                             <Text as="b" color={{ md: "white", base: "black" }}>Suggested for you</Text>
                             <Text fontSize="sm" color={{ md: "white", base: "black" }}>Communities you might be interested in.</Text>
                             <Grid gap={2} templateColumns={{ sm: "repeat(1,1fr)", md: "repeat(2, 1fr)" }} width="100%">
-                                {userData.invitations.map((i) => (
+                                {communitys.invitations.map((i: any) => (
                                     <SuggestBox
                                         key={i.inviteID}
                                         communityName={i.communityName}
