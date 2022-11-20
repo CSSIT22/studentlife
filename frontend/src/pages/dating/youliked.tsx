cf7de2b115535fdd3ea8ebc95f38624594689510
Pawin Nakvisai <pawinnak2@gmail.com>
2022-11-20 11:12:26 +0700

    Fixed people who you liked button color

--- frontend/src/pages/dating/youliked.tsx
+++ frontend/src/pages/dating/youliked.tsx
@@ -55,8 +55,8 @@ const YouLiked = () => {
                 <Box bg="#FFF2E6" position="fixed" w="100%" justifyContent="space-between" top={{ base: 21, md: 157 }} id="bottomBar">
                     <Box maxW="100%" pt={{ base: "40px", md: "7px" }}></Box>
                     <HStack gap={{ base: "20px", md: "100px" }} display="flex" justifyContent="center" pt={{ base: "40px", md: "30px" }} pb="30px">
-                        <DatingLikedYouButton backgroundColor="orange.600" />
-                        <DatingYouLikedButton backgroundColor="orange.800" />
+                        <DatingLikedYouButton backgroundColor="orange.800" />
+                        <DatingYouLikedButton backgroundColor="orange.600" />
                     </HStack>
                 </Box>
             </Box>
