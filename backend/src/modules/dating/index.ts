e4b4185c79fc1edcfd7b1df442a5b7e180c2b298
Pawin Nakvisai <pawinnak2@gmail.com>
2022-11-20 10:25:33 +0700

    Reset DB routes

--- backend/src/modules/dating/index.ts
+++ backend/src/modules/dating/index.ts
@@ -3,6 +3,7 @@ import createAPollRoutes from "./routes/create"
 import interestsRoutes from "./routes/interests"
 import optionRoutes from "./routes/option"
 import readDBRoutes from "./routes/readDB"
+import resetDBRoutes from "./routes/resetDB"
 
 const datingRoutes = express()
 
@@ -15,6 +16,9 @@ datingRoutes.get("/", (_, res) => {
 datingRoutes.use("/interests", interestsRoutes)
 datingRoutes.use("/option", optionRoutes)
 datingRoutes.use("/create", createAPollRoutes)
+
+// Danger zone
 datingRoutes.use("/readDB", readDBRoutes)
+datingRoutes.use("/resetDB", resetDBRoutes)
 
 export default datingRoutes
