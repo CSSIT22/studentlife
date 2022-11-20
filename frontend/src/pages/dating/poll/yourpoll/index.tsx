a1e32f7c94cc7ee5e8acb9b49e4bf491f62bd467
Thitipa <thitipanet@gmail.com>
2022-11-20 10:54:33 +0700

    Finish the CREATE FRONTEND part of all Thitipa's page!

--- frontend/src/pages/dating/poll/yourpoll/index.tsx
+++ frontend/src/pages/dating/poll/yourpoll/index.tsx
@@ -17,7 +17,7 @@ const YourActivityPoll = () => {
                     pl="500px"
                     pt={{ base: "-20px", md: "20px" }}
                     zIndex="4"
-                    pb="e0px"
+                    pb="30px"
                     position="fixed"
                     top={{ base: 20, md: 150 }}
                     justifyContent="center"
