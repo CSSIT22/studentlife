81b980fa6668c8e0f16a6030b865adce5838668b
Pattarapon Koomphon <45892157+Kevinindy2k@users.noreply.github.com>
2022-11-20 19:35:33 +0700

    Update GetABOUTME

--- backend/src/modules/user/routers/getAboutMe.ts
+++ backend/src/modules/user/routers/getAboutMe.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express"
 const getAboutme = async (req: Request, res: Response) => {
     try {
         const { prisma } = res
-        const userId = req.params["id"]
+        const userId = req.user?.userId
         const detail = await prisma.detail.findFirstOrThrow({
             where: { userId },
             select: { phone: true, birth: true, sex: true, hobby: true, year: true },
