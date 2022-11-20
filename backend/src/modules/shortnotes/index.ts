f626032b4af8e1e9501c78846281def57976785e
Pasinun.w <95624019+Unuisad@users.noreply.github.com>
2022-11-20 19:41:13 +0700

    [ADD] postShortnote path

--- backend/src/modules/shortnotes/index.ts
+++ backend/src/modules/shortnotes/index.ts
@@ -2,6 +2,7 @@ import { prisma } from "@prisma/client"
 import express from "express"
 import getCourses from "./routes/getCourses"
 import getResentShortnotes from "./routes/getResentShortnotes"
+import postShortnote from "./routes/postShortnote"
 const shortnotesRoutes = express()
 
 type s = {
@@ -12,83 +13,6 @@ type s = {
     createAt: string
     isPublic: boolean
 }
-// let sn: s[] = [
-//     {
-//         id: "9b1deb4d-3b7d-4bad-fb78-2b0d7b3dcb6d",
-//         topic: "How to make ER diagram in 10 minutes.",
-//         course: "CSC218",
-//         owner: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
-//         createAt: "10-6-22",
-//         isPublic: false,
-//     },
-//     {
-//         id: "f6hjk89o-d458-4bad-9bdd-j8fklg0d9ifh",
-//         topic: "Network foro eginner.",
-//         course: "CSC220",
-//         owner: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
-//         createAt: "10-6-22",
-//         isPublic: false,
-//     },
-//     {
-//         id: "fvb4h8l6-3b7d-f5jv-grt7-lfepgb9ogldg",
-//         topic: "Productive with agile.",
-//         course: "CSC218",
-//         owner: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
-//         createAt: "10-6-22",
-//         isPublic: true,
-//     },
-//     {
-//         id: "9b1deb4d-3b7d-4bad-fb78-2b0d7b3dcb6d",
-//         topic: "How to get away from 'F' in Algorithm",
-//         course: "CSC210",
-//         owner: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
-//         createAt: "10-6-22",
-//         isPublic: false,
-//     },
-//     {
-//         id: "f6hjk89o-d458-4bad-9bdd-j8fklg0d9ifh",
-//         topic: "Tower of Dubai",
-//         course: "CSC210",
-//         owner: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
-//         createAt: "10-6-22",
-//         isPublic: true,
-//     },
-//     {
-//         id: "fvb4h8l6-3b7d-f5jv-grt7-lfepgb9ogldg",
-//         topic: "Meowๆ",
-//         course: "MTH110",
-//         owner: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
-//         createAt: "10-6-22",
-//         isPublic: true,
-//     },
-// ]
-
-export let rsn = [
-    {
-        id: "9b1deb4d-3b7d-4bad-fb78-2b0d7b3dcb6c",
-        topic: "How to make ER diagram in 10 minutes.",
-        course: "CSC218",
-        owner: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
-        createAt: "10-6-22",
-        isPublic: true,
-    },
-    {
-        id: "f6hjk89o-d458-4bad-9bdd-j8fklg0d9ifh",
-        topic: "Shortest path",
-        course: "CSC210",
-        owner: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
-        createAt: "10-6-22",
-        isPublic: false,
-    },
-    {
-        id: "fvb4h8l6-3b7d-f5jv-grt7-lfepgb9ogldg",
-        topic: "Java programming",
-        course: "CSC110",
-        owner: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
-        createAt: "10-6-22",
-        isPublic: true,
-    },
-]
 
 export let course = ["CSC110", "CSC210", "CSC220", "CSC213", "CSC218", "MTH110"]
 
@@ -97,12 +21,14 @@ shortnotesRoutes.use(express.json())
 shortnotesRoutes.get("/getShortnotes", async (req, res) => {
     const prisma = res.prisma
     const sn = await prisma.sn_Head.findMany()
-    console.log(sn)
+    //console.log(sn)
     res.send(sn)
 })
 
 shortnotesRoutes.get("/getResentShortnotes", getResentShortnotes)
 
 shortnotesRoutes.get("/getCourses", getCourses)
+
+shortnotesRoutes.post("/postShortnote", postShortnote)
 
 export default shortnotesRoutes
