8240d763e14a48a8717fd9ca95a2bc682c2e886f
Passakorn Puttama <boss4848988@gmail.com>
2022-11-20 14:43:03 +0700

    [UPDATE] testing craetecommunity api

--- backend/src/modules/group/routes/createCommunity.ts
+++ backend/src/modules/group/routes/createCommunity.ts
@@ -1,39 +1,31 @@
 import { Request, Response } from "express"
 import { createCommunity } from "../../../../../types/group"
 
+const createCommunity = async (req: Request, res: Response) => {
+    const prisma = res.prisma
+    const body = req.body
+    const userid = req.user?.userId
 
-const createCommunity = async (req: Request, res: Response) =>{
-        
-        const prisma = res.prisma
-        const body = req.body
-        const userid = req.user?.userId
-        
-        const createCommunity : any = {   
-            communityName : body.communityName,
-            communityOwnerId : body.user,
-            communityDesc : body.communityDesc,
-            communityPrivacy : body.communityPrivacy,
-            communityPhoto : body.communityCoverPhoto
-        }
-
-        try{
-        await prisma.community.create(   
-            {
-                data: createCommunity
-            }
-        )
-
-        res.status(201).send("Created Success")
-        }
-
-        catch(err){
-            res.status(403)
-        }
-        
+    const createCommunity: any = {
+        communityName: body.communityName,
+        communityOwnerId: body.user,
+        communityDesc: body.communityDesc,
+        communityPrivacy: body.communityPrivacy,
+        communityPhoto: body.communityCoverPhoto,
+        communityTags: body.communityTags,
     }
+    console.log(createCommunity)
+    // console.log(req.body.communityName)
+    // console.log(req.body.communityTags)
+    res.sendStatus(201)
+    // try {
+    //     await prisma.community.create({
+    //         data: createCommunity,
+    //     })
 
-
-
-
-
+    //     res.status(201).send("Created Success")
+    // } catch (err) {
+    //     res.status(403)
+    // }
+}
 export default createCommunity
