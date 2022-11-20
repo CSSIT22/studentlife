a1b68d41d9a9776108aeb02d08db2b874f36be66
JoNoteJoMaMa <93032309+JoNoteJoMaMa@users.noreply.github.com>
2022-11-20 21:22:02 +0700

    Update: เพิ่มfolder blog
    สำรองตอนเกิดconflict

--- backend/src/modules/blog/index1.ts
+++ backend/src/modules/blog/index1.ts
@@ -2,8 +2,12 @@ import express from "express"
 
 const blogRoutes = express()
 
-blogRoutes.get("/getpostinfo", (req, res)=>{
+blogRoutes.get("/post/:post_id", (req, res) => {
     res.send("Hello")
+})
+
+blogRoutes.post("/postcreating", (req, res) => {
+    res.send("Create Post")
 })
 
 export default blogRoutes
--- frontend/src/pages/home/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import App from "../../App"
-import AppBody from "../../components/share/app/AppBody"
-
-const Home = () => {
-    return <AppBody></AppBody>
-}
-
-export default Home
