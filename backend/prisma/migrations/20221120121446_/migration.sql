873efbd05fa7f3ed9de523307df36214c45a4246
Erxical <erxinxiao@gmail.com>
2022-11-20 19:30:21 +0700

    - [Revert] Change created (Sn_Head) from DateTime to Date

---
+++ backend/prisma/migrations/20221120121446_/migration.sql
@@ -0,0 +1,14 @@
+-- AlterTable
+ALTER TABLE "Analytic_User_Report" ALTER COLUMN "end" SET DEFAULT make_timestamp(3000, 1, 1, 0, 0, 0);
+
+-- AlterTable
+ALTER TABLE "Chat_Group" ALTER COLUMN "lefted" SET DEFAULT make_timestamp(3000, 1, 1, 0, 0, 0);
+
+-- AlterTable
+ALTER TABLE "Chat_Room" ALTER COLUMN "deleted" SET DEFAULT make_timestamp(3000, 1, 1, 0, 0, 0);
+
+-- AlterTable
+ALTER TABLE "QR" ALTER COLUMN "expired" SET DEFAULT make_timestamp(3000, 1, 1, 0, 0, 0);
+
+-- AlterTable
+ALTER TABLE "ShortLink" ALTER COLUMN "expired" SET DEFAULT make_timestamp(3000, 1, 1, 0, 0, 0);
