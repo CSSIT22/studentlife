ad76de83bf40d68bfe2a664c975e91e39427be1b
Pasinun.w <95624019+Unuisad@users.noreply.github.com>
2022-11-20 19:40:45 +0700

    [ADD] get resent shortnotes API
    - not order by lastest yet

--- backend/src/modules/shortnotes/routes/getResentShortnotes.ts
+++ backend/src/modules/shortnotes/routes/getResentShortnotes.ts
@@ -1,16 +1,31 @@
 import { Request, Response } from "express"
-import { rsn } from ".."
+//import { rsn } from ".."
 
 const getResentShortnotes = async (req: Request, res: Response) => {
-    // const prisma = res.prisma
-    // const user = req.user?.userId
-    // const rsn:any = await prisma.sn_Recent.findMany({
-    //     include:{
-    //         userId: user,
-    //     },
-    // })
-    // console.log(rsn)
-    res.send(rsn)
+    const prisma = res.prisma
+    const user = req.user?.userId
+
+    const rsn: any = await prisma.sn_Recent.findMany({
+        where: {
+            userId: user,
+        },
+        orderBy: { viewedAt: "desc" },
+        take: 3,
+    })
+    //console.log(rsn)
+
+    const idList = rsn.map((item: any) => {
+        return item.snId
+    })
+
+    const newRsn: any = await prisma.sn_Head.findMany({
+        where: {
+            snId: { in: idList },
+        },
+    })
+
+    //console.log(newRsn)
+    res.send(newRsn)
 }
 
 export default getResentShortnotes
