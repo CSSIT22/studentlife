f65a350161a4ff40e206a1a7681ed3d2d7abe9a6
bbbung <kanyapat.mekv@kmutt.ac.th>
2022-11-21 17:40:42 +0700

    [UPDATE] post no.9

--- backend/src/modules/announcement/index.ts
+++ backend/src/modules/announcement/index.ts
@@ -176,6 +176,23 @@ export let posts: post[] = [
         expiredAfterDelete: new Date(),
         addMoreLang: [],
     },
+    {
+        postId: 9,
+        userId: "wFbN9qrwUbCgrEtfpIEVf",
+        lang_id: 1000,
+        topic: "Hello World9",
+        detail: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        sender: "SAMO-SIT",
+        status: "waiting",
+        pinStatus: false,
+        isApprove: false,
+        targetType: "Everyone",
+        targetValue: "",
+        postAt: new Date(),
+        expiredOfPost: new Date("2022-11-24"),
+        expiredAfterDelete: new Date(),
+        addMoreLang: [],
+    },
 ]
 
 export const getPost = () => {
