0286bfb69c88f312099678357bbe1c2971f62d96
Pasinun.w <95624019+Unuisad@users.noreply.github.com>
2022-11-22 02:28:39 +0700

    [ADD] try catch

--- backend/src/modules/shortnotes/routes/getResentShortnotes.ts
+++ backend/src/modules/shortnotes/routes/getResentShortnotes.ts
@@ -2,31 +2,35 @@ import { Request, Response } from "express"
 //import { rsn } from ".."
 
 const getResentShortnotes = async (req: Request, res: Response) => {
-    const prisma = res.prisma
-    const user = req.user?.userId
+    try {
+        const prisma = res.prisma
+        const user = req.user?.userId
 
-    const rsn: any = await prisma.sn_Recent.findMany({
-        where: {
-            userId: user,
-        },
-        orderBy: { viewedAt: "asc" },
-        take: 3,
-    })
-    //console.log(rsn)
+        const rsn: any = await prisma.sn_Recent.findMany({
+            where: {
+                userId: user,
+            },
+            orderBy: { viewedAt: "asc" },
+            take: 3,
+        })
+        //console.log(rsn)
 
-    const idList = rsn.map((item: any) => {
-        return item.snId
-    })
-    //console.log(idList)
+        const idList = rsn.map((item: any) => {
+            return item.snId
+        })
+        //console.log(idList)
 
-    const newRsn: any = await prisma.sn_Head.findMany({
-        where: {
-            snId: { in: idList },
-        },
-    })
+        const newRsn: any = await prisma.sn_Head.findMany({
+            where: {
+                snId: { in: idList },
+            },
+        })
 
-    //console.log(newRsn)
-    res.send(newRsn)
+        //console.log(newRsn)
+        res.send(newRsn)
+    } catch (err) {
+        res.status(400).send("some error")
+    }
 }
 
 export default getResentShortnotes
