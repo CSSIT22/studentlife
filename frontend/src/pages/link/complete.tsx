d99a88ce59a0763e98e984a514248918099da78e
Kitibhum <98164016+Kitibhum@users.noreply.github.com>
2022-11-21 13:07:38 +0700

    w

--- frontend/src/pages/link/complete.tsx
+++ frontend/src/pages/link/complete.tsx
@@ -3,8 +3,6 @@ import { Container, Center, Box, Text, Flex, HStack } from "@chakra-ui/react"
 import { CheckCircleIcon } from "@chakra-ui/icons"
 import { useNavigate } from "react-router-dom"
 const complete = () => {
-    
-       
     return (
         <AppBody>
             <Container borderWidth="1px" padding="10" borderRadius="xl" background={"white"} gap={2}>
@@ -19,7 +17,6 @@ const complete = () => {
                     border={"3px solid white"}
                     textAlign={"center"}
                 >
-                    
                     <Center>
                         <Text as={"b"}>SHORTLINK UNBLOCK</Text>
                     </Center>
@@ -27,12 +24,11 @@ const complete = () => {
                 <br />
                 <Box bgColor={"orange.200"} p="10rem" borderRadius="15">
                     <Box bgColor={"white"} p="5%" w="15rem" h="10rem" borderRadius="15">
-                    
                         <Text align="center" fontWeight="bold" fontSize="30px">
                             CREATE SHORTLINK COMPLETE
                         </Text>
                     </Box>
-                        <br />
+                    <br />
                     <HStack bgColor={"white"} p="10%" w="15rem" h="10rem" borderRadius="15" justify="center">
                         <CheckCircleIcon w="125" h="125" />
                     </HStack>
