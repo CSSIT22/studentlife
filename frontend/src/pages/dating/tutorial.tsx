d1d5e004d1834f00bf1271c9e40168e3656c4bef
Pawin Nakvisai <pawinnak2@gmail.com>
2022-11-21 17:35:27 +0700

    Tutorial frontend

--- frontend/src/pages/dating/tutorial.tsx
+++ frontend/src/pages/dating/tutorial.tsx
@@ -1,41 +1,41 @@
-import { Box, Button, Center, Container, Text, useBreakpointValue } from '@chakra-ui/react'
+import { Box, Button, Center, Text, useBreakpointValue } from '@chakra-ui/react'
 import DatingAppBody from 'src/components/dating/DatingAppBody'
-import DatingCreatePollButton from 'src/components/dating/DatingCreatePollButton'
-import { Navigation, Pagination } from 'swiper'
+import { Pagination } from 'swiper'
 import { Swiper, SwiperSlide, useSwiper } from 'swiper/react'
-
-import "swiper/css";
-import "swiper/css/lazy";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import { useEffect } from 'react'
+import { BsArrowLeft, BsArrowRight } from 'react-icons/bs'
 
 const FirstPageNextButton = (props: any) => {
     const swiper = useSwiper();
-    return (<Button colorScheme="orange" w={{ base: "132px", md: "178px" }} h={{ base: "54px", md: "61px" }} boxShadow="0px 10px 15px -3px rgba(0, 0, 0, 0.1), 0px 4px 6px -2px rgba(0, 0, 0, 0.05)" onClick={() => swiper.slideNext()}>{props.children}</Button>);
+    return (<Button className="swiper-no-swiping" colorScheme="orange" w={{ base: "132px", md: "178px" }} h={{ base: "54px", md: "61px" }} boxShadow="0px 10px 15px -3px rgba(0, 0, 0, 0.1), 0px 4px 6px -2px rgba(0, 0, 0, 0.05)" onClick={() => swiper.slideNext()}>{props.children}</Button>);
+}
+
+const LeftButton = (props: any) => {
+    const swiper = useSwiper();
+    return (<Button className="swiper-no-swiping" colorScheme="orange" w="80px" h="80px" borderRadius="full" boxShadow="0px 10px 15px -3px rgba(0, 0, 0, 0.1), 0px 4px 6px -2px rgba(0, 0, 0, 0.05)" onClick={() => swiper.slidePrev()}>{props.children}</Button>);
+}
+
+const RightButton = (props: any) => {
+    const swiper = useSwiper();
+    return (<Button className="swiper-no-swiping" colorScheme="orange" w="80px" h="80px" borderRadius="full" boxShadow="0px 10px 15px -3px rgba(0, 0, 0, 0.1), 0px 4px 6px -2px rgba(0, 0, 0, 0.05)" onClick={() => swiper.slideNext()}>{props.children}</Button>);
 }
 
 const Tutorial = () => {
-    
+
     const isMobile = useBreakpointValue({
         base: false,
         md: true,
     })
 
-    const dotColor = {
-        "--swiper-pagination-color": "black",
-    }
-
     return (
         <DatingAppBody>
             <Box>
-                <Swiper id="tutorial" pagination={true} modules={[Pagination, Navigation]} className="mySwiper">
+                <Swiper id="tutorial" pagination={true} modules={[Pagination]} className="mySwiper">
                     <SwiperSlide>
                         <Center>
                             <Box>
                                 <Text textAlign="center" fontWeight="700"
                                     fontSize={{ base: "30px", md: "36px" }}
-                                    lineHeight="133%" color="black" pt={{ base: "39.5px", md: "33px" }} >Hello!</Text>
+                                    lineHeight="133%" color="black" pt={{ base: "39.5px", md: "53px" }} >Hello!</Text>
                                 {
                                     isMobile ?
                                         (<Text textAlign="center" fontWeight="700"
@@ -69,7 +69,7 @@ const Tutorial = () => {
                                                 fontSize="30px"
                                                 lineHeight="133%" color="black" >Finding Friend</Text></>)
                                 }
-                                <Box display="flex" justifyContent="center" pt={{ base: "32px", md: "80px" }} pb={{ base: "178px", md: "80px" }}>
+                                <Box display="flex" justifyContent="center" pt={{ base: "32px", md: "80px" }} pb="178px">
                                     <FirstPageNextButton><Text fontWeight="700"
                                         fontSize={{ base: "20px", md: "25px" }}
                                         lineHeight="120%" color="white"></Text>Next</FirstPageNextButton>
@@ -77,12 +77,246 @@ const Tutorial = () => {
                             </Box>
                         </Center>
                     </SwiperSlide>
-
-
                     <SwiperSlide>
-                        <Box display="flex" h={{ base: "708px", md: "70vh" }}>
-                            <Text>Slide 2</Text>
+                        {isMobile ? (<><Box position="absolute" top={220} left={0}>
+                            <LeftButton><BsArrowLeft color="white" fontSize="50px" /></LeftButton>
                         </Box>
+                            <Box position="absolute" top={220} right={0}>
+                                <RightButton><BsArrowRight color="white" fontSize="50px" /></RightButton>
+                            </Box></>) : (<></>)}
+
+                        <Center>
+                            <Box>
+                                <Text textAlign="center" color="black" fontWeight="700"
+                                    fontSize="36px"
+                                    lineHeight="120%" pt={{ base: "10px", md: "36px" }}>Discovery</Text>
+                                {isMobile ? <Box
+                                    borderRadius="10px"
+                                    mt="26px"
+                                    w="552px"
+                                    h="354px"
+                                    backgroundSize="cover"
+                                    backgroundImage="https://media.giphy.com/media/26FLdmIp6wJr91JAI/giphy.gif"
+                                /> : <Box
+                                    borderRadius="10px"
+                                    mt="36px"
+                                    w="294px"
+                                    h="426px"
+                                    backgroundSize="cover"
+                                    backgroundImage="https://media.tenor.com/l9d2q8-JiQoAAAAM/kermit.gif"
+                                />
+                                }
+
+                            </Box>
+                        </Center>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        {isMobile ? (<><Box position="absolute" top={220} left={0}>
+                            <LeftButton><BsArrowLeft color="white" fontSize="50px" /></LeftButton>
+                        </Box>
+                            <Box position="absolute" top={220} right={0}>
+                                <RightButton><BsArrowRight color="white" fontSize="50px" /></RightButton>
+                            </Box></>) : (<></>)}
+
+                        <Center>
+                            <Box>
+                                <Text textAlign="center" color="black" fontWeight="700"
+                                    fontSize="36px"
+                                    lineHeight="120%" pt={{ base: "10px", md: "36px" }}>History</Text>
+                                {isMobile ? <Box
+                                    borderRadius="10px"
+                                    mt="26px"
+                                    w="552px"
+                                    h="354px"
+                                    backgroundSize="cover"
+                                    backgroundImage="https://media.giphy.com/media/26FLdmIp6wJr91JAI/giphy.gif"
+                                /> : <Box
+                                    borderRadius="10px"
+                                    mt="36px"
+                                    w="294px"
+                                    h="426px"
+                                    backgroundSize="cover"
+                                    backgroundImage="https://media.tenor.com/l9d2q8-JiQoAAAAM/kermit.gif"
+                                />
+                                }
+
+                            </Box>
+                        </Center>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        {isMobile ? (<><Box position="absolute" top={220} left={0}>
+                            <LeftButton><BsArrowLeft color="white" fontSize="50px" /></LeftButton>
+                        </Box>
+                            <Box position="absolute" top={220} right={0}>
+                                <RightButton><BsArrowRight color="white" fontSize="50px" /></RightButton>
+                            </Box></>) : (<></>)}
+
+                        <Center>
+                            <Box>
+                                <Text textAlign="center" color="black" fontWeight="700"
+                                    fontSize="36px"
+                                    lineHeight="120%" pt={{ base: "10px", md: "36px" }}>Matches</Text>
+                                {isMobile ? <Box
+                                    borderRadius="10px"
+                                    mt="26px"
+                                    w="552px"
+                                    h="354px"
+                                    backgroundSize="cover"
+                                    backgroundImage="https://media.giphy.com/media/26FLdmIp6wJr91JAI/giphy.gif"
+                                /> : <Box
+                                    borderRadius="10px"
+                                    mt="36px"
+                                    w="294px"
+                                    h="426px"
+                                    backgroundSize="cover"
+                                    backgroundImage="https://media.tenor.com/l9d2q8-JiQoAAAAM/kermit.gif"
+                                />
+                                }
+
+                            </Box>
+                        </Center>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        {isMobile ? (<><Box position="absolute" top={220} left={0}>
+                            <LeftButton><BsArrowLeft color="white" fontSize="50px" /></LeftButton>
+                        </Box>
+                            <Box position="absolute" top={220} right={0}>
+                                <RightButton><BsArrowRight color="white" fontSize="50px" /></RightButton>
+                            </Box></>) : (<></>)}
+
+                        <Center>
+                            <Box>
+                                <Text textAlign="center" color="black" fontWeight="700"
+                                    fontSize="36px"
+                                    lineHeight="120%" pt={{ base: "10px", md: "36px" }}>Activity polls</Text>
+                                {isMobile ? <Box
+                                    borderRadius="10px"
+                                    mt="26px"
+                                    w="552px"
+                                    h="354px"
+                                    backgroundSize="cover"
+                                    backgroundImage="https://media.giphy.com/media/26FLdmIp6wJr91JAI/giphy.gif"
+                                /> : <Box
+                                    borderRadius="10px"
+                                    mt="36px"
+                                    w="294px"
+                                    h="426px"
+                                    backgroundSize="cover"
+                                    backgroundImage="https://media.tenor.com/l9d2q8-JiQoAAAAM/kermit.gif"
+                                />
+                                }
+
+                            </Box>
+                        </Center>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        {isMobile ? (<><Box position="absolute" top={220} left={0}>
+                            <LeftButton><BsArrowLeft color="white" fontSize="50px" /></LeftButton>
+                        </Box>
+                            <Box position="absolute" top={220} right={0}>
+                                <RightButton><BsArrowRight color="white" fontSize="50px" /></RightButton>
+                            </Box></>) : (<></>)}
+
+                        <Center>
+                            <Box>
+                                <Text textAlign="center" color="black" fontWeight="700"
+                                    fontSize="36px"
+                                    lineHeight="120%" pt={{ base: "10px", md: "36px" }}>Interests</Text>
+                                {isMobile ? <Box
+                                    borderRadius="10px"
+                                    mt="26px"
+                                    w="552px"
+                                    h="354px"
+                                    backgroundSize="cover"
+                                    backgroundImage="https://media.giphy.com/media/26FLdmIp6wJr91JAI/giphy.gif"
+                                /> : <Box
+                                    borderRadius="10px"
+                                    mt="36px"
+                                    w="294px"
+                                    h="426px"
+                                    backgroundSize="cover"
+                                    backgroundImage="https://media.tenor.com/l9d2q8-JiQoAAAAM/kermit.gif"
+                                />
+                                }
+
+                            </Box>
+                        </Center>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        {isMobile ? (<><Box position="absolute" top={220} left={0}>
+                            <LeftButton><BsArrowLeft color="white" fontSize="50px" /></LeftButton>
+                        </Box>
+                            <Box position="absolute" top={220} right={0}>
+                                <RightButton><BsArrowRight color="white" fontSize="50px" /></RightButton>
+                            </Box></>) : (<></>)}
+
+                        <Center>
+                            <Box>
+                                <Text textAlign="center" color="black" fontWeight="700"
+                                    fontSize="36px"
+                                    lineHeight="120%" pt={{ base: "10px", md: "36px" }}>Option</Text>
+                                {isMobile ? <Box
+                                    borderRadius="10px"
+                                    mt="26px"
+                                    w="552px"
+                                    h="354px"
+                                    backgroundSize="cover"
+                                    backgroundImage="https://media.giphy.com/media/26FLdmIp6wJr91JAI/giphy.gif"
+                                /> : <Box
+                                    borderRadius="10px"
+                                    mt="36px"
+                                    w="294px"
+                                    h="426px"
+                                    backgroundSize="cover"
+                                    backgroundImage="https://media.tenor.com/l9d2q8-JiQoAAAAM/kermit.gif"
+                                />
+                                }
+
+                            </Box>
+                        </Center>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        {isMobile ? (<><Box position="absolute" top={220} left={0}>
+                            <LeftButton><BsArrowLeft color="white" fontSize="50px" /></LeftButton>
+                        </Box>
+                        </>) : (<></>)}
+
+                        <Center>
+                            <Box>
+                                <Text textAlign="center" color="black" fontWeight="700"
+                                    fontSize="36px"
+                                    lineHeight="120%" pt={{ base: "10px", md: "36px" }}>Tutorial</Text>
+                                {isMobile ? <Box
+                                    borderRadius="10px"
+                                    mt="26px"
+                                    w="552px"
+                                    h="354px"
+                                    backgroundSize="cover"
+                                    backgroundImage="https://media.giphy.com/media/26FLdmIp6wJr91JAI/giphy.gif"
+                                /> : <><Box
+                                    borderRadius="10px"
+                                    mt="36px"
+                                    w="294px"
+                                    h="426px"
+                                    backgroundSize="cover"
+                                    backgroundImage="https://media.tenor.com/l9d2q8-JiQoAAAAM/kermit.gif"
+                                />
+                                </>
+                                }
+                                <Center>
+                                    <Button className="swiper-no-swiping" colorScheme="orange" w={{ base: "179px", md: "183px" }} h={{ base: "53px", md: "61px" }} boxShadow="0px 10px 15px -3px rgba(0, 0, 0, 0.1), 0px 4px 6px -2px rgba(0, 0, 0, 0.05)" mt={{base: "45px", md: "24px"}}>
+                                        <Box>
+                                            <Text fontWeight="700"
+                                                fontSize="14px"
+                                                lineHeight="120%">Start using Dating</Text>
+                                            <Text fontWeight="700"
+                                                fontSize="14px"
+                                                lineHeight="120%"> & Finding Friend</Text>
+                                        </Box>
+                                    </Button>
+                                </Center>
+                            </Box>
+                        </Center>
                     </SwiperSlide>
                 </Swiper>
             </Box>
