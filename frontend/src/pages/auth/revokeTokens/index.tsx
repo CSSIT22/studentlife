f63ec92ba1d881652739f27c453e30059b123c80
sora-k46 <sorrawit456789@gmail.com>
2022-11-23 01:03:23 +0700

    Fix Card height

--- frontend/src/pages/auth/revokeTokens/index.tsx
+++ frontend/src/pages/auth/revokeTokens/index.tsx
@@ -198,12 +198,12 @@ const index = () => {
                                                     )}
                                                     {item.detail.deviceInfo === "tablet" && (
                                                         <Flex alignItems={"center"} justifyContent={"center"}>
-                                                            <Icon as={MdTabletMac} w="50%" h="155" justifySelf={"center"} alignSelf={"center"} />
+                                                            <Icon as={MdTabletMac} w="50%" h="166" justifySelf={"center"} alignSelf={"center"} />
                                                         </Flex>
                                                     )}
                                                     {item.detail.deviceInfo === "smartphone" && (
                                                         <Flex alignItems={"center"} justifyContent={"center"}>
-                                                            <Icon as={MdPhoneIphone} w="50%" h="155" justifySelf={"center"} alignSelf={"center"} />
+                                                            <Icon as={MdPhoneIphone} w="50%" h="166" justifySelf={"center"} alignSelf={"center"} />
                                                         </Flex>
                                                     )}
                                                 </Box>
