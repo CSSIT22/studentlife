5bbe30e634444062021a1ab9608fb97f09141d57
I_AM_TINE <sutichok46@gmail.com>
2022-11-22 14:27:14 +0700

    [FIX] FORMATE DOCUMENT BACKEND

--- backend/src/modules/restaurant/routes/showFavorite.ts
+++ backend/src/modules/restaurant/routes/showFavorite.ts
@@ -2,13 +2,13 @@ import { Request, Response } from "express"
 import { Restaurant } from "@apiType/restaurant"
 import { getRestaurant } from ".."
 const showFavorite = (req: Request, res: Response) => {
-    const userid = parseInt(req.query.userid+"")
+    const userid = parseInt(req.query.userid + "")
     let isFavorite: Restaurant[] = []
     getRestaurant().forEach((res) => {
         if (res.userid == userid) {
-            if(res.isFavorite == true){
+            if (res.isFavorite == true) {
                 isFavorite.push(res)
-            }          
+            }
         }
     })
     res.send(isFavorite)
