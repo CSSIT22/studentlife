8aaf0916d047d6378819294b308440308f11457e
Pasinun.w <95624019+Unuisad@users.noreply.github.com>
2022-11-23 21:21:03 +0700

    [ADD] get courses from db

--- backend/src/modules/shortnotes/routes/getCourses.ts
+++ backend/src/modules/shortnotes/routes/getCourses.ts
@@ -1,10 +1,15 @@
 import { Request, Response } from "express"
 import { prisma } from "@prisma/client"
-import { course } from ".."
+//import { course } from ".."
 
 const getCourses = async (req: Request, res: Response) => {
-    //const prisma = res.prisma
-    //const course = await prisma.course.findMany()
+    const prisma = res.prisma
+    const course = await prisma.course.findMany({
+        select: {
+            courseId: true,
+            courseName: true,
+        },
+    })
     res.send(course)
     //console.log(course)
 }
