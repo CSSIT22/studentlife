0ef8ee8c021732442f1239279b01898150fdf372
Pasinun.w <95624019+Unuisad@users.noreply.github.com>
2022-11-25 02:07:39 +0700

    [ADD] on hover

--- frontend/src/components/shortnotes/inLiList.tsx
+++ frontend/src/components/shortnotes/inLiList.tsx
@@ -6,7 +6,7 @@ const inLiList: FC<{
     course: String
 }> = ({ name, course }) => {
     return (
-        <Grid templateColumns="repeat(5, 1fr)" boxShadow={"md"} bg={"white"} rounded={8} p={5} w="100%" h={"100px"} alignItems={"center"}>
+        <Grid templateColumns="repeat(5, 1fr)" boxShadow={"md"} bg={"white"} rounded={8} p={5} w="100%" h={"100px"} alignItems={"center"} _hover={{ bg: '#f5f5f5' }} transition='all 0.2s cubic-bezier(.08,.52,.52,1)'>
             <GridItem colSpan={4}>
                 <Text>{name}</Text>
             </GridItem>
