8141d1c86971db01624c0ab5e9ec4ff3d446ec04
Pasinun.w <95624019+Unuisad@users.noreply.github.com>
2022-11-25 02:09:55 +0700

    [ADD] comment details from DB

--- frontend/src/components/shortnotes/cmList.tsx
+++ frontend/src/components/shortnotes/cmList.tsx
@@ -4,13 +4,19 @@ import React, { FC } from "react"
 const cmList: FC<{
     name: String
     desc: String
-}> = ({ name, desc }) => {
+    date: String | any
+}> = ({ name, desc, date }) => {
     return (
-        <Grid templateRows="repeat(3, 1fr)" bg={"white"} boxShadow={"base"} rounded={8} p={4}>
-            <GridItem rowSpan={1}>
+        <Grid templateRows="repeat(3, 1fr)" bg={"white"} boxShadow={"base"} rounded={8} p={4} w={"100%"}>
+            <GridItem>
                 <Heading size={"sm"}>{name}</Heading>
             </GridItem>
-            <GridItem rowSpan={2}>{desc}</GridItem>
+            <GridItem>{desc}</GridItem>
+            <GridItem>
+                <Text fontSize={"xs"} textAlign={"right"}>
+                    {new Date(date).toLocaleString("th-TH", { timeZone: "Asia/Bangkok" })}
+                </Text>
+            </GridItem>
         </Grid>
     )
 }
