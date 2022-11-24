a90a0ccfac07878410ef77ca240440072bf64a65
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2022-11-24 21:45:33 +0700

    [UPDATE] create post connect with prisma

--- backend/src/modules/announcement/routes/createpost.ts
+++ backend/src/modules/announcement/routes/createpost.ts
@@ -2,35 +2,84 @@ import { post } from "@apiType/announcement"
 import { Request, Response } from "express"
 import { getPost, setPost } from ".."
 
-const createPost = (req: Request, res: Response) => {
+const createPost = async (req: Request, res: Response) => {
+    const prisma = res.prisma
     const id = req.user?.userId
     const { topic, detail, targetType, targetValue, expiredPost, addmorelang } = req.body
-    const newPost = {
-        postId: Date.now(),
-        userId: req.user?.userId || "",
-        lang_id: 1000,
-        topic: topic,
-        detail: detail,
-        sender: "SAMO-SIT",
-        status: "waiting",
-        pinStatus: false,
-        isApprove: false,
-        targetType: targetType,
-        targetValue: targetValue,
-        postAt: new Date(),
-        expiredOfPost: expiredPost,
-        expiredAfterDelete: new Date(),
-        addMoreLang: addmorelang,
+    const fId = Date.now();
+    try {
+        const newFilter = await prisma.announcement_Filter.create({
+            data: {
+                filterId:fId,
+                filterType:targetType,
+                value:targetValue
+            }
+        })
+        // let lang;
+        // for(let i =0;i<addmorelang.length;i++){
+        //     const lang = addmorelang[i].map((el:any) => ({
+        //         languageId:el.lang_id, annTopic:el.topic,annDetail:el.detail
+        //     }))
+        // }
+        //  = addmorelang.map((el:any) => (
+        //     {languageId:el.lang_id,annTopic:el.topic,annDetail:el.detail}
+        // ))
+       
+        const ll = addmorelang.map((el:any) => (
+            {languageId:el.languageId,annTopic:el.annTopic,annDetail:el.annDetail}
+        ))
+        ll.push({languageId:1000,annTopic:topic,annDetail:detail})
+        console.log(ll);
+        
+        const newPost = await prisma.announcement.create({
+            data: {
+                annExpired:expiredPost,
+                userId:"w5XS9xnYoChZzXup0Jn2K",
+                filterId:fId,
+                annLanguage:{
+                    createMany:{data:ll}
+                },
+                annPost:{
+                    create:{}
+                },
+                annPin:{
+                    create:{
+                        userId:"w5XS9xnYoChZzXup0Jn2K"
+                    }
+                },
+            }
+        })
+
+        res.status(200)
+    }catch(err) {
+        res.status(400).send(err)
     }
-    let newData: post[] = []
-    for (let i = 0; i < getPost().length; i++) {
-        newData.push(getPost()[i])
-    }
-    newData.push(newPost)
+    // const newPost = {
+    //     postId: Date.now(),
+    //     userId: req.user?.userId || "",
+    //     lang_id: 1000,
+    //     topic: topic,
+    //     detail: detail,
+    //     sender: "SAMO-SIT",
+    //     status: "waiting",
+    //     pinStatus: false,
+    //     isApprove: false,
+    //     targetType: targetType,
+    //     targetValue: targetValue,
+    //     postAt: new Date(),
+    //     expiredOfPost: expiredPost,
+    //     expiredAfterDelete: new Date(),
+    //     addMoreLang: addmorelang,
+    // }
+    // let newData: post[] = []
+    // for (let i = 0; i < getPost().length; i++) {
+    //     newData.push(getPost()[i])
+    // }
+    // newData.push(newPost)
     // setPost([getPost().map((el) => (el)), newPost])
     // console.log(newData);
-    setPost(newData)
-    return res.send(newPost)
+    // setPost(newData)
+    // return res.send(newPost)
     // console.log(newData);
 
     // setPost(newData)
