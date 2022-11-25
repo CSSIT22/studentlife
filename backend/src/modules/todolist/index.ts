5295ffa14806fddbb05565f0fe28d8eace8dcfbd
parnlees <yanisa0439@gmail.com>
2022-11-25 15:42:06 +0700

    array

--- backend/src/modules/todolist/index.ts
+++ backend/src/modules/todolist/index.ts
@@ -1,28 +1,11 @@
 import express from "express"
 import createTask from "./routes/createtask"
+import listTask from "./routes/listtask"
 
 const todolistRoutes = express()
 todolistRoutes.use(express.json())
 
-todolistRoutes.put("/createtask", (req, res) => {
-    const taskName = req.body.taskName
-    const taskId = req.body.taskId
-    const taskUserId = req.body.user
-    const taskDesc = req.body.taskDesc
-    const created = req.body.created
-    const due = req.body.due
-    const taskType = req.body.taskType
-
-    res.send({
-        success: true,
-        name: taskName,
-        id: taskId,
-        taskUserId: taskUserId,
-        taskDesc: taskDesc,
-        created: created,
-        due: due,
-        taskType: taskType,
-    })
-})
+todolistRoutes.put("/createtask", createTask)
+todolistRoutes.get("/listtask", listTask)
 
 export default todolistRoutes
--- frontend/src/pages/todolist/index.tsx
+++ frontend/src/pages/todolist/index.tsx
@@ -1,143 +1,109 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import {
-    List,
-    ListItem,
-    ListIcon,
-    OrderedList,
-    UnorderedList,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    useDisclosure,
-    Button,
-    Image,
-    Heading,
-    Input,
-    Center,
-    Select,
-    Spacer,
-    Box,
-    StackDivider,
-    VStack,
-    GridItem,
-    Flex,
-    Text,
-    Link,
-    ButtonGroup,
-    MenuButton,
-    Menu,
-    MenuItem,
-    MenuList,
-    Progress,
+  List,
+  ListItem,
+  ListIcon,
+  OrderedList,
+  UnorderedList,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+  Image,
+  Heading,
+  Input,
+  Center,
+  Select,
+  Spacer,
+  Box,
+  StackDivider,
+  VStack,
+  GridItem,
+  Flex,
+  Text,
+  Link,
+  ButtonGroup,
+  MenuButton,
+  Menu,
+  MenuItem,
+  MenuList,
+  Progress,
 } from "@chakra-ui/react"
 import AppBody from "src/components/share/app/AppBody"
 import { PhoneIcon, AddIcon, ArrowRightIcon, ChevronDownIcon } from "@chakra-ui/icons"
 import { useBreakpointValue } from "@chakra-ui/react"
 import ToDoListAppBody from "src/components/todolist/ToDoListAppBody"
+import axios from "axios"
 
 
 const index = () => {
-    return (
-        <ToDoListAppBody>
-            <Flex minWidth="max-content" alignItems="center" gap="2" flexDirection={["column", "row"]}>
-                <Box p="2">
-                    <Heading size="2xl">To Do List</Heading>
-                </Box>
-                <Spacer />
-                <Menu>
-                    <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-                        Sorting
-                    </MenuButton>
-                    <Button colorScheme="orange">
-                        <Link href="/todolist/folderpage">Folder</Link>
-                    </Button>
-                    <MenuList>
-                        <MenuItem>Due Date</MenuItem>
-                        <MenuItem>A-Z</MenuItem>
-                        <MenuItem>Complete</MenuItem>
-                        <MenuItem>Incomplete</MenuItem>
-                    </MenuList>
-                </Menu>
+  const [taskList, setTaskList] = useState([])
+
+  const fetchTaskList = async () => {
+    const res = await axios.get("http://localhost:8000/todolist/listtask", {
+      withCredentials: true,
+    })
+
+    console.log(res);
+  }
+
+  useEffect(() => {
+    fetchTaskList();
+  }, [])
+
+  return (
+    <ToDoListAppBody>
+      <Flex minWidth="max-content" alignItems="center" gap="2" flexDirection={["column", "row"]}>
+        <Box p="2">
+          <Heading size="2xl">To Do List</Heading>
+        </Box>
+        <Spacer />
+        <Menu>
+          <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+            Sorting
+          </MenuButton>
+          <Button colorScheme="orange">
+            <Link href="/todolist/folderpage">Folder</Link>
+          </Button>
+          <MenuList>
+            <MenuItem>Due Date</MenuItem>
+            <MenuItem>A-Z</MenuItem>
+            <MenuItem>Complete</MenuItem>
+            <MenuItem>Incomplete</MenuItem>
+          </MenuList>
+        </Menu>
+      </Flex>
+
+      {
+        [].map((el) => (
+          <Box height={"5rem"} width={"100%"} p="5" mt="5" backgroundColor="#ECECEC" rounded="lg">
+            <Flex alignItems={"center"}>
+              <ArrowRightIcon w={3} h={3} color="red.500" marginRight={3} />
+              <Box>
+                <Text fontSize={"2xl"}>{el.taskName}</Text>
+              </Box>
+              <Spacer />
+              <Box textAlign={"right"} as="b" pr={"1rem"} color="green">
+                {el.isChecked && "Finished"}
+              </Box>
             </Flex>
-            <Link href="/todolist/task">
-                <Box height={"5rem"} width={"100%"} p="5" mt="5" backgroundColor="#ECECEC" rounded="lg">
-                    <Flex alignItems={"center"}>
-                        <ArrowRightIcon w={3} h={3} color="red.500" marginRight={3} />
-                        <Box>
-                            <Text fontSize={"2xl"}>CSC210 : Work 1</Text>
-                        </Box>
-                        <Spacer />
-                        <Box textAlign={"right"} as="b" pr={"1rem"} color="green">
-                            Finished
-                        </Box>
-                    </Flex>
-                </Box>
-            </Link>
+          </Box>
+        ))
+      }
 
-            <Box height={"5rem"} width={"100%"} p="5" mt="5" backgroundColor="#ECECEC" rounded="lg">
-                <Flex alignItems={"center"}>
-                    <ArrowRightIcon w={3} h={3} color="red.500" marginRight={3} />
-                    <Box>
-                        <Text as="b" fontSize={"2xl"}>
-                            CSC210 : Work 2
-                        </Text>
-                    </Box>
-                    <Spacer />
-                    <Box textAlign={"right"} pr={"1rem"} width=""></Box>
-                </Flex>
-            </Box>
-
-            <Box height={"5rem"} width={"100%"} p="5" mt="5" backgroundColor="#ECECEC" rounded="lg">
-                <Flex alignItems={"center"}>
-                    <ArrowRightIcon w={3} h={3} color="red.500" marginRight={3} />
-                    <Box>
-                        <Text fontSize={"2xl"}>CSC220</Text>
-                    </Box>
-                    <Spacer />
-                    <Box textAlign={"right"} as="b" pr={"1rem"} color="green">
-                        Finished
-                    </Box>
-                </Flex>
-            </Box>
-
-            <Box height={"5rem"} width={"100%"} p="5" mt="5" backgroundColor="#ECECEC" rounded="lg">
-                <Flex alignItems={"center"}>
-                    <ArrowRightIcon w={3} h={3} color="red.500" marginRight={3} />
-                    <Box>
-                        <Text as="b" fontSize={"2xl"}>
-                            CSC213
-                        </Text>
-                    </Box>
-                    <Spacer />
-                    <Box textAlign={"right"} pr={"1rem"} width=""></Box>
-                </Flex>
-            </Box>
-
-            <Box height={"5rem"} width={"100%"} p="5" mt="5" backgroundColor="#ECECEC" rounded="lg">
-                <Flex alignItems={"center"}>
-                    <ArrowRightIcon w={3} h={3} color="red.500" marginRight={3} />
-                    <Box>
-                        <Text fontSize={"2xl"}>LNG220</Text>
-                    </Box>
-                    <Spacer />
-                    <Box textAlign={"right"} as="b" pr={"1rem"} color="red">
-                        Failed
-                    </Box>
-                </Flex>
-            </Box>
-
-            <Box p="5" alignItems={"center"}>
-                <Text fontSize={"2xl"} textAlign={"center"}>
-                    Work Progress
-                </Text>
-                <Progress marginTop={2} value={20} size="lg" colorScheme="orange" />
-            </Box>
-        </ToDoListAppBody >
-    )
+      <Box p="5" alignItems={"center"}>
+        <Text fontSize={"2xl"} textAlign={"center"}>
+          Work Progress
+        </Text>
+        <Progress marginTop={2} value={20} size="lg" colorScheme="orange" />
+      </Box>
+    </ToDoListAppBody >
+  )
 }
 
 export default index
