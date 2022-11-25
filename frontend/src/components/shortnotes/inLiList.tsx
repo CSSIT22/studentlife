9261ad0c938d78f42364e123942d24a01950b32e
Pasinun.w <pasinun.w@gmail.com>
2022-11-25 14:21:16 +0700

    [FIX] align text to left

--- frontend/src/components/shortnotes/inLiList.tsx
+++ frontend/src/components/shortnotes/inLiList.tsx
@@ -8,7 +8,7 @@ const inLiList: FC<{
     return (
         <Grid templateColumns="repeat(5, 1fr)" boxShadow={"md"} bg={"white"} rounded={8} p={5} w="100%" h={"100px"} alignItems={"center"} _hover={{ bg: '#f5f5f5' }} transition='all 0.2s cubic-bezier(.08,.52,.52,1)'>
             <GridItem colSpan={4}>
-                <Text>{name}</Text>
+                <Text textAlign={"left"}>{name}</Text>
             </GridItem>
             <GridItem colSpan={1} textAlign={"right"}>
                 <Text>{course}</Text>
