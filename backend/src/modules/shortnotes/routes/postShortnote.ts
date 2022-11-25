0d4ec8f45c83adb9e6197eeac4bafada96655932
Pasinun.w <pasinun.w@gmail.com>
2022-11-25 14:53:40 +0700

    [ADD] postShortnote setup

--- backend/src/modules/shortnotes/routes/postShortnote.ts
+++ backend/src/modules/shortnotes/routes/postShortnote.ts
@@ -6,23 +6,21 @@ const postShortnote = async (req: Request<any>, res: Response<any>) => {
         const prisma = res.prisma
         const user = req.user?.userId
 
-        const payload: any = {
-            course: req.body.courseId,
-            owner: req.body.userId,
-            isPublic: req.body.isPublic,
-            snName: req.body.snName,
-            snDesc: req.body.snDesc,
-            snLink: req.body.snLink,
-            //https://stackoverflow.com/questions/68874214/how-to-use-connectorcreate-with-many-to-many-in-prisma
-        }
-
         const sn = await prisma.sn_Head.create({
-            data: payload,
+            data: {
+                snId: req.body.snId,
+                courseId: req.body.courseId,
+                created: req.body.created,
+                userId: req.body.user,
+                isPublic: req.body.isPublic,
+                snName: req.body.snName,
+                snDesc: req.body.snDesc,
+                snLink: req.body.snLink,
+            },
         })
         res.send(sn)
     } catch (err) {
         console.log(err)
-
         return res.send(err)
     }
 }
