4780f1cab7150da1270fb2f00a1ff24d22572cfc
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2022-11-26 18:17:54 +0700

    [UPDATE] change name of status on front

--- frontend/src/components/annoucement/PostOnHistory.tsx
+++ frontend/src/components/annoucement/PostOnHistory.tsx
@@ -12,22 +12,22 @@ const PostOnHistory: FC<{
     onOpen: Function
 }> = ({ topic, sender, status, onClick, id, onOpen }) => {
     const state = (stat: string) => {
-        if (stat == "approve") {
+        if (stat == "Approve") {
             return <HiCheckCircle fontSize={"2rem"} color="#38A169" />
-        } else if (stat == "disapprove") {
+        } else if (stat == "Disapprove") {
             return <HiXCircle fontSize={"2rem"} color="#E53E3E" />
-        } else if (stat == "waiting") {
+        } else if (stat == "Waiting for Approve") {
             return <TbLoader fontSize={"2rem"} color="#CC4900" />
         }
     }
     const button = (status: string) => {
-        if (status == "approve" || status == "disapprove") {
+        if (status == "Approve" || status == "Disapprove") {
             return (
                 <Button fontSize={"0.7rem"} bg="#E53E3E" color="white" as={"b"}>
                     DELETE
                 </Button>
             )
-        } else if (status == "waiting") {
+        } else if (status == "Waiting for Approve") {
             return (
                 <Button fontSize={"0.7rem"} bg="#293B66" color="white" as={"b"}>
                     EDIT
