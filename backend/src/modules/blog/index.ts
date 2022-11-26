aa0e8ca7783b6278ff05a969c0dba61ce867eb2b
JoNoteJoMaMa <93032309+JoNoteJoMaMa@users.noreply.github.com>
2022-11-26 21:51:40 +0700

    change index

--- backend/src/modules/blog/index.ts
+++ backend/src/modules/blog/index.ts
@@ -2,4 +2,12 @@ import express from "express"
 
 const blogRoutes = express()
 
+blogRoutes.get("/getpost", (req, res) => {
+    res.send("Hello")
+})
+
+blogRoutes.post("/postcreating", (req, res) => {
+    res.send("Create Post")
+})
+
 export default blogRoutes
