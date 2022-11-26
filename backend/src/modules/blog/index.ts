fae309ccc5092b060df42fbaa77b3b00ae5ffae0
JoNoteJoMaMa <93032309+JoNoteJoMaMa@users.noreply.github.com>
2022-11-26 10:58:54 +0700

    test

--- backend/src/modules/blog/index.ts
+++ backend/src/modules/blog/index.ts
@@ -2,4 +2,12 @@ import express from "express"
 
 const blogRoutes = express()
 
+blogRoutes.get("/post/:post_id", (req, res) => {
+    res.send("Testๆ")
+})
+
+blogRoutes.post("/postcreating", (req, res) => {
+    res.send("Create Post")
+})
+
 export default blogRoutes
