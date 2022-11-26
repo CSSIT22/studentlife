737a941bab28e57a3e0e86b3d360a90afe23e7ff
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2022-11-27 02:37:40 +0700

    [UPDATE] edit detail post (remain some part)

--- backend/src/modules/announcement/routes/editdetailpost.ts
+++ backend/src/modules/announcement/routes/editdetailpost.ts
@@ -1,8 +1,8 @@
-import { post } from "@apiType/announcement"
+import { post, post_to_language } from "@apiType/announcement"
 import { Request, Response } from "express"
 import { getPost, setPost } from ".."
 
-const editDetailPost = (req: Request, res: Response) => {
+const editDetailPost = async (req: Request, res: Response) => {
     const postId = req.body.postid
     const topic = req.body.topic
     const detail = req.body.detail
@@ -11,24 +11,68 @@ const editDetailPost = (req: Request, res: Response) => {
     const postAt = req.body.postat
     const expiredpost = req.body.expiredpost
     const addmorelang = req.body.addMoreLang
-    let editpost: post | null = null
-    const newData = getPost().map((post) => {
-        if (post.postId == postId) {
-            post.annTopic = topic
-            post.annDetail = detail
-            post.targetType = targetType
-            post.targetValue = targetValue
-            post.postAt = postAt
-            post.expiredOfPost = expiredpost
-            post.addMoreLang = addmorelang
-            editpost = post
-        }
-        return post
-    })
+    const prisma = res.prisma
+   
+    // เหลือ update morelang
+    try {
+        let allLang = []
+        allLang.push({ languageId: 1000, annTopic: topic, annDetail: detail })
+        addmorelang?.map((el: any) => allLang.push({ languageId: el.languageId, annTopic: el.annTopic, annDetail: el.annDetail }))
+        console.log(allLang)
+        res.send(allLang)
+        const newTargetUser = await prisma.announcement_Filter.findFirst({
+            where: {
+                filterType: targetType,
+                value: targetValue,
+            },
+            select: {
+                filterId: true,
+            },
+        })
+
+        const updatePost = await prisma.announcement.update({
+            where: {
+                postId: postId,
+            },
+            data: {
+                annCreated: postAt,
+                annExpired: expiredpost,
+                filterId: newTargetUser?.filterId,
+                annLanguage:{
+                    updateMany:{
+                        where:{
+                            languageId:1000
+                        },
+                        data:{
+                            annTopic:topic,
+                            annDetail:detail
+                        }
+                    }
+                }
+            },
+        })
+    } catch (err) {
+        res.status(400)
+    }
+
+    // let editpost: post | null = null
+    // const newData = getPost().map((post) => {
+    //     if (post.postId == postId) {
+    //         post.annTopic = topic
+    //         post.annDetail = detail
+    //         post.targetType = targetType
+    //         post.targetValue = targetValue
+    //         post.postAt = postAt
+    //         post.expiredOfPost = expiredpost
+    //         post.addMoreLang = addmorelang
+    //         editpost = post
+    //     }
+    //     return post
+    // })
     // console.log(newData);
-    setPost(newData)
+    // setPost(newData)
     // console.log(editpost);
-    res.send(editpost)
+    // res.send(editpost)
 }
 
 export default editDetailPost
