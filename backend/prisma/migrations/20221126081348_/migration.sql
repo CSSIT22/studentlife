fafe63e86e3bf6c387da407c9e7ca63b087a988b
Erxical <erxinxiao@gmail.com>
2022-11-26 15:14:12 +0700

    [Removed] views (Shop_Product)

---
+++ backend/prisma/migrations/20221126081348_/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `views` on the `Shop_Product` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Analytic_User_Report" ALTER COLUMN "end" SET DEFAULT make_timestamp(3000, 1, 1, 0, 0, 0);
+
+-- AlterTable
+ALTER TABLE "QR" ALTER COLUMN "expired" SET DEFAULT make_timestamp(3000, 1, 1, 0, 0, 0);
+
+-- AlterTable
+ALTER TABLE "Shop_Product" DROP COLUMN "views";
+
+-- AlterTable
+ALTER TABLE "ShortLink" ALTER COLUMN "expired" SET DEFAULT make_timestamp(3000, 1, 1, 0, 0, 0);
+
+-- AlterTable
+ALTER TABLE "User_To_Room" ALTER COLUMN "lefted" SET DEFAULT make_timestamp(3000, 1, 1, 0, 0, 0);
