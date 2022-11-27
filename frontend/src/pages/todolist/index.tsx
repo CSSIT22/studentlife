0610454ee478cfea94bb9868e3b3cb3efb1b07e6
parnlees <yanisa0439@gmail.com>
2022-11-28 00:59:45 +0700

    fix bracket

--- frontend/src/pages/todolist/index.tsx
+++ frontend/src/pages/todolist/index.tsx
@@ -35,11 +35,9 @@ import {
   Progress,
 } from "@chakra-ui/react"
 import AppBody from "src/components/share/app/AppBody"
-import { PhoneIcon, AddIcon, ArrowRightIcon, ChevronDownIcon } from "@chakra-ui/icons"
-import { useBreakpointValue } from "@chakra-ui/react"
+import { ArrowRightIcon, ChevronDownIcon } from "@chakra-ui/icons"
 import ToDoListAppBody from "src/components/todolist/ToDoListAppBody"
 import axios from "axios"
-import { FaEllipsisH } from "react-icons/fa"
 
 const index = () => {
   const [taskList, setTaskList] = useState([])
@@ -51,7 +49,6 @@ const index = () => {
   //     console.log('data', res.data);
   //     setTaskList(res.data);
   // }
-
 
   useEffect(() => {
     // fetchTaskList();
@@ -83,9 +80,12 @@ const index = () => {
         </Menu>
       </Flex>
 
+
+
       {
         taskList.map((el: any) => (
-          <Box height={"5rem"} width={"100%"} p="5" mt="5" backgroundColor="#ECECEC" rounded="lg">
+          <Box height={"5rem"} width={"100%"} p="5" mt="5" backgroundColor="#ECECEC" rounded="lg" key={el.taskId}>
+
             <Flex alignItems={"center"}>
               <ArrowRightIcon w={3} h={3} color="red.500" marginRight={3} />
               <Box>
@@ -96,36 +96,21 @@ const index = () => {
                 {el.isChecked && "Finished"}
               </Box>
             </Flex>
+          </Box>
+        ))
+      }
+
+      <Box p="5" alignItems={"center"}>
+        <Text fontSize={"2xl"} textAlign={"center"}>
+          Work Progress
+        </Text>
+        <Progress marginTop={2} value={20} size="lg" colorScheme="orange" />
+      </Box>
+      )
 
 
-            {
-
-              taskList.map((el) => (
-                <Box height={"5rem"} width={"100%"} p="5" mt="5" backgroundColor="#ECECEC" rounded="lg" key={el.taskId}>
-
-                  <Flex alignItems={"center"}>
-                    <ArrowRightIcon w={3} h={3} color="red.500" marginRight={3} />
-                    <Box>
-                      <Text fontSize={"2xl"}>{el.taskName}</Text>
-                    </Box>
-                    <Spacer />
-                    <Box textAlign={"right"} as="b" pr={"1rem"} color="green">
-                      {el.isChecked && "Finished"}
-                    </Box>
-                  </Flex>
-                </Box>
-              ))
-            }
-
-
-            <Box p="5" alignItems={"center"}>
-              <Text fontSize={"2xl"} textAlign={"center"}>
-                Work Progress
-              </Text>
-              <Progress marginTop={2} value={20} size="lg" colorScheme="orange" />
-            </Box>
-          </ToDoListAppBody >
-        )
+    </ToDoListAppBody >
+  )
 }
 
-      export default index
+export default index
