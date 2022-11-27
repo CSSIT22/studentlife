6b7fd0484d9caf8d614ac99cc728187e6d954faf
icesaran3 <icesaran3@gmail.com>
2022-11-27 23:02:17 +0700

    [ADD] delete Library

---
+++ backend/src/modules/shortnotes/routes/deleteLibrary.ts
@@ -0,0 +1,21 @@
+import { prisma } from "@prisma/client"
+import { Request, Response } from "express"
+
+const deleteLibrary = async (req: Request<any>, res: Response<any>) => {
+    try {
+        const prisma = res.prisma
+        const user = req.user?.userId
+
+        const Li = await prisma.sn_Library.delete({
+            where: {
+                libId: req.body.libId,
+            },
+        })
+        res.send(Li)
+    } catch (err) {
+        console.log(err)
+        return res.send(err)
+    }
+}
+
+export default deleteLibrary
