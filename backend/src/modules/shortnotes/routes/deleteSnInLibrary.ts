79a26f224480e8a833be033a8533826167c71926
icesaran3 <icesaran3@gmail.com>
2022-11-27 23:02:53 +0700

    [ADD] delete shortnote in Library

---
+++ backend/src/modules/shortnotes/routes/deleteSnInLibrary.ts
@@ -0,0 +1,22 @@
+import { prisma } from "@prisma/client"
+import { Request, Response } from "express"
+
+const deleteSnInLibrary = async (req: Request<any>, res: Response<any>) => {
+    try {
+        const prisma = res.prisma
+        const user = req.user?.userId
+            
+        const sn = await prisma.sn_In_Library.deleteMany({
+            where: {
+                libId: req.body.libId,
+                snId: req.body.snId,
+            },
+        })
+        res.send(sn)
+    } catch (err) {
+        console.log(err)
+        return res.send(err)
+    }
+}
+
+export default deleteSnInLibrary
