ef6907eebbea340a09ab98c7959c7d0eac8a809c
Phatdanai Shinpanjapol <phatdanai.shin@kmutt.ac.th>
2022-11-27 22:14:37 +0700

    [update] getcommunity

--- backend/src/modules/group/routes/getCommunity.ts
+++ backend/src/modules/group/routes/getCommunity.ts
@@ -2,18 +2,16 @@ import { Request, Response } from "express"
 
 const getCommunity = async (req: Request, res: Response) => {
     const prisma = res.prisma
-    const userId = req.body.user//req.user?.userId
+    const userId = req.user?.userId
 
     try {
         const communityUser = await prisma.community_User.findMany({
-            select: {
-                communityId: true,
-                status: true,
-            },
+
             where: {
                 userId: userId,
                 status: true
             },
+            
         })
 
         const communityUserInvite = await prisma.community_User.findMany({
@@ -28,43 +26,41 @@ const getCommunity = async (req: Request, res: Response) => {
         })
 
 
-        const commuinityJoin = await prisma.community.findMany({
+        const joinedCommunitys = await prisma.community.findMany({
             where: {
                 communityId: { in: communityUser.map((item: any) => item.communityId) },
             },
         })
 
 
-        const communityManage = await prisma.community.findMany({
+        const ownCommunitys = await prisma.community.findMany({
             where: {
                 communityOwnerId: userId,
             },
         })
 
 
-        const communityInvite = await prisma.community.findMany({
+        const invitations = await prisma.community.findMany({
             where: {
                 communityId: { in: communityUserInvite.map((item: any) => item.communityId) },
             },
         })
 
 
-        const countJoin = commuinityJoin.length
-        const countManage = communityManage.length
-        const countInvite = communityInvite.length
+        const countJoin = joinedCommunitys.length
+        const countManage = ownCommunitys.length
+        const countInvite = invitations.length
 
 
-        const commuinities: any[] = [
+        const commuinities: any = {
             
-            {commuinityJoin,countJoin},
-
-            {communityManage,countManage},
-
-            {communityInvite,countInvite}
+            ownCommunitys,
+            joinedCommunitys,
+            invitations
             
-        ]
+        }
 
-        res.status(200).json(commuinities)
+        res.send(commuinities)
     } catch (err) {
         res.status(404)
     }
