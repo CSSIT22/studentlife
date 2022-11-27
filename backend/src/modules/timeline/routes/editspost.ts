c8e382eaa6d957c2f559badf02f29059ff178059
RNU1815 <kunsee555@gmail.com>
2022-11-27 23:12:14 +0700

    Merge branch 'timeline' of https://github.com/CSSIT22/studentlife into timeline

--- backend/src/modules/timeline/routes/editspost.ts
+++ backend/src/modules/timeline/routes/editspost.ts
@@ -4,11 +4,39 @@ import { Post, getPost, setPost } from ".."
 const editedPost = (req: Request, res: Response) => {
     const id = req.body.id
     const name = req.body.name
+    const dateTime = req.body.dateTime
+    const message = req.body.message
+    const likes = req.body.likes
+    const comments = req.body.comments
+    const shares = req.body.shares
+    const avatar = req.body.avatar
+    const media = req.body.media
+
     let editedPost: Post | null = null
     const newdata = getPost().map((post) => {
         if (post.id == id) {
-            editedPost = { id: id, name: name }
-            return { id: id, name: name }
+            editedPost = {
+                id: id,
+                name: name,
+                dateTime: dateTime,
+                message: message,
+                likes: likes,
+                comments: comments,
+                shares: shares,
+                avatar: avatar,
+                media: media,
+            }
+            return {
+                id: id,
+                name: name,
+                dateTime: dateTime,
+                message: message,
+                likes: likes,
+                comments: comments,
+                shares: shares,
+                avatar: avatar,
+                media: media,
+            }
         }
         return post
     })
