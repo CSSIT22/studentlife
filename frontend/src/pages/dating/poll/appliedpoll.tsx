04841287e51e9057b2bf1fcfe6dd08455d487be7
IMFROHT <89932182+Tatathoraberd@users.noreply.github.com>
2022-11-28 02:31:00 +0700

    update matches and applied act poll page

--- frontend/src/pages/dating/poll/appliedpoll.tsx
+++ frontend/src/pages/dating/poll/appliedpoll.tsx
@@ -1,11 +1,13 @@
-import { HStack, Box, Center, useToast } from "@chakra-ui/react"
-import { useEffect, useState } from "react"
+import { HStack, Box, Center, useToast, Button,Text,Image } from "@chakra-ui/react"
+import React from "react"
+import React, { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import DatingAllActivityButton from "src/components/dating/DatingAllActivityButton"
 import DatingAppliedActivityButton from "src/components/dating/DatingAppliedActivityButton"
 import DatingYourActivityButton from "src/components/dating/DatingYourActivityButton"
 import API from "src/function/API"
 import DatingAppBody from "../../../components/dating/DatingAppBody"
+import ChatImg from "../../components/dating/pic/chat.png"
 
 const YourAppliedActivityPoll = () => {
     const didMount = useDidMount()
@@ -87,6 +89,114 @@ const YourAppliedActivityPoll = () => {
                     </HStack>
                 </Box>
             </Center>
+            <Box
+                w="100%"
+                minHeight={{ base: "95px", md: "129px" }}
+                backgroundColor="white"
+                boxShadow="0px 25px 50px -12px rgba(0, 0, 0, 0.25)"
+                mb={{ base: "8px", md: "25px" }}
+                borderRadius="10px"
+                mt="130px"
+            >
+                <Box h="90%" mb="35px">
+                    <Text pt="17px" pl="31px" pr="31px" color="black" fontWeight="700" fontSize={{ base: "20px", md: "26px" }} lineHeight="120%">
+                        Poll Header
+                    </Text>
+                    {isMobile ? (
+                        <Text ml="30px" fontWeight="700" fontSize="20px" lineHeight="133%" color="black">
+                            Firstname Lastname
+                        </Text>
+                    ) : (
+                        <Text ml="30px" fontSize="16px" lineHeight="133%" color="black">
+                            Firstname L.
+                        </Text>
+                    )}
+                </Box>
+                <Box display="flex" justifyContent="end" w="35%" alignItems="center" mr={{ base: "20px", md: "24px" }}>
+                    <Button
+                        borderRadius="full"
+                        w={{ base: "50px", md: "72px" }}
+                        h={{ base: "50px", md: "72px" }}
+                        backgroundColor="white"
+                        border="1px solid"
+                        boxShadow="0px 10px 15px -3px rgba(0, 0, 0, 0.1), 0px 4px 6px -2px rgba(0, 0, 0, 0.05)">
+                        <Image src={ChatImg} />
+                    </Button>
+                </Box>
+            </Box>
+
+            <Box
+                w="100%"
+                minHeight={{ base: "95px", md: "129px" }}
+                backgroundColor="white"
+                boxShadow="0px 25px 50px -12px rgba(0, 0, 0, 0.25)"
+                mb={{ base: "8px", md: "25px" }}
+                borderRadius="10px"
+            // mt="130px"
+            >
+                <Box h="90%" mb="35px" alignItems="center" w="65%">
+                    <Text pt="17px" pl="31px" pr="31px" color="black" fontWeight="700" fontSize={{ base: "20px", md: "26px" }} lineHeight="120%">
+                        Poll Header
+                    </Text>
+                    {isMobile ? (
+                        <Text ml="30px" fontWeight="700" fontSize="20px" lineHeight="133%" color="black">
+                            Firstname Lastname
+                        </Text>
+                    ) : (
+                        <Text ml="30px" fontSize="16px" lineHeight="133%" color="black">
+                            Firstname L.
+                        </Text>
+                    )}
+                    <Box display="flex" justifyContent="end" w="35%" alignItems="center" mr={{ base: "20px", md: "24px" }}>
+                        <Button
+                            borderRadius="full"
+                            w={{ base: "50px", md: "72px" }}
+                            h={{ base: "50px", md: "72px" }}
+                            backgroundColor="white"
+                            border="1px solid"
+                            boxShadow="0px 10px 15px -3px rgba(0, 0, 0, 0.1), 0px 4px 6px -2px rgba(0, 0, 0, 0.05)">
+                            <Image src={ChatImg} />
+                        </Button>
+                    </Box>
+                </Box>
+            </Box>
+
+            <Box w="100%"
+                height={{ base: "90px", md: "100px" }}
+                backgroundColor="white"
+                boxShadow="0px 25px 50px -12px rgba(0, 0, 0, 0.25)"
+                borderRadius="10px"
+                mb={{ base: "8px", md: "12px" }}
+                display="flex">
+
+                <Box h="90%" mb="35px">
+                    <Text pt="17px" pl="31px" pr="31px" color="black" fontWeight="700" fontSize={{ base: "20px", md: "26px" }} lineHeight="120%">
+                        Poll Header
+                    </Text>
+                    {isMobile ? (
+                        <Text ml="30px" fontWeight="700" fontSize="20px" lineHeight="133%" color="black">
+                            Firstname Lastname
+                        </Text>
+                    ) : (
+                        <Text ml="30px" fontSize="16px" lineHeight="133%" color="black">
+                            Firstname L.
+                        </Text>
+                    )}
+                </Box>
+                <Box display="flex" justifyContent="end" w="35%" alignItems="center" mr={{ base: "20px", md: "24px" }}>
+                    <Button
+                        borderRadius="full"
+                        w={{ base: "50px", md: "72px" }}
+                        h={{ base: "50px", md: "72px" }}
+                        backgroundColor="white"
+                        border="1px solid"
+                        boxShadow="0px 10px 15px -3px rgba(0, 0, 0, 0.1), 0px 4px 6px -2px rgba(0, 0, 0, 0.05)">
+                        <Image src={ChatImg} />
+                    </Button>
+                </Box>
+            </Box>
+            
+
         </DatingAppBody>
     )
 }
