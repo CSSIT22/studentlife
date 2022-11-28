821dc896b0a297b0476a36c91257c2de5f65bc01
Awat Nimsiriwangso <98152926+phoomaw10@users.noreply.github.com>
2022-11-28 13:16:56 +0700

    connectdb-desc

--- frontend/src/pages/todolist/folderpage.tsx
+++ frontend/src/pages/todolist/folderpage.tsx
@@ -37,7 +37,7 @@ import axios from "axios"
 const folderpage = () => {
   const { isOpen: isCreateOpen, onOpen: onCreateOpen, onClose: onCreateClose } = useDisclosure()
 
-  const [folderList, setFolderList] = useState()
+  const [folderList, setFolderList] = useState([])
 
   const fetchFolderList = async () => {
     const res = await axios.get("http://localhost:8000/todolist/listfolder", {
@@ -48,47 +48,48 @@ const folderpage = () => {
   }
 
   useEffect(() => {
-  fetchFolderList();
-}, [])
+    fetchFolderList();
+  }, [])
 
-return (
-  <ToDoListAppBody>
-    <Heading as="h1" size="3xl">
-      Folder Page
-    </Heading>
-    <Box display="flex" justifyContent="end" alignItems="center" marginY={10}>
-      <Button bgColor="orange.200" onClick={onCreateOpen}>
-        <AddIcon />
-      </Button>
-    </Box>
+  return (
+    <ToDoListAppBody>
+      <Heading as="h1" size="3xl">
+        Folder Page
+      </Heading>
+      <Box display="flex" justifyContent="end" alignItems="center" marginY={10}>
+        <Button bgColor="orange.200" onClick={onCreateOpen}>
+          <AddIcon />
+        </Button>
+      </Box>
 
+      {
+        folderList.map((el: any) => (
+          <Modal isOpen={isCreateOpen} onClose={onCreateClose}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Create Folder</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <Heading as="h2" size="ms" noOfLines={1} display="flex" alignItems="center">
+                  <AiFillFolder />
+                  Folder Name
+                </Heading>
+                <Input placeholder="Folder Name" size="md" />
+              </ModalBody>
 
-    {
-      taskList.map((el: any) => (
-        <Modal isOpen={isCreateOpen} onClose={onCreateClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Create Folder</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-              <Heading as="h2" size="ms" noOfLines={1} display="flex" alignItems="center">
-                <AiFillFolder />
-                Folder Name
-              </Heading>
-              <Input placeholder="Folder Name" size="md" />
-            </ModalBody>
+              <ModalFooter>
+                <Button variant="ghost" mr={3} onClick={onCreateClose}>
+                  Close
+                </Button>
+                <Button colorScheme="blue">Done</Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
 
-            <ModalFooter>
-              <Button variant="ghost" mr={3} onClick={onCreateClose}>
-                Close
-              </Button>
-              <Button colorScheme="blue">Done</Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
+        ))
+      }
     </ToDoListAppBody>
   )
-   
-    } 
- 
+}
+
 export default folderpage
