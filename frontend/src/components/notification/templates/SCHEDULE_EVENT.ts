d6357b03e8d63c1882d96677c78055b0bf6eea7c
Ryyyyyy12 <kevalin.sris@kmutt.ac.th>
2022-11-28 18:21:21 +0700

    final sprint#3

--- frontend/src/components/notification/templates/SCHEDULE_EVENT.ts
+++ frontend/src/components/notification/templates/SCHEDULE_EVENT.ts
@@ -1,5 +1,5 @@
 export const SCHEDULE_EVENT = [
     {
-        template: "<p><b>v1</b> will start at v2</p>"
-    }//[NewEvent_title] will start at [Event_startTime]
+        template: "<p><b>v1</b> will start at v2</p>",
+    }, //[NewEvent_title] will start at [Event_startTime]
 ]
--- frontend/src/components/notification/templates/transaction_success.ts
+++ frontend/src/components/notification/templates/transaction_success.ts
@@ -1,5 +1,5 @@
 export const TRANSACTION_SUCCESS = [
     {
-        template: "v1  your payment order:vs was successful"
-    }//v1=payment time, v2=order id
+        template: "v1  your payment order:v2 was successful",
+    }, //v1=payment time, v2=order id
 ]
