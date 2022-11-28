cdb686db5552abe957f9fdc98e49347f0ab080f3
Pasinun.w <95624019+Unuisad@users.noreply.github.com>
2022-11-28 22:27:21 +0700

    [ADD] loading + index transition

--- frontend/src/components/shortnotes/snComments.tsx
+++ frontend/src/components/shortnotes/snComments.tsx
@@ -1,8 +1,10 @@
-import { Box, Button, Flex, Heading, Textarea, useToast, VStack } from '@chakra-ui/react'
+import { Box, Button, Flex, Heading, Textarea, useBoolean, useToast, VStack } from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import API from 'src/function/API'
 import CmList from './cmList'
+import Lottie from "lottie-react";
+import loading from "./lottie/loading.json";
 
 const snComments = () => {
     const [comments, setComments] = useState<any>([])
@@ -11,6 +13,8 @@ const snComments = () => {
     useEffect(() => {
         API.get("shortnotes/getComments/" + param.id).then((item) => {
             setComments(item.data)
+        }).finally(() => {
+            setCmLoad.off()
         })
     }, [])
 
@@ -22,6 +26,10 @@ const snComments = () => {
         })
     }
     const toast = useToast()
+    const [cmLoad, setCmLoad] = useBoolean(true)
+    const style = {
+        height: 96,
+    };
     return (
         <Box>
             <Box mb={4} rounded={8}>
@@ -46,15 +54,20 @@ const snComments = () => {
                     </Flex>
                 </Box>
             </Box>
-            <VStack gap={4}>
-                {comments.map((cm: any) => (
-                    <CmList
-                        name={cm.commentor.fName + " " + cm.commentor.lName}
-                        desc={cm.comment}
-                        date={cm.commentedAt}
-                    />
-                ))}
-            </VStack>
+            {cmLoad ? <Box><Lottie style={style} animationData={loading}></Lottie></Box>
+
+                :
+                <VStack gap={4}>
+                    {comments.map((cm: any) => (
+                        <CmList
+                            name={cm.commentor.fName + " " + cm.commentor.lName}
+                            desc={cm.comment}
+                            date={cm.commentedAt}
+                        />
+                    ))}
+                </VStack>
+            }
+
         </Box>
 
     )
--- frontend/src/pages/shortnotes/index.tsx
+++ frontend/src/pages/shortnotes/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Text, Flex, Spacer, HStack, SimpleGrid, VStack, Select, useDisclosure, Stack, Collapse, SlideFade } from "@chakra-ui/react"
+import { Box, Heading, Text, Flex, Spacer, HStack, SimpleGrid, VStack, Select, useDisclosure, Stack, Collapse, SlideFade, useBoolean } from "@chakra-ui/react"
 import React, { useEffect, useState } from "react"
 import { Link, Navigate, useNavigate } from "react-router-dom"
 import AppBody from "../../components/share/app/AppBody"
@@ -9,12 +9,16 @@ import BtnMl from "../../components/shortnotes/btnMyLibrary"
 import BtnNs from "../../components/shortnotes/btnNewShortnote"
 import { FaLock } from "react-icons/fa"
 import API from "src/function/API"
+import Lottie from "lottie-react";
+import loading from "./lottie/loading.json";
 const index = () => {
     const [sn, setSn] = useState([])
     useEffect(() => {
         API.get("/shortnotes/getShortnotes").then((item) => {
             setSn(item.data)
             //console.log(item.data)
+        }).finally(() => {
+            setsnLoad.off()
             onToggle()
         })
     }, [])
@@ -24,6 +28,9 @@ const index = () => {
         API.get("/shortnotes/getResentShortnotes").then((item) => {
             setRsn(item.data)
             //console.log(item.data)
+
+        }).finally(() => {
+            setRsnLoad.off()
             rsnOnToggle()
         })
     }, [])
@@ -66,6 +73,11 @@ const index = () => {
     }
     const navigate = useNavigate()
 
+    const [rsnLoad, setRsnLoad] = useBoolean(true)
+    const [snLoad, setsnLoad] = useBoolean(true)
+    const style = {
+        height: 100,
+    };
     return (
         <AppBody>
             {/*Recent view list section*/}
@@ -76,22 +88,29 @@ const index = () => {
                 <Spacer />
                 <BtnMl />
             </HStack>
-            <SlideFade in={isOpen} offsetY='20px'>
-                <Box mt={4} mb={12}>
-                    <SimpleGrid columns={{ base: 1, md: 3 }} gap={{ base: 4, sm: 6 }} textAlign={"center"}>
-                        {rsn.map((rsn: any, key) => (
-                            <Box as="button" onClick={() => {
-                                navigate({
-                                    pathname: "./" + rsn.shortNote.snId,
-                                })
-                            }}>
-                                <Rsn key={key} topic={rsn.shortNote.snName} viewAt={rsn.viewedAt} lock={rsn.shortNote.isPublic}></Rsn>
+            {rsnLoad ?
+                <Box h={152}>
 
-                            </Box>
-                        ))}
-                    </SimpleGrid>
+                    <Lottie style={style} animationData={loading}></Lottie>
                 </Box>
-            </SlideFade>
+                :
+                <SlideFade in={isOpen} offsetY='20px'>
+                    <Box mt={4} mb={12}>
+                        <SimpleGrid columns={{ base: 1, md: 3 }} gap={{ base: 4, sm: 6 }} textAlign={"center"}>
+                            {rsn.map((rsn: any, key) => (
+                                <Box as="button" onClick={() => {
+                                    navigate({
+                                        pathname: "./" + rsn.shortNote.snId,
+                                    })
+                                }}>
+                                    <Rsn key={key} topic={rsn.shortNote.snName} viewAt={rsn.viewedAt} lock={rsn.shortNote.isPublic}></Rsn>
+
+                                </Box>
+                            ))}
+                        </SimpleGrid>
+                    </Box>
+                </SlideFade>
+            }
 
             {/*Shortnote list section*/}
             <Flex alignItems={"end"}>
@@ -116,47 +135,51 @@ const index = () => {
                     </VStack>
                 </Stack>
             </Flex>
-            <Collapse in={rsnIsOpen} animateOpacity>
-                <VStack gap={2} pt={4}>
-                    {coursePicked == "" ? (
-                        <>
-                            {sn.map((sn: any, key) => (
-                                <Box
-                                    as="button"
-                                    w={"100%"}
-                                    onClick={() => {
-                                        setSnPicked(sn.id)
-                                        navigate({
-                                            pathname: "./" + sn.snId,
-                                        })
-                                        console.log(snPicked)
-                                    }}
-                                >
-                                    <SnList key={key} topic={sn.snName} course={sn.course.courseName} date={sn.created} lock={sn.isPublic ? "" : <FaLock />} />
-                                </Box>
-                            ))}
-                        </>
-                    ) : (
-                        <>
-                            {filtered.map((sn: any, key: any) => (
-                                <Box
-                                    as="button"
-                                    w={"100%"}
-                                    onClick={() => {
-                                        setSnPicked(sn.id)
-                                        navigate({
-                                            pathname: "./" + sn.snId,
-                                        })
-                                        console.log(snPicked)
-                                    }}
-                                >
-                                    <SnList key={key} topic={sn.snName} course={sn.courseId} date={sn.created} lock={sn.isPublic ? "" : <FaLock />} />
-                                </Box>
-                            ))}
-                        </>
-                    )}
-                </VStack>
-            </Collapse>
+            {snLoad ? <Lottie style={style} animationData={loading}></Lottie>
+                :
+                <Collapse in={rsnIsOpen} animateOpacity>
+                    <VStack gap={2} pt={4}>
+                        {coursePicked == "" ? (
+                            <>
+                                {sn.map((sn: any, key) => (
+                                    <Box
+                                        as="button"
+                                        w={"100%"}
+                                        onClick={() => {
+                                            setSnPicked(sn.id)
+                                            navigate({
+                                                pathname: "./" + sn.snId,
+                                            })
+                                            console.log(snPicked)
+                                        }}
+                                    >
+                                        <SnList key={key} topic={sn.snName} course={sn.course.courseName} date={sn.created} lock={sn.isPublic ? "" : <FaLock />} />
+                                    </Box>
+                                ))}
+                            </>
+                        ) : (
+                            <>
+                                {filtered.map((sn: any, key: any) => (
+                                    <Box
+                                        as="button"
+                                        w={"100%"}
+                                        onClick={() => {
+                                            setSnPicked(sn.id)
+                                            navigate({
+                                                pathname: "./" + sn.snId,
+                                            })
+                                            console.log(snPicked)
+                                        }}
+                                    >
+                                        <SnList key={key} topic={sn.snName} course={sn.courseId} date={sn.created} lock={sn.isPublic ? "" : <FaLock />} />
+                                    </Box>
+                                ))}
+                            </>
+                        )}
+                    </VStack>
+                </Collapse>
+            }
+
         </AppBody>
     )
 }
