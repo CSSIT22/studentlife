00e63df9933a57c85793202900cf9075bf37ae18
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2022-11-28 16:39:27 +0700

    tt

--- backend/src/modules/shortlink/index.ts
+++ backend/src/modules/shortlink/index.ts
@@ -1,14 +1,31 @@
 import express from "express"
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 const shortlinkRoutes = express()
 
-shortlinkRoutes.get('/',async(req,res)=>{
-    const prisma = res.prisma
-    const result = await prisma.shortLink.createMany({
-        
+shortlinkRoutes.get('/test',async(req,res)=>{
+    const allUsers = await prisma.shortLink.findMany()
+    res.json(allUsers)
     })
+
+shortlinkRoutes.get('/shortlink' ,async (req,res)=>{
+    const allUsers = await prisma.shortLink.findMany()
+    res.status(200).json(res)
+    console.log(allUsers)
+    res.json({msg:"Hello world"})
 })
 
-
-
+shortlinkRoutes.post('/test' , async(req,res)=>{
+    const[name1,link] = req.body;
+    const product = await prisma.shortLink.create({
+        data:{
+            name:name1,
+            link:link
+            
+        }
+    })
+    res.status(201).json
+})
 export default shortlinkRoutes
+
