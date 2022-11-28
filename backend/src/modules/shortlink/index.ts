0d513b08d6d61c413300eb33ba93d2a98b6b76be
PoomPak <passapolpoom@gmail.com>
2022-11-28 16:29:55 +0700

    pooh

--- backend/src/modules/shortlink/index.ts
+++ backend/src/modules/shortlink/index.ts
@@ -1,6 +1,14 @@
 import express from "express"
+import shortenlink from "./route/shortenlink"
 
 const shortlinkRoutes = express()
+shortlinkRoutes.use(express.json())
+
+shortlinkRoutes.post("/generate", shortenlink)
+
+shortlinkRoutes.get("/test", (req, res) => {
+    res.send("himom")
+})
 
 shortlinkRoutes.get('/',async(req,res)=>{
     const prisma = res.prisma
