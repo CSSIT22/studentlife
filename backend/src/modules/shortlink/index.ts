0a02189b07d373e15576794196b8526a86f57f25
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2022-11-28 11:04:05 +0700

    Update index.ts

--- backend/src/modules/shortlink/index.ts
+++ backend/src/modules/shortlink/index.ts
@@ -2,4 +2,13 @@ import express from "express"
 
 const shortlinkRoutes = express()
 
+shortlinkRoutes.get('/',async(req,res)=>{
+    const prisma = res.prisma
+    const result = await prisma.shortLink.createMany({
+        
+    })
+})
+
+
+
 export default shortlinkRoutes
