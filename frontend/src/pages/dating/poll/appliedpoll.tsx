82da531df525e2ce915f122223868f5376516203
IMFROHT <89932182+Tatathoraberd@users.noreply.github.com>
2022-11-29 01:08:39 +0700

    update applied act poll page

--- frontend/src/pages/dating/poll/appliedpoll.tsx
+++ frontend/src/pages/dating/poll/appliedpoll.tsx
@@ -1,4 +1,4 @@
-import { HStack, Box, Center, useToast, Button, Text, Image, useBreakpointValue } from "@chakra-ui/react"
+import { HStack, Box, Center, useToast, Button, Text, Image, useBreakpointValue, Flex, Spacer, Grid, ButtonGroup } from "@chakra-ui/react"
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import DatingAllActivityButton from "src/components/dating/DatingAllActivityButton"
@@ -168,10 +168,10 @@ const YourAppliedActivityPoll = () => {
                         </Button>
                     </Box>
                 </Box>
-            </Box> */} 
+            </Box> */}
 
             {/* 3 */}
-            <Box w="100%"
+            {/* <Box w="100%"
                 height={{ base: "90px", md: "100px" }}
                 backgroundColor="white"
                 boxShadow="0px 25px 50px -12px rgba(0, 0, 0, 0.25)"
@@ -205,17 +205,61 @@ const YourAppliedActivityPoll = () => {
                         <Image src={ChatImg} />
                     </Button>
                 </Box>
-            </Box>
+            </Box> */}
+
+            {/* 3 1*/}
+            {/* <Box w="100%"
+                height={{ base: "90px", md: "100px" }}
+                backgroundColor="white"
+                boxShadow="0px 25px 50px -12px rgba(0, 0, 0, 0.25)"
+                borderRadius="10px"
+                mb={{ base: "8px", md: "25px" }}
+            display="flex"
+            > */}
+{/* 
+                <Flex>
+                    <Box>
+                        <Text pt="17px" pl="31px" pr="31px" color="black" fontWeight="700" fontSize={{ base: "20px", md: "26px" }} lineHeight="120%">
+                            Poll Header
+                        </Text>
+                    </Box>
+                    <Spacer />
+                    <Box>
+                        <Text mt="15px" mr="30" lineHeight="133%" color="black" fontSize="15px">
+                            Your Poll Status
+                        </Text>
+                    </Box>
+                </Flex>
+
+                <Grid alignItems='center' templateColumns='repeat(2, 1fr)' gap={6}>
+                    <Box pt='6' pb='6'>
+                        <Text ml="30px" fontSize="20px" lineHeight="133%" color="black">
+                            Firstname Lastname
+                        </Text>
+                    </Box>
+                    <Flex>
+                        <Box>
+                            <Text mt="20px" lineHeight="133%" color="black" fontSize="15px" >
+                                Click to see more
+                            </Text>
+                        </Box>
+                        <Spacer />
+                        <ButtonGroup gap='2'>
+                            <Button colorScheme='teal'>Chat</Button>
+                        </ButtonGroup>
+                    </Flex>
+                </Grid>
+            </Box> */}
 
             {/* 4 */}
-            <Box w="100%"
+            {/* <Box w="100%"
                 height={{ base: "90px", md: "100px" }}
                 backgroundColor="white"
                 boxShadow="0px 25px 50px -12px rgba(0, 0, 0, 0.25)"
                 borderRadius="10px"
                 mb={{ base: "8px", md: "25px" }}
                 display="flex"
-                >
+            >
 
                 <Box h="90%" mb="35px">
                     <Text pt="17px" pl="31px" pr="31px" color="black" fontWeight="700" fontSize={{ base: "20px", md: "26px" }} lineHeight="120%">
@@ -253,8 +297,230 @@ const YourAppliedActivityPoll = () => {
                         <Image src={ChatImg} />
                     </Button>
                 </Box>
+            </Box> */}
+
+            {/* Test */}
+            {/* <Box backgroundColor="white"
+                boxShadow="0px 25px 50px -12px rgba(0, 0, 0, 0.25)"
+                borderRadius="10px"
+                mb={{ base: "8px", md: "25px" }}
+
+            >
+                <Flex>
+                    <Box>
+                        <Text pt="17px" pl="30px" pr="31px" color="black" fontWeight="700" fontSize={{ base: "20px", md: "26px" }} lineHeight="120%">
+                            Poll Header
+                        </Text>
+                    </Box>
+                    <Spacer />
+                    <Box>
+                        <Text mt="15px" mr="30px" lineHeight="133%" color="black" fontSize="15px">
+                            Your Poll Status
+                        </Text>
+                    </Box>
+                </Flex>
+
+                <Grid alignItems='center' templateColumns='repeat(2, 1fr)' gap={6}>
+                    <Box pt='6' pb='6'>
+                        {isMobile ? (
+                            <Text ml="30px" fontWeight="700" fontSize="20px" lineHeight="133%" color="black">
+                                Firstname Lastname
+                            </Text>
+                        ) : (
+                            <Text ml="30px" fontSize="16px" lineHeight="133%" color="black">
+                                Firstname L.
+                            </Text>
+                        )}
+                    </Box>
+                    <Flex>
+                        <Box>
+                            <Text mt="20px" lineHeight="133%" color="black" fontSize="15px" >
+                                Click to see more
+                            </Text>
+                        </Box>
+                        <Spacer />
+                        <Box display="flex" justifyContent="end" w="35%" alignItems="center" mr={{ base: "20px", md: "24px" }}>
+                            <Button
+                                borderRadius="full"
+                                w={{ base: "50px", md: "72px" }}
+                                h={{ base: "50px", md: "72px" }}
+                                backgroundColor="white"
+                                border="1px solid"
+                                mr={{ base: "12px", md: "24px" }}
+                                boxShadow="0px 10px 15px -3px rgba(0, 0, 0, 0.1), 0px 4px 6px -2px rgba(0, 0, 0, 0.05)"
+                            >
+                                <Image src={GroupChatImg} />
+                            </Button>
+                            <Button
+                                borderRadius="full"
+                                w={{ base: "50px", md: "72px" }}
+                                h={{ base: "50px", md: "72px" }}
+                                backgroundColor="white"
+                                border="1px solid"
+                                boxShadow="0px 10px 15px -3px rgba(0, 0, 0, 0.1), 0px 4px 6px -2px rgba(0, 0, 0, 0.05)">
+                                <Image src={ChatImg} />
+                            </Button>
+                        </Box> */}
+
+                        {/* <ButtonGroup gap='2' mr="30px">
+                            <Button
+                                borderRadius="full"
+                                w={{ base: "50px", md: "72px" }}
+                                h={{ base: "50px", md: "72px" }}
+                                backgroundColor="white"
+                                border="1px solid"
+                                mr={{ base: "12px", md: "24px" }}
+                                boxShadow="0px 10px 15px -3px rgba(0, 0, 0, 0.1), 0px 4px 6px -2px rgba(0, 0, 0, 0.05)"
+                            >
+                                <Image src={GroupChatImg} />
+                            </Button>
+                            <Button
+                                borderRadius="full"
+                                w={{ base: "50px", md: "72px" }}
+                                h={{ base: "50px", md: "72px" }}
+                                backgroundColor="white"
+                                border="1px solid"
+                                boxShadow="0px 10px 15px -3px rgba(0, 0, 0, 0.1), 0px 4px 6px -2px rgba(0, 0, 0, 0.05)">
+                                <Image src={ChatImg} />
+                            </Button>
+                        </ButtonGroup> */}
+                    {/* </Flex>
+                </Grid>
+            </Box> */}
+
+            {/* Test 1*/}
+            <Box backgroundColor="white"
+                boxShadow="0px 25px 50px -12px rgba(0, 0, 0, 0.25)"
+                borderRadius="10px"
+                mb={{ base: "8px", md: "25px" }}
+                mt="135px"
+            >
+                <Flex>
+                    <Box>
+                        <Text pt="17px" pl="30px" pr="31px" color="black" fontWeight="700" fontSize={{ base: "20px", md: "26px" }} lineHeight="120%">
+                            Poll Header
+                        </Text>
+                    </Box>
+                    <Spacer />
+                    <Box>
+                        <Text mt="15px" mr="30px" lineHeight="133%" color="black" fontSize="15px">
+                            Your Poll Status
+                        </Text>
+                    </Box>
+                </Flex>
+
+                <Flex>
+                    <Box pt='6' pb='6'>
+                        {isMobile ? (
+                            <Text ml="30px" fontWeight="700" fontSize="20px" lineHeight="133%" color="black">
+                                Firstname Lastname
+                            </Text>
+                        ) : (
+                            <Text ml="30px" fontSize="16px" lineHeight="133%" color="black">
+                                Firstname L.
+                            </Text>
+                        )}
+                    </Box>
+                    <Spacer />
+                    <Box display="flex" justifyContent="end" w="35%" alignItems="center" mr={{ base: "20px", md: "24px" }}>
+                        <Button
+                            borderRadius="full"
+                            w={{ base: "50px", md: "72px" }}
+                            h={{ base: "50px", md: "72px" }}
+                            backgroundColor="white"
+                            border="1px solid"
+                            mr={{ base: "12px", md: "24px" }}
+                            boxShadow="0px 10px 15px -3px rgba(0, 0, 0, 0.1), 0px 4px 6px -2px rgba(0, 0, 0, 0.05)"
+                        >
+                            <Image src={GroupChatImg} />
+                        </Button>
+                        <Button
+                            borderRadius="full"
+                            w={{ base: "50px", md: "72px" }}
+                            h={{ base: "50px", md: "72px" }}
+                            backgroundColor="white"
+                            border="1px solid"
+                            boxShadow="0px 10px 15px -3px rgba(0, 0, 0, 0.1), 0px 4px 6px -2px rgba(0, 0, 0, 0.05)">
+                            <Image src={ChatImg} />
+                        </Button>
+                    </Box>
+                </Flex>
+
+                <Box >
+                    <Center>
+                        <Text lineHeight="133%" color="black" fontSize="15px" as='u' mb="20px">
+                            Click to see more
+                        </Text>
+                    </Center>
+                </Box>
             </Box>
 
+            {/* Test 2*/}
+            <Box backgroundColor="white"
+                boxShadow="0px 25px 50px -12px rgba(0, 0, 0, 0.25)"
+                borderRadius="10px"
+                mb={{ base: "8px", md: "25px" }}
+
+            >
+                <Flex>
+                    <Box>
+                        <Text pt="17px" pl="30px" pr="31px" color="black" fontWeight="700" fontSize={{ base: "20px", md: "26px" }} lineHeight="120%">
+                            Poll Header
+                        </Text>
+                    </Box>
+                    <Spacer />
+                    <Box>
+                        <Text mt="15px" mr="30px" lineHeight="133%" color="black" fontSize="15px">
+                            Your Poll Status
+                        </Text>
+                    </Box>
+                </Flex>
+
+                <Flex>
+                    <Box pt='6' pb='6'>
+                        {isMobile ? (
+                            <Text ml="30px" fontWeight="700" fontSize="20px" lineHeight="133%" color="black">
+                                Firstname Lastname
+                            </Text>
+                        ) : (
+                            <Text ml="30px" fontSize="16px" lineHeight="133%" color="black">
+                                Firstname L.
+                            </Text>
+                        )}
+                    </Box>
+                    <Spacer />
+                    <Box display="flex" justifyContent="end" w="35%" alignItems="center" mr={{ base: "20px", md: "24px" }}>
+                        <Button
+                            borderRadius="full"
+                            w={{ base: "50px", md: "72px" }}
+                            h={{ base: "50px", md: "72px" }}
+                            backgroundColor="white"
+                            border="1px solid"
+                            mr={{ base: "12px", md: "24px" }}
+                            boxShadow="0px 10px 15px -3px rgba(0, 0, 0, 0.1), 0px 4px 6px -2px rgba(0, 0, 0, 0.05)"
+                        >
+                            <Image src={GroupChatImg} />
+                        </Button>
+                        <Button
+                            borderRadius="full"
+                            w={{ base: "50px", md: "72px" }}
+                            h={{ base: "50px", md: "72px" }}
+                            backgroundColor="white"
+                            border="1px solid"
+                            boxShadow="0px 10px 15px -3px rgba(0, 0, 0, 0.1), 0px 4px 6px -2px rgba(0, 0, 0, 0.05)">
+                            <Image src={ChatImg} />
+                        </Button>
+                    </Box>
+                </Flex>
+
+                <Box >
+                    <Center>
+                        <Text lineHeight="133%" color="black" fontSize="15px" as='u' mb="20px">
+                            Click to see more
+                        </Text>
+                    </Center>
+                </Box>
+            </Box>
 
         </DatingAppBody>
     )
