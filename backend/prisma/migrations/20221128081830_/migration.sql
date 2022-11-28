d45ac0dd769a26bad2206238d7cf009ce892d3e7
Erxical <erxinxiao@gmail.com>
2022-11-28 15:19:14 +0700

    [Fixed] Restaurant

---
+++ backend/prisma/migrations/20221128081830_/migration.sql
@@ -0,0 +1,14 @@
+-- AlterTable
+ALTER TABLE "Analytic_User_Report" ALTER COLUMN "end" SET DEFAULT make_timestamp(3000, 1, 1, 0, 0, 0);
+
+-- AlterTable
+ALTER TABLE "QR" ALTER COLUMN "expired" SET DEFAULT make_timestamp(3000, 1, 1, 0, 0, 0);
+
+-- AlterTable
+ALTER TABLE "Restaurant_Like_By_User" ADD COLUMN     "updatedAt" TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "ShortLink" ALTER COLUMN "expired" SET DEFAULT make_timestamp(3000, 1, 1, 0, 0, 0);
+
+-- AlterTable
+ALTER TABLE "User_To_Room" ALTER COLUMN "lefted" SET DEFAULT make_timestamp(3000, 1, 1, 0, 0, 0);
