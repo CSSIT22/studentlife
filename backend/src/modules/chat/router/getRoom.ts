5ac57166578bb2a79c17897e0abb674f5282ec6e
Neng Sitiporn <sitiporn.wimo@kmutt.ac.th>
2022-11-30 00:41:45 +0700

    hot fix

--- backend/src/modules/chat/router/getRoom.ts
+++ backend/src/modules/chat/router/getRoom.ts
@@ -4,36 +4,11 @@ const getRoom = async (req: Request, res: Response) => {
     try {
         const user = req.user?.userId
         const prisma = res.prisma
-        const Room_list = await prisma.user_To_Room.findMany({
-            select: {
-                room: {
-                    select: {
-                        roomIndividual: {
-                            select: {
-                                chatWith: {
-                                    select: {
-                                        image: true,
-                                    },
-                                },
-                            },
-                        },
-                        roomId: true,
-                        chatColor: true,
-                        roomType: true,
-                        roomName: true,
-                        roomGroup: {
-                            select: {
-                                groupImg: true,
-                            },
-                        },
-                    },
-                },
-            },
-            where: {
-                userId: user,
-            },
+        const Room_list = await prisma.chat_Room.findFirst({
+          select:{
+            
+          }
         })
-        res.send(Room_list.map((e) => e.room))
     } catch {
         res.status(400).send("Error can't find room")
     }
