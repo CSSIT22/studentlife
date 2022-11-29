bef49bb1c2dfb2aa5afa0fba9979b85ceebb6a93
Kitibhum <98164016+Kitibhum@users.noreply.github.com>
2022-11-29 16:03:54 +0700

    Update index.ts

--- backend/src/modules/shortlink/index.ts
+++ backend/src/modules/shortlink/index.ts
@@ -2,6 +2,7 @@ import { verifyUser } from "./../backendService/middleware/verifyUser"
 import express from "express"
 import shortenlink from "./route/shortenlink"
 import getRedirect from "./route/getRedireect"
+import { prisma } from "@prisma/client"
 
 const shortlinkRoutes = express()
 shortlinkRoutes.use(express.json())
@@ -9,14 +10,5 @@ shortlinkRoutes.use(express.json())
 shortlinkRoutes.post("/generate", verifyUser, shortenlink)
 shortlinkRoutes.get("/redirect", verifyUser, getRedirect)
 
-shortlinkRoutes.get("/test", async (req, res) => {
-    const allUsers = await prisma.shortLink.findMany()
-    res.json(allUsers)
-})
-shortlinkRoutes.get("/", async (req, res) => {
-    const prisma = res.prisma
-    // const result = await prisma.shortLink.createMany({
 
-    // })
-})
 export default shortlinkRoutes
