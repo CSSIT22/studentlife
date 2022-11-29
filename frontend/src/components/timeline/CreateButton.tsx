9d20547f5bff12432614dcb1a7b9429e7065aee6
JoNoteJoMaMa <93032309+JoNoteJoMaMa@users.noreply.github.com>
2022-11-29 14:27:44 +0700

    Update - Create button
    เปลี่ยนสีปุ่มที่จะไปหน้าcreate post บน ไทม์ไลน์

--- frontend/src/components/timeline/CreateButton.tsx
+++ frontend/src/components/timeline/CreateButton.tsx
@@ -1,4 +1,5 @@
 import { Button, Text } from "@chakra-ui/react"
+import { AiFillSmile, AiFillPlusCircle } from "react-icons/all";
 import { useNavigate } from "react-router-dom";
 
 const CreateButton = () => {
@@ -8,8 +9,12 @@ const CreateButton = () => {
         navigate(path);
     }
     return (
-        <Button onClick={routeChange} size="lg" colorScheme="orange" p="1" borderRadius="50%" float="right" position="fixed" right="20%" bottom="5%">
-            <Text fontSize='2xl' textAlign="center"> + </Text>
+        // <Button onClick={routeChange} size="lg" colorScheme="orange" p="1" borderRadius="50%" float="right" position="fixed" right="20%" bottom="5%">
+        //     <Text fontSize='2xl' textAlign="center"> + </Text>
+        // </Button>
+
+        <Button onClick={routeChange} size="85px" bg="who" color="tomato" float="right" position="fixed" right="5%" bottom="5%" borderRadius={100} _hover={{ color: "black" }}>
+            <AiFillPlusCircle size="85px" />
         </Button>
     )
 }
