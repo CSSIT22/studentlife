c041af7ec422a07bfa848905a5d11365ccb2f22d
parnlees <yanisa0439@gmail.com>
2022-11-29 15:54:49 +0700

    fix floderlist

--- backend/src/modules/todolist/index.ts
+++ backend/src/modules/todolist/index.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import createTask from "./routes/createtask"
 import listTask from "./routes/listtask"
+import listFolder from "./routes/listfolder"
 // import createTask from "./routes/createTask"
 
 const todolistRoutes = express()
@@ -10,7 +11,7 @@ todolistRoutes.use(express.json())
 // todolistRoutes.delete("/deleteTask", deleteTask)
 // todolistRoutes.get("/editTask", editTask)
 todolistRoutes.get("/listtask", listTask)
-todolistRoutes.get("/listfolder", listfolder)
+todolistRoutes.get("/listfolder", listFolder)
 
 todolistRoutes.put("/createtask", (req, res) => {
     const taskName = req.body.taskName
