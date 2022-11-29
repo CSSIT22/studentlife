8b589468142953227b3947d55e3f03194407f2ee
Pn41e <98150552+Pn41e@users.noreply.github.com>
2022-11-29 07:58:44 +0700

    Add route to /blog/create/

--- frontend/src/components/timeline/CreateButton.tsx
+++ frontend/src/components/timeline/CreateButton.tsx
@@ -1,14 +1,14 @@
 import { Button, Text } from "@chakra-ui/react"
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-// let navigate = useNavigate();
-// const routeChange = () => {
-//     let path = '/blog/create/';
-//     navigate(path);
-// }
 const CreateButton = () => {
+    let navigate = useNavigate();
+    const routeChange = () => {
+        let path = '/blog/create/';
+        navigate(path);
+    }
     return (
-        <Button /*onClick={routeChange}*/ size="lg" colorScheme="orange" p="1" borderRadius="50%" float="right" position="fixed" right="20%" bottom="5%">
+        <Button onClick={routeChange} size="lg" colorScheme="orange" p="1" borderRadius="50%" float="right" position="fixed" right="20%" bottom="5%">
             <Text fontSize='2xl' textAlign="center"> + </Text>
         </Button>
     )
