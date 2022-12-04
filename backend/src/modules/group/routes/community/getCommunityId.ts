02f4a502ccec83e082a984f5ca75f40cac4529ab
Passakorn Puttama <boss4848988@gmail.com>
2022-12-04 11:40:29 +0700

    [FIX] getCommunityId API

--- backend/src/modules/group/routes/community/getCommunityId.ts
+++ backend/src/modules/group/routes/community/getCommunityId.ts
@@ -6,31 +6,66 @@ const getCommunityId = async (req: Request, res: Response) => {
     const id = req.params.id
 
     try {
+        //     const communityById = await prisma.community.findUnique({
+        //         where: {
+        //             communityId: id,
+        //         },
+        //         include: {
+        //             tags: true,
+        //         },
+        //     })
+        //     const tag = await prisma.tag.findMany({
+        //         where: {
+        //             tagId: { in: communityById?.tags.map((item: any) => item.tagId) },
+        //         },
+        //     })
+        //     const community = {
+        //         communityById,
+        //         tag,
+        //     }
+        //     console.log(community)
+        //     res.send(community)
 
-
-
-        
         const communityById = await prisma.community.findUnique({
             where: {
                 communityId: id,
             },
             include: {
                 tags: true,
+                member: true,
+                owner: true,
+                posts: true,
             },
         })
-
         const tag = await prisma.tag.findMany({
             where: {
                 tagId: { in: communityById?.tags.map((item: any) => item.tagId) },
             },
         })
-
-        const community = {
-            communityById,
-            tag,
+        if (communityById?.communityId === id) {
+            const data = {
+                communityId: communityById?.communityId,
+                communityName: communityById?.communityName,
+                communityDesc: communityById?.communityDesc,
+                communityPrivacy: communityById?.communityPrivacy,
+                communityPhoto: communityById?.communityPhoto,
+                tags: tag,
+                isOwner: communityById?.communityOwnerId === userId,
+                isPending: communityById?.member.some((item: any) => item.userId === userId && item.status === false),
+                isMember:
+                    communityById?.communityOwnerId === userId ||
+                    communityById?.member.some((item: any) => item.userId === userId && item.status === true),
+                memberCount: communityById?.member.length,
+            }
+            // res.send(data)
+            res.send(data)
+            res.sendStatus(200)
+            // console.log(data)
+            // console.log(isUserPending)
+        } else {
+            res.sendStatus(400)
         }
-        console.log(community)
-        res.send(community)
+        // console.log(tag)
     } catch (err) {
         console.log(err)
         res.status(400)
