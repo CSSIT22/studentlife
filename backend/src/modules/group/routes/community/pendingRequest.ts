913aaa8aa7bc3aab56f5d0b1d714c14eec069edf
Passakorn Puttama <boss4848988@gmail.com>
2022-12-04 11:38:28 +0700

    [TEST] pendingRequest API

--- backend/src/modules/group/routes/community/pendingRequest.ts
+++ backend/src/modules/group/routes/community/pendingRequest.ts
@@ -6,16 +6,9 @@ const pendingRequest = async (req: Request, res: Response) => {
     const body = req.body
     const userid = req.user?.userId
 
-    
-
-    
-
-    
-
     try {
-        
         const pendingRequest: any = {
-            userId: body.user,
+            userId: userid,
             communityId: body.communityId, //req.params.communityId
             roleId: "clavjs04i0004v32wxmjn3kvk",
             joined: new Date(),
@@ -24,13 +17,11 @@ const pendingRequest = async (req: Request, res: Response) => {
         const pending = await prisma.community_User.create({
             data: pendingRequest,
         })
-
+        console.log("pendingRequest", pendingRequest)
         res.status(201).send("Request has been send")
-        
     } catch (err) {
         console.log(err)
         res.status(400)
-        
     }
 }
 
