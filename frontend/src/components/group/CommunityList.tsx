88ab0917d175e36e56922136f700203ca97be7d9
Passakorn Puttama <boss4848988@gmail.com>
2022-12-05 03:21:46 +0700

    [TEST]

--- frontend/src/components/group/CommunityList.tsx
+++ frontend/src/components/group/CommunityList.tsx
@@ -1,13 +1,14 @@
 import { OwnCommunity } from "@apiType/group"
-import { HStack, Box, Image, Text } from "@chakra-ui/react"
+import { HStack, Box, Image, Text, Badge } from "@chakra-ui/react"
 import React, { FC } from "react"
 import { MdPublic, MdPublicOff } from "react-icons/md"
 import { Link } from "react-router-dom"
 
-const CommunityList: FC<OwnCommunity> = ({ communityName, lastActive, communityCoverPhoto, communityPrivacy, communityId }) => {
+const CommunityList: FC<OwnCommunity> = ({ pendingRequest, communityName, lastActive, communityCoverPhoto, communityPrivacy, communityId }) => {
     return (
         <Link to={`/groups/id/${communityId}`}>
             <Box
+                position='relative'
                 sx={{ transition: "transform ease 300ms" }}
                 _hover={{ cursor: "pointer", transform: "translate(0, -3px)", shadow: "xl" }}
                 borderRadius="md"
@@ -17,22 +18,38 @@ const CommunityList: FC<OwnCommunity> = ({ communityName, lastActive, communityC
                 color="black"
             >
                 <Box p={2} borderRadius="md">
+
                     <HStack gap={2}>
                         <Image ml={1} borderRadius="md" boxSize="55px" src={communityCoverPhoto ? communityCoverPhoto : "https://149366088.v2.pressablecdn.com/wp-content/uploads/2017/02/ubuntu-1704-default-wallpaper-750x422.jpg"} alt="Cover Photo" />
                         <div>
-                            <Box display="flex" alignItems="center" gap={1}>
+                            <HStack >
                                 {communityPrivacy ? <MdPublicOff /> : <MdPublic />}
                                 <Text display={{ base: 'block', md: 'none' }} as="b" fontSize="sm">
                                     {communityName}
                                 </Text>
+
                                 <Text display={{ base: 'none', md: 'block' }} as="b" fontSize="sm">
                                     {communityName.length > 16 ? communityName.slice(0, 14) + "..." : communityName}
                                 </Text>
-                            </Box>
+
+                            </HStack>
                             <Text fontSize="sm">Last active {lastActive} days ago</Text>
                         </div>
                     </HStack>
                 </Box>
+                <Badge
+                    display={pendingRequest ? 'block' : 'none'}
+                    colorScheme='red'
+                    position='absolute'
+                    top='5px'
+                    right='5px'
+                    padding='1'
+                    // padding='2px 8px'
+                    // fontSize='10px'
+                    borderRadius='50%'
+                    background='red'
+                // color='white'
+                ></Badge>
             </Box>
         </Link>
     )
