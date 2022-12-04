070c493adaaf2a7d068f1980c2039caf52ec5063
Passakorn Puttama <boss4848988@gmail.com>
2022-12-04 11:51:55 +0700

    [FIX] community member page

--- frontend/src/pages/groups/id/[communityID]/member.tsx
+++ frontend/src/pages/groups/id/[communityID]/member.tsx
@@ -63,120 +63,112 @@ const Member = () => {
                 activeBtn={1}
                 tags={tag}
             />
-            <Flex direction={{ base: "column-reverse", md: "row" }} gap={2} align="flex-start" mb={4}>
-                <Flex width="100%" gap={3} direction={"column"}>
-                    <MemberBox boxType="request" data={communityData} title="Request" subTitle="These people have been invited to join the goup" />
-                    <MemberBox boxType="admin" data={communityData} title="Admin" />
-                    <MemberBox boxType="moderator" data={communityData} title="Moderator" />
-                    <MemberBox boxType="member" data={communityData} title="Member" />
-                    <MemberBox boxType="blacklist" data={communityData} title="Blacklist" />
-                </Flex>
 
-                <Flex
-                    width='100%'
-                    mt='2'
-                    direction={{ base: "column-reverse", md: "row" }}
-                    gap={2}
+            <Flex
+                width='100%'
+                mt='2'
+                direction={{ base: "column-reverse", md: "row" }}
+                gap={2}
+                justifyContent='center'
+                alignItems='center'
+                mb={4}>
+                <VStack
                     justifyContent='center'
                     alignItems='center'
-                    mb={4}>
-                    <VStack
-                        justifyContent='center'
-                        alignItems='center'
-                        // align='center'
-                        maxWidth='580px'
-                        width="100%"
-                        gap={3}
+                    // align='center'
+                    maxWidth='580px'
+                    width="100%"
+                    gap={3}
+                >
+                    <MemberBox
+                        displayBox={community.pendingRequest != 0 && community?.isOwner}
+                        boxType="request"
+                        pendingRequest={community.pendingRequest}
+                        title="Request"
+                        subTitle="These people have been invited to join the group" />
+
+
+                    <Box
+                        display={community?.isMember || !community?.communityPrivacy ? "block" : 'none'}
+                        boxShadow={"2xl"} width={"100%"}
+                        backgroundColor="orange.400"
+                        p={4}
+                        borderRadius="md"
                     >
-                        <MemberBox
-                            displayBox={community.pendingRequest != 0 && community?.isOwner}
-                            boxType="request"
-                            pendingRequest={community.pendingRequest}
-                            title="Request"
-                            subTitle="These people have been invited to join the group" />
-
-
-                        <Box
-                            display={community?.isMember || !community?.communityPrivacy ? "block" : 'none'}
-                            boxShadow={"2xl"} width={"100%"}
-                            backgroundColor="orange.400"
-                            p={4}
-                            borderRadius="md"
-                        >
-                            <HStack align='flex-end'>
-                                <Text as="b" color={"white"}>
-                                    Admins & moderators
-                                </Text>
-                                <Text as='p' fontSize='sm' color='white'>
-                                    {
-                                        ` · ${community?.communityMember.admin.length + community?.communityMember.coAdmin.length + 1} `
-                                    }
-                                </Text>
-                            </HStack>
-                            <Flex mt={3} gap={2} direction="column" justify={"center"} align="center" width={"100%"}>
+                        <HStack align='flex-end'>
+                            <Text as="b" color={"white"}>
+                                Admins & moderators
+                            </Text>
+                            <Text as='p' fontSize='sm' color='white'>
+                                {
+                                    ` · ${community?.communityMember.admin.length + community?.communityMember.coAdmin.length + 1} `
+                                }
+                            </Text>
+                        </HStack>
+                        <Flex mt={3} gap={2} direction="column" justify={"center"} align="center" width={"100%"}>
+                            <UserList
+                                key={owner.userId}
+                                isHigherPriority={community?.isOwner}
+                                userId={owner.userId}
+                                avatar={owner.image}
+                                firstName={owner.fName}
+                                lastName={owner.lName}
+                                majorId={owner.majorId}
+                                role={'owner'}
+                            />
+                            {community?.communityMember?.admin.map((member: any) => (
                                 <UserList
-                                    key={owner.userId}
+                                    key={member.user.userId}
                                     isHigherPriority={community?.isOwner}
-                                    userId={owner.userId}
-                                    avatar={owner.image}
-                                    firstName={owner.fName}
-                                    lastName={owner.lName}
-                                    majorId={owner.majorId}
-                                    role={'owner'}
-                                />
-                                {community?.communityMember?.admin.map((member: any) => (
-                                    <UserList
-                                        key={member.user.userId}
-                                        isHigherPriority={community?.isOwner}
-                                        userId={member.user.userId}
-                                        avatar={member.user.image}
-                                        firstName={member.user.fName}
-                                        lastName={member.user.lName}
-                                        majorId={member.user.majorId}
-                                        role={'member'}
-                                    />))}
-                                {community?.communityMember?.coAdmin.map((member: any) => (
-                                    <UserList
-                                        key={member.user.userId}
-                                        isHigherPriority={community?.isOwner}
-                                        userId={member.user.userId}
-                                        avatar={member.user.image}
-                                        firstName={member.user.fName}
-                                        lastName={member.user.lName}
-                                        majorId={member.user.majorId}
-                                        role={'member'}
-                                    />))}
-                            </Flex>
-                            <HStack
-                                display={community?.communityMember?.member.length > 0 ? "flex" : "none"}
-                                align='flex-end' mt={3}>
-                                <Text as="b" color={"white"}>
-                                    Members
-                                </Text>
-                                <Text as='p' fontSize='sm' color='white'>
-                                    {
-                                        community?.memberCount - community?.pendingRequest.length > 0
-                                            ? (` · ${community?.memberCount - community?.pendingRequest.length}`)
-                                            : ""
-                                    }
-                                </Text>
-                            </HStack>
-                            <Flex mt={3} gap={2} direction="column" justify={"center"} align="center" width={"100%"}>
-                                {community?.communityMember?.member.map((member: any) => (
-                                    <UserList
-                                        key={member.user.userId}
-                                        isHigherPriority={community?.isOwner}
-                                        userId={member.user.userId}
-                                        avatar={member.user.image}
-                                        firstName={member.user.fName}
-                                        lastName={member.user.lName}
-                                        majorId={member.user.majorId}
-                                        role={'member'}
-                                    />))}
-                            </Flex>
-                        </Box>
-                    </VStack>
-                </Flex>
+                                    userId={member.user.userId}
+                                    avatar={member.user.image}
+                                    firstName={member.user.fName}
+                                    lastName={member.user.lName}
+                                    majorId={member.user.majorId}
+                                    role={'member'}
+                                />))}
+                            {community?.communityMember?.coAdmin.map((member: any) => (
+                                <UserList
+                                    key={member.user.userId}
+                                    isHigherPriority={community?.isOwner}
+                                    userId={member.user.userId}
+                                    avatar={member.user.image}
+                                    firstName={member.user.fName}
+                                    lastName={member.user.lName}
+                                    majorId={member.user.majorId}
+                                    role={'member'}
+                                />))}
+                        </Flex>
+                        <HStack
+                            display={community?.communityMember?.member.length > 0 ? "flex" : "none"}
+                            align='flex-end' mt={3}>
+                            <Text as="b" color={"white"}>
+                                Members
+                            </Text>
+                            <Text as='p' fontSize='sm' color='white'>
+                                {
+                                    community?.memberCount - community?.pendingRequest.length > 0
+                                        ? (` · ${community?.memberCount - community?.pendingRequest.length}`)
+                                        : ""
+                                }
+                            </Text>
+                        </HStack>
+                        <Flex mt={3} gap={2} direction="column" justify={"center"} align="center" width={"100%"}>
+                            {community?.communityMember?.member.map((member: any) => (
+                                <UserList
+                                    key={member.user.userId}
+                                    isHigherPriority={community?.isOwner}
+                                    userId={member.user.userId}
+                                    avatar={member.user.image}
+                                    firstName={member.user.fName}
+                                    lastName={member.user.lName}
+                                    majorId={member.user.majorId}
+                                    role={'member'}
+                                />))}
+                        </Flex>
+                    </Box>
+                </VStack>
+            </Flex>
         </AppBody>
     )
 }
