37485d37da7131900a0a361fb3c3216a561078ec
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2022-12-14 16:29:36 +0700

    [UPDATE] set breakpoint for each type of user

--- frontend/src/components/annoucement/AnnounceNav.tsx
+++ frontend/src/components/annoucement/AnnounceNav.tsx
@@ -1,21 +1,59 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { authContext } from 'src/context/AuthContext'
 import AppBody from '../share/app/AppBody'
 
-const AnnounceNav = (props:any) => {
-  return (
-    <AppBody
-            secondarynav={[
-                { name: "Announcement", to: "/announcement" },
-                { name: "Approval", to: "/announcement/approval" },
-                { name: "History", to: "/announcement/history" },
-                { name: "Recycle bin", to: "/announcement/recyclebin" },
-            ]}
-            p={{ md: "3rem" }}
-            
-        >
-            {props.children}
-        </AppBody>
-  )
+const AnnounceNav = (props: any) => {
+    const user = useContext(authContext)
+    const roles = user?.roles
+    console.log(roles);
+    const roleName: string[] = []
+
+    return (
+        <>
+            {(() => {
+                user?.roles.forEach((r) => {
+                    roleName.push(r.roleName)
+                })
+                if (roleName.includes('ANNOUNCEMENT_APPROVER')) {
+                    return (
+                        <AppBody
+                            secondarynav={[
+                                { name: "Announcement", to: "/announcement" },
+                                { name: "Approval", to: "/announcement/approval" },
+                                { name: "History", to: "/announcement/history" },
+                                { name: "Recycle bin", to: "/announcement/recyclebin" },
+                            ]}
+                            p={{ md: "3rem" }}
+                        >
+                        </AppBody>
+                    )
+                } else if (roleName.includes('ANNOUNCEMENT_ANNOUNCER')) {
+                    return (
+                        <AppBody
+                            secondarynav={[
+                                { name: "Announcement", to: "/announcement" },
+                                { name: "History", to: "/announcement/history" },
+                                { name: "Recycle bin", to: "/announcement/recyclebin" },
+                            ]}
+                            p={{ md: "3rem" }}
+                        >
+                        </AppBody>
+                    )
+                } else {
+                    return (
+                        <AppBody
+                            secondarynav={[
+                                { name: "Announcement", to: "/announcement" },
+                            ]}
+                            p={{ md: "3rem" }}
+                        >
+                        </AppBody>
+                    )
+                }
+            })()}
+        </>
+
+    )
 }
 
 export default AnnounceNav
