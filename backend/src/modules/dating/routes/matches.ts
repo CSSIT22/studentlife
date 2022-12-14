ad1b625f54ea6fd93955bac00710f08bcb3b8bd0
IMFROHT <89932182+Tatathoraberd@users.noreply.github.com>
2022-12-14 16:13:56 +0700

    update match and applied poll page

--- backend/src/modules/dating/routes/matches.ts
+++ backend/src/modules/dating/routes/matches.ts
@@ -12,6 +12,18 @@ matchesRoutes.get("/", (_, res) => {
 // Get matches people from heart history joined with user profile table
 matchesRoutes.get("/getMatches", verifyUser, async (req: Request, res: Response) => {
     // Put Songnapha's code here
+    try {
+        const reqUserId = req.user?.userId
+        const hearthistoryDB = await prisma.heart_History.findMany({
+            where: {
+                userId: reqUserId,
+                isSkipped: false,
+            },
+        })
+        return res.send(hearthistoryDB)
+    }catch (err) {
+        return res.status(404).send("Match not found")
+    }
 })
 
 export default matchesRoutes
