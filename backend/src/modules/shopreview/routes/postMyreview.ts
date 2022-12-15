34f08696082a16f5d04d6493cf6bbc0cf07d3e42
Pituchai <Phantom_14706@hotmail.com>
2022-12-15 16:39:19 +0700

    test1
    codas

--- backend/src/modules/shopreview/routes/postMyreview.ts
+++ backend/src/modules/shopreview/routes/postMyreview.ts
@@ -1,28 +1,31 @@
 import { Request, Response } from "express"
 
 const postmyreview = async (req: Request<any>, res: Response<any>) => {
-    // try {
-    //     const prisma = res.prisma
-    //     // const user = useContext(authContext)
-    //     const user = req.user?.userId
-    //     //console.log(req)
-    //     const postmyreview: any = {
-    //       //ให้ไปทำงานที่ sre_re ก่อน เอาไอดีมาก่่อน
-    //         reviewId: "016",
-    //         shopId: req.body.shopId,
-    //         userId: user,
-    //         text: req.body.text,
-    //         rating: 4,
-    //         likeReceived: 0,
-    //     }
-    //     const rev = await prisma.sReview_Shop_Review.create({
-    //       data: postmyreview,
-    //     })
-    //     res.send(rev)
-    // } catch (err) {
-    //     console.log(err)
-    //     res.send("some error")
-    // }
-    // console.log(req.body)
+    try {
+        const prisma = res.prisma
+        // const user = useContext(authContext)
+        const user = req.user?.userId
+        //console.log(req)
+        const postmyreview: any = {
+            //ให้ไปทำงานที่ sre_re ก่อน เอาไอดีมาก่่อน
+            // reviewId: req.body.reviewId,
+            shopId: req.body.shopId + "",
+            // resId: req.body.resId || null,
+            userId: user,
+            text: req.body.text,
+            rating: 5,
+            likeReceived: 0,
+        }
+        const rev = await prisma.sReview_Review.create({
+            data: {
+                ...postmyreview,
+            },
+        })
+        res.send(rev)
+    } catch (err) {
+        console.log(err)
+        res.send("some error")
+    }
+    console.log(req.body)
 }
 export default postmyreview
--- frontend/src/pages/shopreview/shopdetails/shop/[shopId].tsx
+++ frontend/src/pages/shopreview/shopdetails/shop/[shopId].tsx
@@ -16,18 +16,18 @@ import API from 'src/function/API'
 // main component
 const shopId = () => {
     const [rating, setRating] = useState(0) // rating star max = 5
-    //const [text, setText] = useState("") // review description 
+    const [text, setText] = useState("") // review description 
     const [detail, setDetail] = useState<any>([]) // shop's detail fetch from backend
-   //  const [review, setReview] = useState<any>([]) // user's reviews fetch from backend
-   // const navigate = useNavigate() // navigation function for handling navigate to shop's review comment page
-   // const { isOpen, onOpen, onClose } = useDisclosure() // chakra disclosure for open/close modal
+    const [review, setReview] = useState<any>([]) // user's reviews fetch from backend
+    const navigate = useNavigate() // navigation function for handling navigate to shop's review comment page
+    const { isOpen, onOpen, onClose } = useDisclosure() // chakra disclosure for open/close modal
     let param = useParams() // get data from param
     const buttons = []
 
-    // const Navigate = (target: any) => {
-    //     navigate(`/shopreview/review/${target}`)
-    //     window.scrollTo(0, 0)
-    // }
+    const Navigate = (target: any) => {
+         navigate(`/shopreview/review/${target}`)
+         window.scrollTo(0, 0)
+     }
 
     // handle onclick
     const onClick = (idx: any) => {
@@ -40,9 +40,7 @@ const shopId = () => {
         }
     }
     window.scrollTo(0, 0)
-    const { isOpen, onOpen, onClose } = useDisclosure()
 
-    const [text, setText] = useState("")
     
     const submit = () => {
 
@@ -60,18 +58,13 @@ const shopId = () => {
         API.get(`/shopreview/shopdetails/shop/${param.shopId}`)
             .then((res) => setDetail(res.data))
     }, [param])
-    const [review, setReview] = useState<any>([])
-    // const getReview = API.get("/shopreview/getmyreviewDb")
-    // useEffect(() => {
-    //     getReview.then((res) => {
-    //         setReview(res.data)
-    //     })
-    // }, [])
-    const navigate = useNavigate()
-    function Navigate(target: any) {
-        navigate(`/shopreview/review/${target}`)
-        window.scrollTo(0, 0)
-    }
+     const getReview = API.get("/shopreview/getmyreviewDb")
+    useEffect(() => {
+        getReview.then((res) => {
+            setReview(res.data)
+        })
+    }, [])
+  
     return (
         <AppBody>
             {detail.map((item: any, index: any) => (
