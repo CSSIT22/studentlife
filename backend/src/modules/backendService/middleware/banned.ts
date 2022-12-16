fafdad22210f87ef25e648f130dd9b7ed6f3c37f
sora-k46 <sorrawit456789@gmail.com>
2022-12-16 23:28:30 +0700

    Add NextFunction to banned middleware

--- backend/src/modules/backendService/middleware/banned.ts
+++ backend/src/modules/backendService/middleware/banned.ts
@@ -1,6 +1,6 @@
-import { Request, Response } from "express"
+import { Request, Response, NextFunction } from "express"
 
-export const banned = async (req: Request, res: Response) => {
+export const banned = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const bannedUser = await res.prisma.ban_Status.findFirst({
             where: {
@@ -12,9 +12,11 @@ export const banned = async (req: Request, res: Response) => {
         })
         if (bannedUser?.banTo || new Date() > new Date()) {
             req.logout((err) => {
+                console.log("YOU ARE BANNED FROM THE SYSTEM!")
                 return err
             })
         }
+        return next()
     } catch (err) {
         return res.status(400).send("Fail to Banned.")
     }
