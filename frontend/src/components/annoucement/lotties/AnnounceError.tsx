5e106d54dec60730dc6d32328a7034845d2ffa55
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2022-12-17 01:25:07 +0700

    [UPDATE] increase size of error animation

--- frontend/src/components/annoucement/lotties/AnnounceError.tsx
+++ frontend/src/components/annoucement/lotties/AnnounceError.tsx
@@ -8,8 +8,8 @@ const AnnounceError = () => {
         <Box width={"100%"} height="100%">
             <Box pos={"fixed"} bottom={{ base: "40%", md: "38%", lg: "30%" }} left={{ base: "30%", md: "38%", lg: "42%" }}>
                 <Flex flexDirection={"column"} justifyContent={"center"} alignItems={"center"}>
-                    <Lottie animationData={annError} loop={true} style={{ maxWidth: "10rem" }} />
-                    <Heading textAlign={"center"} >LOADING...</Heading>
+                    <Lottie animationData={annError} loop={true} style={{ maxWidth: "15rem" }} />
+                    {/* <Heading textAlign={"center"} >LOADING...</Heading> */}
                 </Flex>
             </Box>
         </Box>
