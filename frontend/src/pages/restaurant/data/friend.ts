b945f21c36cd34551f35540e40e4d898baabd17c
Jakrapat-M <jakrapat.matures@mail.kmutt.ac.th>
2022-12-17 00:44:26 +0700

    share function navigate to /chat/:roomid/:resid

--- frontend/src/pages/restaurant/data/friend.ts
+++ frontend/src/pages/restaurant/data/friend.ts
@@ -1,33 +1,24 @@
 export const friend = [
     {
-        Id:"101",
-        name: "realไร่",
-        picture: "https://cdn.discordapp.com/attachments/900658140704559116/1023630299717963848/92C5C070-F9DC-44BA-AF57-F4162FFDCA03.jpg",
-    },
-    {   
-        Id:"201",
-        name: "ยืนหนึง",
-        picture: "https://cdn.discordapp.com/attachments/900658140704559116/1023887278273208360/2FB66B55-BCAA-4DE2-BE62-02B193D6369E.jpg",
-    },
-    {
-        Id:"301",
-        name: "ผมรู้ผมเห็น",
-        picture: "https://cdn.discordapp.com/attachments/900658140704559116/1022175158334656573/IMG_20220918_224548.jpg",
+        roomId:"101",
+        
+        group:[{
+            roomnName: "realไร่",}],
+        nick:[{
+            // nameWho:[{
+            //     image: "https://cdn.discordapp.com/attachments/900658140704559116/1023630299717963848/92C5C070-F9DC-44BA-AF57-F4162FFDCA03.jpg",
+            // }]
+        }]
     },
     {
-        Id:"401",
-        name: "Night N",
-        picture: "",
+        roomId:"201",
+        
+        group:[{
+            roomnName: "realไร่",}],
+        nick:[{
+            // nameWho:[{
+            //     image: "https://cdn.discordapp.com/attachments/900658140704559116/1023630299717963848/92C5C070-F9DC-44BA-AF57-F4162FFDCA03.jpg",
+            // }]
+        }]
     },
-    {
-        Id:"501",
-        name: "Pun J",
-        picture: "",
-    },
-    {
-        Id:"601",
-        name: "Ping T",
-        picture: "",
-    },
-
 ]
