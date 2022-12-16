672a288bdf4f5ca37198bc1cb0c49f14bc96442d
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2022-12-16 14:59:11 +0700

    aads
    sdffds

--- backend/src/modules/shortlink/index.ts
+++ backend/src/modules/shortlink/index.ts
@@ -1,15 +1,9 @@
 import { verifyUser } from "./../backendService/middleware/verifyUser"
 import express from "express"
-<<<<<<< Updated upstream
 import shortenlink from "./route/shortenlink"
 import getRedirect from "./route/getRedireect"
 import { prisma } from "@prisma/client"
 import customlink from "./route/customlink"
-=======
-import { PrismaClient } from '@prisma/client'
-import shortenlink from "./route/shortenlink"
-const prisma = new PrismaClient()
->>>>>>> Stashed changes
 
 const shortlinkRoutes = express()
 shortlinkRoutes.use(express.json())
@@ -17,12 +11,4 @@ shortlinkRoutes.post("/generate", verifyUser, shortenlink)
 shortlinkRoutes.post("/custom" , verifyUser , customlink)
 shortlinkRoutes.get("/redirect", verifyUser, getRedirect)
 
-<<<<<<< Updated upstream
-=======
-shortlinkRoutes.post("/generate", verifyUser,shortenlink)
-
-shortlinkRoutes.get("/", async (req, res) => {
-    const prisma = res.prisma
-})
->>>>>>> Stashed changes
 export default shortlinkRoutes
