6278987117014c4b534d527a076c2a7e716b266f
Pattarapon Koomphon <45892157+Kevinindy2k@users.noreply.github.com>
2022-12-17 19:16:09 +0700

    Update

--- backend/src/modules/user/routers/getFollowList.ts
+++ backend/src/modules/user/routers/getFollowList.ts
@@ -1,1 +1,15 @@
 import { Request, Response } from "express"
+
+const getFriendListuser = async (req: Request, res: Response) => {
+    try {
+        const { prisma } = res
+        const userId = req.params["id"]
+        const profile = await prisma.user_Profile.findUniqueOrThrow({ where: { userId } })
+        res.status(200).json({ user: profile })
+        console.log(profile)
+    } catch (err) {
+        return res.redirect(`${process.env.SUCCESS_REDIRECT_URL}/NotFound`)
+    }
+}
+
+export default getFriendListuser
