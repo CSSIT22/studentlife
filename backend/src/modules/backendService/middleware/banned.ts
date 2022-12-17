a67ab54da80adaefdfb04e1247d86a7e851ff443
sora-k46 <sorrawit456789@gmail.com>
2022-12-17 18:37:21 +0700

    Implement outdated ban period

--- backend/src/modules/backendService/middleware/banned.ts
+++ backend/src/modules/backendService/middleware/banned.ts
@@ -9,11 +9,25 @@ export const banned = async (req: Request, res: Response, next: NextFunction) =>
                     gte: 10,
                 },
             },
+            select: {
+                banTo: true,
+                userId: true,
+                reason: true,
+            },
         })
         if (bannedUser && bannedUser.banTo > new Date()) {
             req.logout((err) => {
                 console.log("YOU ARE BANNED FROM THE SYSTEM!")
                 return err
+            })
+        } else if (bannedUser && bannedUser.banTo < new Date()) {
+            await res.prisma.ban_Status.delete({
+                where: {
+                    userId_reason: {
+                        userId: bannedUser.userId,
+                        reason: bannedUser.reason,
+                    },
+                },
             })
         }
         return next()
