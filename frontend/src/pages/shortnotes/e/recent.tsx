1e067ab42a15d30dabb0ed8133d926ad19f3e410
Pasinun.w <95624019+Unuisad@users.noreply.github.com>
2022-12-17 15:45:23 +0700

    [ADD] isLoad animation

--- frontend/src/pages/shortnotes/e/recent.tsx
+++ frontend/src/pages/shortnotes/e/recent.tsx
@@ -1,16 +1,20 @@
 import React, { FC, useContext, useEffect, useState } from "react"
-import { Box, Heading, VStack } from "@chakra-ui/react"
+import { Box, Heading, useBoolean, VStack } from "@chakra-ui/react"
 import { Link, useNavigate, useParams } from "react-router-dom"
 import API from "src/function/API"
 import AppBody from "src/components/share/app/AppBody"
 import ExtraRsnList from "src/components/shortnotes/extraRsnList"
 import { FaLock, FaLockOpen, FaUnlock } from "react-icons/fa"
 import { TiLockClosed, TiLockOpen } from "react-icons/ti"
+import loading from "../lottie/loading.json";
+import Lottie from "lottie-react"
+
 const extraRsn = () => {
     useEffect(() => {
         API.get("/shortnotes/getExtraRsn").then((item) => {
             setRecent(item.data)
-            console.log(item.data)
+        }).then((res) => {
+            setIsLoad.off()
         })
     }, [])
     const [recent, setRecent] = useState<any>([])
@@ -32,28 +36,40 @@ const extraRsn = () => {
     }, [access])
 
     const navigate = useNavigate()
+
+    const [isLoad, setIsLoad] = useBoolean(true)
+    const style = {
+        height: 100,
+    };
     return (
         <AppBody>
             <Heading fontSize={"2xl"}>Recent View</Heading>
             <br></br>
-            <VStack gap={2}>
-                {recent.map((recent: any, key: any) => (
-                    <Box as="button"
-                        w={"100%"} onClick={() => {
+            {isLoad ?
+                <Box h={180}>
+                    <Lottie style={style} animationData={loading}></Lottie>
+                </Box>
+                :
+                <VStack gap={2}>
+                    {recent.map((recent: any, key: any) => (
+                        <Box as="button"
+                            w={"100%"} onClick={() => {
 
-                            navigate({
-                                pathname: "../shortnotes/" + recent.shortNote.snId,
-                            })
-                        }}>
-                        {accessId.includes(recent.shortNote.snId) ?
-                            <ExtraRsnList key={key} topic={recent.shortNote.snName} course={recent.shortNote.course.courseName} date={recent.viewedAt} lock={recent.shortNote.isPublic ? "" : <TiLockOpen />}></ExtraRsnList>
-                            :
-                            <ExtraRsnList key={key} topic={recent.shortNote.snName} course={recent.shortNote.course.courseName} date={recent.viewedAt} lock={recent.shortNote.isPublic ? "" : <TiLockClosed />}></ExtraRsnList>
+                                navigate({
+                                    pathname: "../shortnotes/" + recent.shortNote.snId,
+                                })
+                            }}>
+                            {accessId.includes(recent.shortNote.snId) ?
+                                <ExtraRsnList key={key} topic={recent.shortNote.snName} course={recent.shortNote.course.courseName} date={recent.viewedAt} lock={recent.shortNote.isPublic ? "" : <TiLockOpen />}></ExtraRsnList>
+                                :
+                                <ExtraRsnList key={key} topic={recent.shortNote.snName} course={recent.shortNote.course.courseName} date={recent.viewedAt} lock={recent.shortNote.isPublic ? "" : <TiLockClosed />}></ExtraRsnList>
 
-                        }
-                    </Box>
-                ))}
-            </VStack>
+                            }
+                        </Box>
+                    ))}
+                </VStack>
+            }
+
         </AppBody>
     )
 }
