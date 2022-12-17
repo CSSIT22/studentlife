277daacc7894f165cff188e1bce8793b795de55c
Eve-nichakon <91275476+nichakontae@users.noreply.github.com>
2022-12-17 10:58:08 +0700

    [UPDATE] set breakpoint on index

--- frontend/src/pages/announcement/index.tsx
+++ frontend/src/pages/announcement/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { IoIosAddCircle } from "react-icons/io"
 import { Link } from "react-router-dom"
 import HeaderPage from "../../components/annoucement/HeaderPage"
@@ -11,6 +11,7 @@ import API from "src/function/API"
 import AnnounceError from "src/components/annoucement/AnnounceError"
 import AnnounceLoading from "src/components/annoucement/AnnounceLoading"
 import AnnounceNav from "src/components/annoucement/AnnounceNav"
+import { authContext } from "src/context/AuthContext"
 
 
 const index = () => {
@@ -26,6 +27,8 @@ const index = () => {
     const getpostidAndpinstatus = () => {
         settoggle(!toggle)
     }
+    const user = useContext(authContext)
+    const roleName: string[] = []
     return (
         <AnnounceNav>
             {(() => {
@@ -37,52 +40,102 @@ const index = () => {
                     if (isError) {
                         return <AnnounceError />
                     } else {
-                        return (
-                            <>
-                                <Flex alignItems={"center"}>
-                                    <HeaderPage head="Announcement" />
-                                    <Link to={"/announcement/create"}>
-                                        <IoIosAddCircle fontSize={"2rem"} color="#E65300" />
-                                    </Link>
-                                </Flex>
-                                {allPost
-                                    .filter((p) => {
-                                        return p.annPin[0].status == true
-                                    })
-                                    .map((el) => {
-                                        return (
-                                            <PostOnAnnouncementPage
-                                                topic={el.annLanguage[0].annTopic}
-                                                sender={el.annCreator.fName + " " + el.annCreator.lName}
-                                                status={el.annPin[0].status}
-                                                allPost={allPost}
-                                                setAllPost={setAllPost}
-                                                id={el.postId}
-                                                key={el.postId}
-                                                onClick={getpostidAndpinstatus}
-                                            />
-                                        )
-                                    })}
-                                {allPost
-                                    .filter((p) => {
-                                        return p.annPin[0].status == false
-                                    })
-                                    .map((el) => {
-                                        return (
-                                            <PostOnAnnouncementPage
-                                                topic={el.annLanguage[0].annTopic}
-                                                sender={el.annCreator.fName + " " + el.annCreator.lName}
-                                                status={el.annPin[0].status}
-                                                allPost={allPost}
-                                                setAllPost={setAllPost}
-                                                id={el.postId}
-                                                key={el.postId}
-                                                onClick={getpostidAndpinstatus}
-                                            />
-                                        )
-                                    })}
-                            </>
-                        )
+                        user?.roles.forEach((r) => {
+                            roleName.push(r.roleName)
+                        })
+                        if (roleName.includes('ANNOUNCEMENT_ANNOUNCER') || roleName.includes('ANNOUNCEMENT_APPROVER')) {
+                            return (
+                                <>
+                                    <Flex alignItems={"center"}>
+                                        <HeaderPage head="Announcement" />
+                                        <Link to={"/announcement/create"}>
+                                            <IoIosAddCircle fontSize={"2rem"} color="#E65300" />
+                                        </Link>
+                                    </Flex>
+                                    {allPost
+                                        .filter((p) => {
+                                            return p.annPin[0].status == true
+                                        })
+                                        .map((el) => {
+                                            return (
+                                                <PostOnAnnouncementPage
+                                                    topic={el.annLanguage[0].annTopic}
+                                                    sender={el.annCreator.fName + " " + el.annCreator.lName}
+                                                    status={el.annPin[0].status}
+                                                    allPost={allPost}
+                                                    setAllPost={setAllPost}
+                                                    id={el.postId}
+                                                    key={el.postId}
+                                                    onClick={getpostidAndpinstatus}
+                                                />
+                                            )
+                                        })}
+                                    {allPost
+                                        .filter((p) => {
+                                            return p.annPin[0].status == false
+                                        })
+                                        .map((el) => {
+                                            return (
+                                                <PostOnAnnouncementPage
+                                                    topic={el.annLanguage[0].annTopic}
+                                                    sender={el.annCreator.fName + " " + el.annCreator.lName}
+                                                    status={el.annPin[0].status}
+                                                    allPost={allPost}
+                                                    setAllPost={setAllPost}
+                                                    id={el.postId}
+                                                    key={el.postId}
+                                                    onClick={getpostidAndpinstatus}
+                                                />
+                                            )
+                                        })}
+                                </>
+                            )
+                        } else {
+                            return (
+                                <>
+                                    <Flex alignItems={"center"}>
+                                        <HeaderPage head="Announcement" />
+                                    </Flex>
+                                    {allPost
+                                        .filter((p) => {
+                                            return p.annPin[0].status == true
+                                        })
+                                        .map((el) => {
+                                            return (
+                                                <PostOnAnnouncementPage
+                                                    topic={el.annLanguage[0].annTopic}
+                                                    sender={el.annCreator.fName + " " + el.annCreator.lName}
+                                                    status={el.annPin[0].status}
+                                                    allPost={allPost}
+                                                    setAllPost={setAllPost}
+                                                    id={el.postId}
+                                                    key={el.postId}
+                                                    onClick={getpostidAndpinstatus}
+                                                />
+                                            )
+                                        })}
+                                    {allPost
+                                        .filter((p) => {
+                                            return p.annPin[0].status == false
+                                        })
+                                        .map((el) => {
+                                            return (
+                                                <PostOnAnnouncementPage
+                                                    topic={el.annLanguage[0].annTopic}
+                                                    sender={el.annCreator.fName + " " + el.annCreator.lName}
+                                                    status={el.annPin[0].status}
+                                                    allPost={allPost}
+                                                    setAllPost={setAllPost}
+                                                    id={el.postId}
+                                                    key={el.postId}
+                                                    onClick={getpostidAndpinstatus}
+                                                />
+                                            )
+                                        })}
+                                </>
+                            )
+                        }
+
                     }
 
                 }
