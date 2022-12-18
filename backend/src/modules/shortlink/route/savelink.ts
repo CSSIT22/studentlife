19becf4b41b27bba2f791953fc8285610afa8053
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2022-12-19 00:19:10 +0700

    dd
    d

--- backend/src/modules/shortlink/route/savelink.ts
+++ backend/src/modules/shortlink/route/savelink.ts
@@ -7,7 +7,7 @@ const savelink = async(req : Request , res : Response)=>{
     try{
         const prisma = res.prisma
 
-        const result = await prisma.ShortLink_Save.create(
+        const result = await res.prisma.shortlink
             )
         // const result = await prisma.shortLink.create({
         //     data:{
