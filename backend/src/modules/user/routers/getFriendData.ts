6c52efddbefb97cbf30aad0b4eb5df0c5ae13daa
MrT-jesus-1995 <livemlg1995@gmail.com>
2022-12-18 15:30:10 +0700

    Change user data from backend

--- backend/src/modules/user/routers/getFriendData.ts
+++ backend/src/modules/user/routers/getFriendData.ts
@@ -4,8 +4,49 @@ const getFriendData = async (req: Request, res: Response) => {
     try {
         const { prisma } = res
         const userId = req.params["id"]
-        const profile = await prisma.user_Profile.findUniqueOrThrow({ where: { userId } })
-        res.status(200).json({ user: profile })
+        const profile = await prisma.user_Profile.findUniqueOrThrow({
+            where: {
+                userId,
+            },
+            select: {
+                studentId: true,
+                fName: true,
+                lName: true,
+                image: true,
+                details: {
+                    select: {
+                        phone: true,
+                        sex: true,
+                        hobby: true,
+                        birth: true,
+                        year: true,
+                        address: true,
+                    },
+                },
+                studentMajor: {
+                    select: {
+                        majorId: true,
+                        facultyId: true,
+                    },
+                },
+            },
+        })
+        res.status(200).json({
+            user: {
+                studentId: profile.studentId,
+                fName: profile.fName,
+                lName: profile.lName,
+                image: profile.image,
+                phone: profile.details?.phone,
+                sex: profile.details?.sex,
+                hobby: profile.details?.hobby,
+                birth: profile.details?.birth,
+                year: profile.details?.year,
+                address: profile.details?.address,
+                majorId: profile.studentMajor?.majorId,
+                facultyId: profile.studentMajor?.facultyId,
+            },
+        })
         console.log(profile)
     } catch (err) {
         return res.redirect(`${process.env.SUCCESS_REDIRECT_URL}/NotFound`)
