f77266d7e7d3f4e4d65e18a34844e83908c51b72
Thitipa <thitipanet@gmail.com>
2022-12-18 16:44:39 +0700

    Fix all activity poll page bug 07

--- frontend/src/components/dating/DatingYourActivityBox.tsx
+++ frontend/src/components/dating/DatingYourActivityBox.tsx
@@ -33,16 +33,22 @@ const DatingYourActivityBox: FC<{ poll: PollInfo[] }> = ({ poll }) => {
     // Convert date in to format that easy to read
     function handlePollDate(dateTime: string) {
         const chooseDate = new Date(dateTime)
-        const d = chooseDate.toLocaleDateString()
-        // console.log(d)
-        return d.substring(3, 5) + "/" + d.substring(0, 2) + "/" + chooseDate.getFullYear()
+        chooseDate.setHours(chooseDate.getHours() - 7);
+        // console.log(chooseDate.getMonth())
         // return chooseDate.getDate() + "/" + (chooseDate.getMonth() + 1) + "/" + chooseDate.getFullYear()
+        // const d = chooseDate.toLocaleDateString()
+        // const strDate = chooseDate.toLocaleDateString()
+        // return chooseDate.getDay() + "/" + chooseDate.getMonth() + "/" + chooseDate.getFullYear()
+        const d = chooseDate.toISOString()
+        return d.substring(8, 10) + "/" + d.substring(5, 7) + "/" + chooseDate.getFullYear()
     }
 
     // Convert time in to format that easy to read
     function hanlePollTime(dateTime: string) {
-        const time = new Date(dateTime)
-        let hours = time.getHours() + 5
+        const result = new Date(dateTime);
+        result.setHours(result.getHours() - 7);
+        const time = new Date(result)
+        let hours = time.getHours()
         let minutes = time.getMinutes()
         let ampm = hours >= 12 ? "pm" : "am"
         hours = hours % 12
