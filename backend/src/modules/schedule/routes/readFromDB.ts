06814b3f8c69d55c8f9ed8496f579f12bf723604
saranratR <fahhiha36@gmail.com>
2022-12-19 04:29:02 +0700

    [FIX] readDB

--- backend/src/modules/schedule/routes/readFromDB.ts
+++ backend/src/modules/schedule/routes/readFromDB.ts
@@ -8,14 +8,7 @@ readFromDBRoutes.get("/", (_, res) => {
     return res.send("Schedule module read DB API")
 })
 //schedule module
-readFromDBRoutes.get("/timetable", async(req:Request, res: Response) =>{
-    try{
-        const timetableDB = await prisma.timetable.findMany()
-        res.send(timetableDB)
-    }catch(err){
-        res.status(404).send("Timetable not found")
-    }
-})
+
 readFromDBRoutes.get("/event", async (req: Request, res: Response) => {
     try {
         const eventDB = await prisma.event.findMany()
@@ -24,22 +17,7 @@ readFromDBRoutes.get("/event", async (req: Request, res: Response) => {
         res.status(404).send("Event not found")
     }
 })
-readFromDBRoutes.get("/event_Place", async (req: Request, res: Response) => {
-    try {
-        const event_PlaceDB = await prisma.event_Place.findMany()
-        res.send(event_PlaceDB)
-    }catch (err){
-        res.status(404).send("Event place not found")
-    }
-})
-readFromDBRoutes.get("/event_Type", async (req: Request, res: Response) => {
-    try {
-        const event_TypeDB = await prisma.event_Type.findMany()
-        res.send(event_TypeDB)
-    }catch (err){
-        res.status(404).send("Event place not found")
-    }
-})
+
 readFromDBRoutes.get("/assignment", async (req: Request, res: Response) =>{
     try{
         const assignmentDB = await prisma.assignment.findMany()
