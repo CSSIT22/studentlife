9be5d01a81466be0ac5a402154fe755279bd4e01
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2022-12-19 01:58:42 +0700

    update savelnik
    status 200 OK

--- backend/src/modules/shortlink/index.ts
+++ backend/src/modules/shortlink/index.ts
@@ -6,17 +6,18 @@ import { prisma } from "@prisma/client"
 import customlink from "./route/customlink"
 import getUserData from "./route/getUserData"
 import getlinkData from "./route/getlinkData"
-import savelink from "./route/savelink"
+import savelink from "./route/funcsavelink"
+import funcsavelink from "./route/funcsavelink"
 
 const shortlinkRoutes = express()
 shortlinkRoutes.use(express.json())
 shortlinkRoutes.post("/generate", verifyUser, shortenlink)
-shortlinkRoutes.post("/custom" , verifyUser , customlink)
+shortlinkRoutes.post("/custom", verifyUser, customlink)
+shortlinkRoutes.post("/funcsavelink", verifyUser, funcsavelink)
 shortlinkRoutes.get("/redirect", verifyUser, getRedirect)
-shortlinkRoutes.get("/getUser", getUserData )
-shortlinkRoutes.get("/getlink", getlinkData )
-shortlinkRoutes.get("/savelink",savelink )
+shortlinkRoutes.get("/getUser", getUserData)
+shortlinkRoutes.get("/getlink", getlinkData)
+
 
 
 export default shortlinkRoutes
-         
---
+++ backend/src/modules/shortlink/route/funcsavelink.ts
@@ -0,0 +1,29 @@
+import {Request , Response } from "express"
+import { customAlphabet } from "nanoid"
+
+const funcsavelink = async(req : Request , res : Response)=>{
+    
+    const body = req.body
+    const userId = req.user?.userId || ""
+    const customNanoid = customAlphabet("abcdefghijklmnopqrstuvwxyz", 6)
+
+    console.log(req.body.title)
+
+    try{
+        const prisma = res.prisma
+
+        const result = await prisma.shortLink_Save.create({
+            data: {
+                
+                title:req.body.title,
+                desc:""
+            },
+        })
+        console.log(result)
+        res.status(200).json({result : result})
+    }catch (err){
+        console.log(err)
+        res.status(500).json({message:"error" , err: err})
+    }
+}
+export default funcsavelink
