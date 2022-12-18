ba5fca8c6333e0601607e808fafda71814e3ddac
Pattarapon Koomphon <45892157+Kevinindy2k@users.noreply.github.com>
2022-12-18 20:39:43 +0700

    Up

---
+++ backend/src/modules/user/routers/getFollower.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express"
+
+const getFollow = async (req: Request, res: Response) => {
+    try {
+        const { prisma } = res
+        const userId = req.user ? req.user.userId : ""
+        const isBlocked = await prisma.user_Blocked.findFirst({
+            // include: { anotherUserId: true },
+            where: {
+                userId: userId,
+                anotherUserId: req.params.userId,
+            },
+        })
+
+        if (isBlocked) {
+            return res.redirect(`${process.env.SUCCESS_REDIRECT_URL}/NotFound`)
+        }
+
+        // res.status(200).json({ user: profile })
+    } catch (err) {
+        return res.redirect(`${process.env.SUCCESS_REDIRECT_URL}/NotFound`)
+    }
+}
+
+export default getFollow
