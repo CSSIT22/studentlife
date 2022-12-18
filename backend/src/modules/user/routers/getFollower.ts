8d876b7e7edb6455d48306a865d1775d7a2f80ca
Pattarapon Koomphon <45892157+Kevinindy2k@users.noreply.github.com>
2022-12-18 23:48:50 +0700

    Update getFollower.ts

--- backend/src/modules/user/routers/getFollower.ts
+++ backend/src/modules/user/routers/getFollower.ts
@@ -4,8 +4,8 @@ const getFollow = async (req: Request, res: Response) => {
     try {
         const { prisma } = res
         const userId = req.user ? req.user.userId : ""
-        const Follower = await prisma.user_Blocked.findFirst({
-            // include: { anotherUserId: true },
+        const Follower = await prisma.follow.findFirst({
+            include: { follower: true },
             where: {
                 userId: userId,
                 anotherUserId: req.params.id,
