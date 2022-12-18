5e04efab13df2734d9547dbb238b913f7154f14e
RNU1815 <kunsee555@gmail.com>
2022-12-18 23:29:51 +0700

    Count react

--- backend/src/modules/timeline/routes/getReacted.ts
+++ backend/src/modules/timeline/routes/getReacted.ts
@@ -4,7 +4,15 @@ import getPostList from "./getPostList"
 const getReacted = async (req: Request, res: Response) => {
     try {
         const { prisma } = res
-        const getReact = await prisma.student_Reacted.findMany()
+        const getReact = await prisma.student_Reacted.findMany({
+            // include: {
+            //     _count: {
+            //         select: {
+            //             postId: true,
+            //         },
+            //     },
+            // },
+        })
         res.send(getReact)
     } catch (error) {
         res.status(400).send("Error: can not get reaction")
