bb3892ff032674e72c3fa2e5b51f6c45bebe88cc
Pn41e <98150552+Pn41e@users.noreply.github.com>
2022-12-18 21:32:16 +0700

    Update Feed and Post to return 20 more post when reached bottom of webpage (2.0)

--- backend/src/modules/timeline/routes/getStudentPost.ts
+++ backend/src/modules/timeline/routes/getStudentPost.ts
@@ -3,18 +3,19 @@ import { Request, Response } from "express"
 const getStudentPost = async (req: Request, res: Response) => {
     try {
         const { prisma } = res
-        var i = 0
+        const i = parseInt(req.params.i) * 20
         const getStudentP = await prisma.student_Post.findMany({
-            take: 20,
             skip: i,
+            take: 20,
             orderBy: { score: "desc" },
             include: {
                 postOwner: true,
             },
         })
-        i += 20
         res.send(getStudentP)
-    } catch (error) {}
+    } catch (error) {
+        res.status(400).send("Error: can not get post in getStudentPost.ts")
+    }
 }
 
 export default getStudentPost
